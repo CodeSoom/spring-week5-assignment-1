98fef34660d3d146e4bceb015715372bd4347db8
wenodev <logicalstark1105@gmail.com>
2021-02-23 01:46:08 +0900

    entity 테이블 mapping

--- app/src/main/java/com/codesoom/assignment/domain/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -1,10 +1,21 @@
 package com.codesoom.assignment.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
+@NoArgsConstructor
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String email;
     private String password;
