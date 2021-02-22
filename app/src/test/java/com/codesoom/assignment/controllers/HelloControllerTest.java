0edaa24a84335de55bc5e9b593ed58322064e78b
Lee Dong Wook <sh95119@gmail.com>
2021-02-22 20:13:41 +0900

    DOC: add doc of HelloController

--- app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
@@ -1,10 +1,13 @@
 package com.codesoom.assignment.controllers;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class HelloControllerTest {
+
+    @DisplayName("sayHello()를 호출하면 Hello, world!를 반환한다.")
     @Test
     void sayHello() {
         HelloController controller = new HelloController();
