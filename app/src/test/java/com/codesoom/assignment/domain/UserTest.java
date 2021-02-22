434dc54c7edc1e60b0815049b7380494ac5aca36
wenodev <logicalstark1105@gmail.com>
2021-02-23 01:41:47 +0900

    q변수확인  test-fail

--- app/src/test/java/com/codesoom/assignment/domain/UserTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/UserTest.java
@@ -3,13 +3,21 @@ package com.codesoom.assignment.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
+    User user;
+
     @BeforeEach
     void setUp(){
-        User user = new User("name", "email", "password");
+        user = new User("name", "email", "password");
+    }
+
+    @Test
+    void test_변수값확인(){
+        assertThat("name").isEqualTo(user.getName());
     }
 
 }
