03f0fa8b5966fa395da45359a319597139ab943b
JSS <rlawlstjd077@gmail.com>
2021-02-22 21:12:37 +0900

    test: Add 'User' test

---
+++ app/src/test/java/com/codesoom/assignment/domain/UserTest.java
@@ -0,0 +1,27 @@
+package com.codesoom.assignment.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+    final String NAME = "User";
+    final String EMAIL = "User@gmail.com";
+    final String PASSWORD = "1234";
+
+    @DisplayName("주어진 유저 정보로 유저를 생성합니다.")
+    @Test
+    void testCreateUserWithBuilder() {
+        User user = User.builder()
+            .name(NAME)
+            .email(EMAIL)
+            .password(PASSWORD)
+            .build();
+
+        assertThat(user.getName()).isEqualTo(NAME);
+        assertThat(user.getEmail()).isEqualTo(EMAIL);
+        assertThat(user.getPassword()).isEqualTo(PASSWORD);
+    }
+}
