0f6737821e76ef9cb04f7d4577ac0abf9ceb28a0
newoo <newoo4297@naver.com>
2021-02-23 19:53:09 +0900

    Pass getUsers method tests

--- app/src/main/java/com/codesoom/assignment/application/UserService.java
+++ app/src/main/java/com/codesoom/assignment/application/UserService.java
@@ -1,4 +1,22 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.domain.ProductRepository;
+import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.domain.UserRepository;
+import com.github.dozermapper.core.Mapper;
+
+import java.util.List;
+
 public class UserService {
+    private final Mapper mapper;
+    private final UserRepository userRepository;
+
+    public UserService(Mapper dozerMapper, UserRepository userRepository) {
+        this.mapper = dozerMapper;
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
 }
--- app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
@@ -1,21 +1,47 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.domain.ProductRepository;
+import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.domain.UserRepository;
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 @DataJpaTest
 @DisplayName("UserService의")
 class UserServiceTest {
+    private final Long givenSavedId = 1L;
+    private final Long givenUnsavedId = 100L;
+    private final String givenName = "newoo";
+    private final String givenEmail = "newoo@codesoom.com";
+    private final String givenPassword = "codesoom123";
+
     private UserService userService;
+    private UserRepository userRepository = mock(UserRepository.class);
+    private User user;
 
     @BeforeEach
     void setUp() {
-        userService = new UserService();
+        Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+
+        userService = new UserService(mapper, userRepository);
+
+        user = User.builder()
+                .id(givenSavedId)
+                .name(givenName)
+                .email(givenEmail)
+                .password(givenPassword)
+                .build();
     }
 
     @Nested
@@ -24,6 +50,11 @@ class UserServiceTest {
         @Nested
         @DisplayName("저장된 user가 없다면")
         class Context_without_any_saved_user {
+            @BeforeEach
+            void setEmptyList() {
+                given(userRepository.findAll()).willReturn(List.of());
+            }
+
             @Test
             @DisplayName("비어있는 리스트를 리턴한다.")
             void it_return_empty_list() {
@@ -34,6 +65,15 @@ class UserServiceTest {
         @Nested
         @DisplayName("저장된 user가 있다면")
         class Context_with_any_saved_user {
+            private List<User> givenUserList;
+
+            @BeforeEach
+            void setSavedUser() {
+                givenUserList = List.of(user);
+
+                given(userRepository.findAll()).willReturn(givenUserList);
+            }
+
             @Test
             @DisplayName("user 리스트를 리턴한다.")
             void it_return_user_list() {
