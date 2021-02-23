4b46c7c5f13b1e34bd250d52184f0d17ebb97f06
etff <wish2rich@gmail.com>
2021-02-23 20:56:02 +0900

    test: add getUser

--- app/src/test/java/com/codesoom/assignment/user/controllers/UserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/user/controllers/UserControllerTest.java
@@ -1,6 +1,6 @@
 package com.codesoom.assignment.user.controllers;
 
-import com.codesoom.assignment.product.domain.Product;
+import com.codesoom.assignment.user.application.UserNotFoundException;
 import com.codesoom.assignment.user.application.UserService;
 import com.codesoom.assignment.user.domain.User;
 import com.codesoom.assignment.user.dto.UserResponseDto;
@@ -11,11 +11,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
@@ -31,6 +32,8 @@ class UserControllerTest {
     private static final String USER2_NAME = "user2";
     private static final String USER2_PASSWORD = "pass2";
     private static final String USER2_EMAIL = "user2@test.com";
+
+    private static final Long NOT_EXIST_ID = -1L;
 
     private User user1;
     private User user2;
@@ -48,18 +51,42 @@ class UserControllerTest {
     void setUp() {
         userController = new UserController(userService);
         setUpFixtures();
-
-        given(userService.getUsers()).willReturn(usersResponses);
     }
 
     @Test
     @DisplayName("전체 사용자를 조회하고 사용자 목록을 확인한다.")
     void getUsers() {
+        given(userService.getUsers()).willReturn(usersResponses);
+
         List<UserResponseDto> users = userController.getUsers();
         assertThat(users).isNotEmpty();
         assertThat(users).containsExactly(userResponseDto1, userResponseDto2);
 
         verify(userService).getUsers();
+    }
+
+    @Test
+    @DisplayName("특정 사용자를 조회하고 사용자 정보를 확인한다.")
+    void getUser() {
+        given(userService.getUser(anyLong())).willReturn(userResponseDto1);
+        UserResponseDto user = userController.getUser(USER1_ID);
+
+        assertThat(user.getId()).isEqualTo(USER1_ID);
+        assertThat(user.getEmail()).isEqualTo(USER1_EMAIL);
+        assertThat(user.getPassword()).isEqualTo(USER1_PASSWORD);
+        assertThat(user.getName()).isEqualTo(USER1_NAME);
+
+        verify(userService).getUser(anyLong());
+    }
+
+    @Test
+    @DisplayName("특정 사용자를 조회하고 존재하지 않으면 예외를 던진다.")
+    void getNotExistedUser() {
+        given(userService.getUser(NOT_EXIST_ID))
+                .willThrow(new UserNotFoundException(NOT_EXIST_ID));
+
+        assertThatThrownBy(() -> userService.getUser(NOT_EXIST_ID))
+                .isInstanceOf(UserNotFoundException.class);
     }
 
     void setUpFixtures() {
