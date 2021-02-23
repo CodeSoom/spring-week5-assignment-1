5af4a0d249590f18a9e71d572d6668c1e18ec854
JamieShin0201 <jamieshin1450@gmail.com>
2021-02-23 11:56:34 +0900

    Add JavaDoc in UserService

--- app/src/main/java/com/codesoom/assignment/user/service/UserService.java
+++ app/src/main/java/com/codesoom/assignment/user/service/UserService.java
@@ -13,6 +13,9 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * 회원과 관련된 비즈니스 로직을 담당합니다.
+ */
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -21,6 +24,9 @@ public class UserService {
 
     private final Mapper mapper;
 
+    /**
+     * 모든 회원을 리턴합니다.
+     */
     public List<UserResponse> getUsers() {
         List<User> users = userRepository.findAll();
 
@@ -29,26 +35,53 @@ public class UserService {
                 .collect(Collectors.toList());
     }
 
-    public UserResponse getUser(Long id) {
+    /**
+     * 주어진 id에 해당하는 회원을 찾아 리턴합니다.
+     *
+     * @param id 찾고자 하는 회원의 식별자
+     * @return 찾은 회원
+     */
+    public UserResponse getUser(Long id) throws UserNotFoundException {
         User foundUser = findUser(id);
 
         return mapper.map(foundUser, UserResponse.class);
     }
 
+    /**
+     * 주어진 회원을 저장한 뒤, 저장된 회원을 리턴합니다.
+     *
+     * @param createRequest 저장하고자 하는 회원
+     * @return 저장된 회원
+     */
     public UserResponse createUser(UserCreateRequest createRequest) {
         User savedUser = userRepository.save(mapper.map(createRequest, User.class));
 
         return mapper.map(savedUser, UserResponse.class);
     }
 
-    public UserResponse updateUser(Long id, UserUpdateRequest updateRequest) {
+    /**
+     * 주어진 id에 해당하는 회원을 찾아 수정하고, 수정된 회원을 리턴합니다.
+     *
+     * @param id 수정하고자 하는 회원의 식별자
+     * @param updateRequest 수정하고자 하는 회원
+     * @return 수정된 회원
+     * @throws UserNotFoundException 주어진 식별자에 해당하는 회원을 찾지 못했을 경우
+     */
+    public UserResponse updateUser(Long id, UserUpdateRequest updateRequest)
+            throws UserNotFoundException {
         User user = findUser(id);
         User updatedUser = user.changeWith(mapper.map(updateRequest, User.class));
 
         return mapper.map(updatedUser, UserResponse.class);
     }
 
-    public void deleteUser(Long id) {
+    /**
+     * 주어진 id에 해당하는 회원을 찾아 삭제합니다.
+     *
+     * @param id 삭제하고자 하는 회원의 식별자
+     * @throws UserNotFoundException 주어진 식별자에 해당하는 회원을 찾지 못했을 경우
+     */
+    public void deleteUser(Long id) throws UserNotFoundException {
         User user = findUser(id);
 
         userRepository.delete(user);
