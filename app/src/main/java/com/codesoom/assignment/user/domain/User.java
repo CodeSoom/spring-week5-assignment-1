38b7c1dfeaeafb65fa7f0d1dfb1718de037cb526
developerOlive <chloehiho@gmail.com>
2021-02-24 02:35:01 +0900

    Feat : 사용자 정보를 담고 있는 User 클래스 생성

---
+++ app/src/main/java/com/codesoom/assignment/user/domain/User.java
@@ -0,0 +1,35 @@
+package com.codesoom.assignment.user.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * 사용자 정보.
+ */
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    @Builder
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
