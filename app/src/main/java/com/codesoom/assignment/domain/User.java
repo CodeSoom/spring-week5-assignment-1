bd6a2d8f0dc8f5abd9f1f98ec5f0b205f26e3191
Lee Dong Wook <sh95119@gmail.com>
2021-02-23 21:26:31 +0900

    FEATURE: add User domain

---
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -0,0 +1,19 @@
+package com.codesoom.assignment.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    private String sex;
+}
