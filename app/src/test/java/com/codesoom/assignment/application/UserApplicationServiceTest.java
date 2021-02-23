8330a68744c9805a015797ff8756e9b352fd4bee
Las <haho013039@gmail.com>
2021-02-23 23:35:33 +0900

    Write test for change user name

--- app/src/test/java/com/codesoom/assignment/application/UserApplicationServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/UserApplicationServiceTest.java
@@ -16,7 +16,11 @@ public class UserApplicationServiceTest {
     String name;
     String mail;
     String password;
+
     User createdUser;
+    User changedUser;
+
+    String newName;
 
     @Given("이름, 이메일, 비밀번호가 올바르게 주어진다면")
     public void provideRightInformation() {
@@ -33,5 +37,24 @@ public class UserApplicationServiceTest {
     @Then("정상적으로 회원이 생성된다")
     public void userCreatedNormally() {
         assertThat(createdUser).isNotNull();
+    }
+
+    @Given("이미 회원이 생성되었다면")
+    public void alreadyCreateUser() {
+        createdUser = userApplicationService.createUser(name, mail, password);
+    }
+
+    @When("회원의 이름을 변경하는 경우")
+    public void changeName() {
+        String newName = "Las";
+        changedUser = userApplicationService.changeName(newName);
+    }
+
+    @Then("회원의 이름이 변경된다")
+    public void changeUserName() {
+        assertThat(changedUser.getId()).isEqualTo(createdUser.getId());
+        assertThat(changedUser.getName()).isEqualTo(newName);
+        assertThat(changedUser.getMail()).isEqualTo(mail);
+        assertThat(changedUser.getPassword()).isEqualTo(password);
     }
 }
