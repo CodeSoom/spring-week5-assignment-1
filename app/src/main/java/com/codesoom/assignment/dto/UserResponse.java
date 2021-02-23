d4f38b5b884b4639ea5b52759b44ddbf1ae52140
Lee Dong Wook <dwlee@crscube.io>
2021-02-24 07:46:46 +0900

    FIX: apply lombok to UserRequest, UserResponse

--- app/src/main/java/com/codesoom/assignment/dto/UserResponse.java
+++ app/src/main/java/com/codesoom/assignment/dto/UserResponse.java
@@ -3,8 +3,18 @@ package com.codesoom.assignment.dto;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-public class UserResponse {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
     private Long id;
 
     @NotBlank
@@ -15,46 +25,4 @@ public class UserResponse {
 
     @NotBlank
     private String sex;
-
-    public UserResponse() {
-    }
-
-    public UserResponse(Long id, String name, Integer age, String sex) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.sex = sex;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
 }
