2f6128a9c9918e57c57a7f93cd92d905f03ac3c1
developerOlive <chloehiho@gmail.com>
2021-02-24 03:02:44 +0900

    Feat : JpaUserRepository 인터페이스 생성

---
+++ app/src/main/java/com/codesoom/assignment/user/infra/JpaUserRepository.java
@@ -0,0 +1,19 @@
+package com.codesoom.assignment.user.infra;
+
+import com.codesoom.assignment.user.domain.User;
+import com.codesoom.assignment.user.domain.UserRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface JpaUserRepository extends UserRepository, CrudRepository<User, Long> {
+    List<User> findAll();
+
+    Optional<User> findById(Long id);
+
+    User save(User user);
+
+    void delete(User user);
+
+}
