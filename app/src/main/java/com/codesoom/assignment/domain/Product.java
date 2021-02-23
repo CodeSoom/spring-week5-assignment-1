8058f9b412374fa10543dc88f7f69f981b06d137
양효주 <yhyojoo@icloud.com>
2021-02-24 02:13:54 +0900

    Remove @AllArgsConstructor in Product

--- app/src/main/java/com/codesoom/assignment/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -1,41 +1,45 @@
 package com.codesoom.assignment.domain;
 
-import lombok.*;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 // 1. Entity (domain)
 //  ** RDB의 Entity와 다름
 // 2. JPA의 Entity 역할도 같이 함
-@Getter @Setter
-@AllArgsConstructor
+@Getter
+@Setter
 @NoArgsConstructor
-@Builder
 @Entity
 public class Product {
     @Id
     @GeneratedValue
     private Long id;
 
-    @NotBlank
     private String name;
 
-    @NotBlank
     private String maker;
 
-    @NotNull
     private Integer price;
 
     private String image;
 
-    public void change(Product source) {
-        this.id = source.getId();
-        this.name = source.getName();
-        this.maker = source.getMaker();
-        this.price = source.getPrice();
+    @Builder
+    public Product(String name, String maker, Integer price) {
+        this.name = name;
+        this.maker = maker;
+        this.price = price;
+    }
+
+    public void change(String name, String maker, Integer price) {
+        this.name = getName();
+        this.maker = getMaker();
+        this.price = getPrice();
     }
 }
