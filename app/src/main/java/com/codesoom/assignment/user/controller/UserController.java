ac850c59d78a3330bf51e2d7113137568c75b0a6
JamieShin0201 <jamieshin1450@gmail.com>
2021-02-23 11:51:10 +0900

    Add JavaDoc in UserController

--- app/src/main/java/com/codesoom/assignment/user/controller/UserController.java
+++ app/src/main/java/com/codesoom/assignment/user/controller/UserController.java
@@ -20,6 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.List;
 
+/**
+ * 회원과 관련된 HTTP 요청 처리를 담당합니다.
+ */
 @RestController
 @CrossOrigin
 @RequestMapping("/users")
@@ -28,22 +31,44 @@ public class UserController {
 
     private final UserService userService;
 
+    /**
+     * 모든 회원을 응답합니다.
+     */
     @GetMapping
     public List<UserResponse> list() {
         return userService.getUsers();
     }
 
+    /**
+     * 주어진 id에 해당하는 회원을 찾아 응답합니다.
+     *
+     * @param id 찾고자 하는 회원의 식별자
+     * @return 찾은 회원
+     */
     @GetMapping("{id}")
     public UserResponse find(@PathVariable Long id) {
         return userService.getUser(id);
     }
 
+    /**
+     * 주어진 회원을 저장한 뒤, 저장된 회원을 응답합니다.
+     *
+     * @param createRequest 저장하고자 하는 회원
+     * @return 저장된 회원
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public UserResponse create(@RequestBody @Valid UserCreateRequest createRequest) {
         return userService.createUser(createRequest);
     }
 
+    /**
+     * 주어진 id에 해당하는 회원을 찾아 수정하고, 수정된 회원을 응답합니다.
+     *
+     * @param id 수정하고자 하는 회원의 식별자
+     * @param updateRequest 수정하고자 하는 회원
+     * @return 수정된 회원
+     */
     @PatchMapping("{id}")
     public UserResponse update(
             @PathVariable Long id,
@@ -51,6 +76,11 @@ public class UserController {
         return userService.updateUser(id, updateRequest);
     }
 
+    /**
+     * 주어진 id에 해당하는 회원을 찾아 삭제합니다.
+     *
+     * @param id 삭제하고자 하는 회원의 식별자
+     */
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
