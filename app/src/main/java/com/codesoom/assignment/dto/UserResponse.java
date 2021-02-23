1f64b112023033e552c6e3ffdf7ab10b1847817d
Lee Dong Wook <sh95119@gmail.com>
2021-02-23 21:27:01 +0900

    FEATURE: add UserRequest, UserResponse

---
+++ app/src/main/java/com/codesoom/assignment/dto/UserResponse.java
@@ -0,0 +1,60 @@
+package com.codesoom.assignment.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class UserResponse {
+
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    private Integer age;
+
+    @NotBlank
+    private String sex;
+
+    public UserResponse() {
+    }
+
+    public UserResponse(Long id, String name, Integer age, String sex) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
