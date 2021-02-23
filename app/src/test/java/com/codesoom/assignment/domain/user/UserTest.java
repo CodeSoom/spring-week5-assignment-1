c37dcc950a96d000ea33f8a15ac9a7ef6bc0f143
etff <wish2rich@gmail.com>
2021-02-23 10:31:38 +0900

    test: add user getter test

---
+++ app/src/test/java/com/codesoom/assignment/domain/user/UserTest.java
@@ -0,0 +1,39 @@
+package com.codesoom.assignment.domain.user;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserTest {
+
+    private static final String NAME = "test";
+    private static final String EMAIL = "test@test.com";
+    private static final String PASSWORD = "test";
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = User.of(NAME, EMAIL, PASSWORD);
+    }
+
+    @DisplayName("getName은 이름을 리턴한다")
+    @Test
+    void getName() {
+        assertThat(user.getName()).isEqualTo(NAME);
+    }
+
+    @DisplayName("getEmail은 이메일을 리턴한다")
+    @Test
+    void getMaker() {
+        assertThat(user.getEmail()).isEqualTo(EMAIL);
+    }
+
+    @DisplayName("getPassword는 비밀번호를 리턴한다")
+    @Test
+    void getPrice() {
+        assertThat(user.getPassword()).isEqualTo(PASSWORD);
+    }
+}
