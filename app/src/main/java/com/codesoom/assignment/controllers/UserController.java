c7b0d636dd920855dcfaf009e05bab6151bddb14
JamieShin0201 <jamieshin1450@gmail.com>
2021-02-23 10:15:56 +0900

    [Green] Implement GET /users

--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -1,9 +1,14 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.UserService;
+import com.codesoom.assignment.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -11,5 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> list() {
+        return userService.getUsers();
+    }
 
 }
