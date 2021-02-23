734028da740d63f0778bfe57524f5d1acd89b7b8
Gyu <melon9751@naver.com>
2021-02-23 20:34:06 +0900

    Add given, content at POST /users controllertest

--- app/src/main/java/com/codesoom/assignment/application/UserService.java
+++ app/src/main/java/com/codesoom/assignment/application/UserService.java
@@ -1,4 +1,12 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.domain.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    public User createUser(User source) {
+        return null;
+    }
 }
--- app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.UserService;
 import com.codesoom.assignment.domain.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -7,9 +8,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.any;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,6 +26,9 @@ class UserControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @MockBean
+    UserService userService;
+
     @Nested
     @DisplayName("create 메서드는")
     class Describe_create {
@@ -28,15 +36,18 @@ class UserControllerTest {
         @DisplayName("만약 유저 객체가 주어진다면")
         class Context_WithUser {
             @Test
-            @DisplayName("객체를 저장하고 저장된 객체와 Created를 리턴한다")
+            @DisplayName("객체를 저장하고 저장된 객체와 CREATED를 리턴한다")
             void itSavesUserAndReturnsUser() throws Exception {
                 User user = new User(1L, "paik", "melon", "1234");
+                //given(userService.createUser(any(User.class))).willReturn(user);
 
-                mockMvc.perform(post("/users"))
-                        .andExpect(jsonPath("id").value(1L))
-                        .andExpect(jsonPath("name").value("paik"))
-                        .andExpect(jsonPath("mail").value("melon"))
-                        .andExpect(jsonPath("password").value("1234"))
+                mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"id\":1L,\"name\":\"paik\",\"email\":\"melon\",\"password\":\"1234\"}"))
+                        //.andExpect(jsonPath("id").value(1L))
+                        //.andExpect(jsonPath("name").value("paik"))
+                        //.andExpect(jsonPath("mail").value("melon"))
+                        //.andExpect(jsonPath("password").value("1234"))
                         .andExpect(status().isOk());
             }
         }
