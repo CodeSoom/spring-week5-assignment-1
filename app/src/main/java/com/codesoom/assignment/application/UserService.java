e6094e6e0e6ec87ef7433360cd697cf672dbeb29
Lee Dong Wook <dwlee@crscube.io>
2021-02-24 07:42:34 +0900

    FEATURE: add createUser(), updateUser(), deleteUser() methods

---
+++ app/src/main/java/com/codesoom/assignment/application/UserService.java
@@ -0,0 +1,22 @@
+package com.codesoom.assignment.application;
+
+import org.springframework.stereotype.Service;
+
+import com.codesoom.assignment.dto.UserRequest;
+import com.codesoom.assignment.dto.UserResponse;
+
+@Service
+public class UserService {
+
+    public UserResponse createUser(UserRequest userRequest) {
+        return null;
+    }
+
+    public UserResponse updateUser(Long id, UserRequest userRequest) {
+        return null;
+    }
+
+    public UserResponse deleteUser(Long id) {
+        return null;
+    }
+}
