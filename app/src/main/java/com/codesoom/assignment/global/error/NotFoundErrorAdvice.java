068bf1e144e730ef01206b2b874536292bc24e1e
etff <wish2rich@gmail.com>
2021-02-23 12:23:40 +0900

    feature: add controller advice
    response not found when user not found

--- app/src/main/java/com/codesoom/assignment/global/error/NotFoundErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/global/error/NotFoundErrorAdvice.java
@@ -1,6 +1,7 @@
 package com.codesoom.assignment.global.error;
 
 import com.codesoom.assignment.product.application.ProductNotFoundException;
+import com.codesoom.assignment.user.application.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,7 +13,14 @@ public class NotFoundErrorAdvice {
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ProductNotFoundException.class)
-    public ErrorResponse handleProductTaskNotFound() {
+    public ErrorResponse handleProductNotFound() {
         return new ErrorResponse("Product not found");
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public ErrorResponse handleUserNotFound() {
+        return new ErrorResponse("User not found");
     }
 }
