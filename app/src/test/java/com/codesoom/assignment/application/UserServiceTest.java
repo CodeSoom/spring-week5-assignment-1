85a1e9f9df83cfe406e31ce681f49038308d1ffa
wenodev <logicalstark1105@gmail.com>
2021-02-23 20:19:56 +0900

    UserService getALlUser test-fail

--- app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
@@ -17,4 +20,13 @@ import static org.mockito.BDDMockito.given;
 5. deleteUser
  */
 class UserServiceTest {
+
+    private UserService userService;
+
+    @Test
+    void getAllUsers(){
+        List<User> users = userService.getAllUsers();
+        assertThat(users).isNotEmpty();
+    }
+
 }
