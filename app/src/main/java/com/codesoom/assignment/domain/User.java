0deedcb58067e5351d72fed33bb0ee5eb8761dfd
seungin yang <rhfpdk92@naver.com>
2021-02-23 17:17:10 +0900

    Add user domain

---
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -0,0 +1,32 @@
+package com.codesoom.assignment.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    @Builder
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
