d3d5f510478d92bb821d3ab9e5659c453a797651
Gyu <melon9751@naver.com>
2021-02-23 20:22:39 +0900

    Make fast User class

--- app/src/main/java/com/codesoom/assignment/domain/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -1,4 +1,28 @@
 package com.codesoom.assignment.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
 }
