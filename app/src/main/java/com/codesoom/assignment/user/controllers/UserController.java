4683f95a422a544b3c69c18f1d3f60b442b1fcf6
etff <wish2rich@gmail.com>
2021-02-23 13:01:25 +0900

    feature: add update request validation

--- app/src/main/java/com/codesoom/assignment/user/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/user/controllers/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -37,7 +38,7 @@ public class UserController {
 
     @PatchMapping("/{id}")
     public UserResponseDto updateUser(@PathVariable Long id,
-                                      @RequestBody UserUpdateRequestDto requestDto) {
+                                      @Valid @RequestBody UserUpdateRequestDto requestDto) {
         return userService.updateUser(id, requestDto);
     }
 }
