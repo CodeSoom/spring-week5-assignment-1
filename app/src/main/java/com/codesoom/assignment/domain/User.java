d643b040e0537e7448525117aaedbcd5a5e8c35d
wenodev <logicalstark1105@gmail.com>
2021-02-23 19:51:43 +0900

    colum으로 등록

--- app/src/main/java/com/codesoom/assignment/domain/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -3,10 +3,7 @@ package com.codesoom.assignment.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
@@ -16,8 +13,14 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String name;
+
+    @Column
     private String email;
+
+    @Column
     private String password;
 
     public User(String name, String email, String password) {
