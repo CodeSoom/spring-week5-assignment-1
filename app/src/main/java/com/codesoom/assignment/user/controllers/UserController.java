c78076d73c80e1d7a8978fb31b02e3f5dc7398ce
etff <wish2rich@gmail.com>
2021-02-23 12:03:19 +0900

    feature: add get user method controller

--- app/src/main/java/com/codesoom/assignment/user/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/user/controllers/UserController.java
@@ -1,9 +1,26 @@
 package com.codesoom.assignment.user.controllers;
 
+import com.codesoom.assignment.user.application.UserService;
+import com.codesoom.assignment.user.dto.UserResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@CrossOrigin
+@RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public List<UserResponseDto> getProducts() {
+        return userService.getUsers();
+    }
+
 }
