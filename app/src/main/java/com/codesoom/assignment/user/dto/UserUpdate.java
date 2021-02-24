7c64257a56510583621cc16f1adc39273adab035
developerOlive <chloehiho@gmail.com>
2021-02-25 06:06:58 +0900

    Refactor : UserService 테스트를 하기 위해 UserUpdate 클래스에 id, email 변수 추가 및 생성자 수정

--- app/src/main/java/com/codesoom/assignment/user/dto/UserUpdate.java
+++ app/src/main/java/com/codesoom/assignment/user/dto/UserUpdate.java
@@ -6,18 +6,25 @@ import lombok.Builder;
 import javax.validation.constraints.NotBlank;
 
 public class UserUpdate {
+    private Long id;
 
     @NotBlank
     @Mapping("name")
     private String name;
 
     @NotBlank
+    @Mapping("email")
+    private String email;
+
+    @NotBlank
     @Mapping("password")
     private String password;
 
     @Builder
-    public UserUpdate(String name, String password) {
+    public UserUpdate(Long id, @NotBlank String name, @NotBlank String email, @NotBlank String password) {
+        this.id = id;
         this.name = name;
+        this.email = email;
         this.password = password;
     }
 }
