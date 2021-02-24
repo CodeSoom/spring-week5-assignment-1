aaea3a08e6440c356ddbe635a738bd01b1c54573
etff <wish2rich@gmail.com>
2021-02-24 20:34:29 +0900

    fix: change validation

--- app/src/main/java/com/codesoom/assignment/product/dto/ProductData.java
+++ app/src/main/java/com/codesoom/assignment/product/dto/ProductData.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Getter
@@ -22,7 +22,7 @@ public class ProductData {
     @Mapping("maker")
     private String maker;
 
-    @Positive
+    @NotNull
     @Mapping("price")
     private BigDecimal price;
 
