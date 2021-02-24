7e2c031d11477fe663020221fc033c94de2481b0
Lee Dong Wook <sh95119@gmail.com>
2021-02-24 22:33:33 +0900

    FEATURE: add UserService class

--- app/src/main/java/com/codesoom/assignment/application/UserService.java
+++ app/src/main/java/com/codesoom/assignment/application/UserService.java
@@ -2,14 +2,40 @@ package com.codesoom.assignment.application;
 
 import org.springframework.stereotype.Service;
 
+import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.domain.UserRepository;
 import com.codesoom.assignment.dto.UserRequest;
 import com.codesoom.assignment.dto.UserResponse;
+import com.github.dozermapper.core.Mapper;
 
 @Service
 public class UserService {
 
+    private final Mapper mapper;
+    private final UserRepository userRepository;
+
+    public UserService(Mapper dozerMapper, UserRepository userRepository) {
+        this.mapper = dozerMapper;
+        this.userRepository = userRepository;
+    }
+
     public UserResponse createUser(UserRequest userRequest) {
-        return null;
+        User user = new User()
+            .builder()
+            .name(userRequest.getName())
+            .email(userRequest.getEmail())
+            .password(userRequest.getPassword())
+            .build();
+
+        User response = userRepository.save(user);
+
+        return new UserResponse()
+            .builder()
+            .id(response.getId())
+            .name(response.getName())
+            .email(response.getEmail())
+            .password(response.getPassword())
+            .build();
     }
 
     public UserResponse updateUser(Long id, UserRequest userRequest) {
