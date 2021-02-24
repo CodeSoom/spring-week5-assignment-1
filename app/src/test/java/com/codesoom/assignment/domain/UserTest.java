85626d2bec39c28bd8eed00ebf481ae00d9cdf82
thkim <thkim2017@gmail.com>
2021-02-24 18:03:30 +0900

    Implement User.class It has id, name, email and password field

--- app/src/test/java/com/codesoom/assignment/domain/UserTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/UserTest.java
@@ -9,14 +9,14 @@ class UserTest {
     void creationWithBuilder() {
         User user = User.builder()
                 .id(1L)
-                .name("홍길")
+                .name("홍길동")
                 .email("test@gmail.com")
                 .password("1234")
                 .build();
 
         assertThat(user.getId()).isEqualTo(1L);
         assertThat(user.getName()).isEqualTo("홍길동");
-        assertThat(user.getEmail()).isEquallTo("test@gmail.com");
-        assertThat(user.getPassword()).isEquallTo("1234");
+        assertThat(user.getEmail()).isEqualTo("test@gmail.com");
+        assertThat(user.getPassword()).isEqualTo("1234");
     }
 }
