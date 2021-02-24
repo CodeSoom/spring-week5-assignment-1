e988660a2728eed0d32251f8731cae2b18700bab
etff <wish2rich@gmail.com>
2021-02-24 20:32:09 +0900

    refactor: change validation

--- app/src/main/java/com/codesoom/assignment/product/dto/ProductData.java
+++ app/src/main/java/com/codesoom/assignment/product/dto/ProductData.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 
 @Getter
@@ -22,7 +22,7 @@ public class ProductData {
     @Mapping("maker")
     private String maker;
 
-    @NotNull
+    @Positive
     @Mapping("price")
     private BigDecimal price;
 
