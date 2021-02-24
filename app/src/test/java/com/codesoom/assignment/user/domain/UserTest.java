54e8d3f45932b5c20c1de450931f41f9234163b1
etff <wish2rich@gmail.com>
2021-02-24 10:50:05 +0900

    refactor: remove lombok getter test

--- app/src/test/java/com/codesoom/assignment/user/domain/UserTest.java
+++ app/src/test/java/com/codesoom/assignment/user/domain/UserTest.java
@@ -1,6 +1,5 @@
 package com.codesoom.assignment.user.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,45 +15,31 @@ class UserTest {
     private static final String UPDATE_EMAIL = "new@test.com";
     private static final String UPDATE_PASSWORD = "new_test";
 
-    private User user;
-
-    @BeforeEach
-    void setUp() {
-        user = User.builder()
+    @DisplayName("사용자를 생성하고 설정한 값을 리턴한다.")
+    @Test
+    void create() {
+        User user = User.builder()
                 .id(ID)
                 .name(NAME)
                 .email(EMAIL)
                 .password(PASSWORD)
                 .build();
-    }
 
-    @DisplayName("getId은 식별자를 리턴한다")
-    @Test
-    void getId() {
         assertThat(user.getId()).isEqualTo(ID);
-    }
-
-    @DisplayName("getName은 이름을 리턴한다")
-    @Test
-    void getName() {
         assertThat(user.getName()).isEqualTo(NAME);
-    }
-
-    @DisplayName("getEmail은 이메일을 리턴한다")
-    @Test
-    void getMaker() {
         assertThat(user.getEmail()).isEqualTo(EMAIL);
-    }
-
-    @DisplayName("getPassword는 비밀번호를 리턴한다")
-    @Test
-    void getPrice() {
         assertThat(user.getPassword()).isEqualTo(PASSWORD);
     }
 
     @DisplayName("사용자의 정보를 갱신한다.")
     @Test
     void change() {
+        User user = User.builder()
+                .id(ID)
+                .name(NAME)
+                .email(EMAIL)
+                .password(PASSWORD)
+                .build();
         User source = User.builder()
                 .name(UPDATE_NAME)
                 .email(UPDATE_EMAIL)
