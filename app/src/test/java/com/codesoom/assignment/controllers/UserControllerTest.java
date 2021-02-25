dcb89eee4bd88028d7f82bd68d3863f6bb3cac41
thkim <thkim2017@gmail.com>
2021-02-25 13:17:15 +0900

    Create a test for UserController Test for create() in UserController

---
+++ app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
@@ -0,0 +1,24 @@
+package com.codesoom.assignment.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserController userController;
+
+    @Test
+    void create() throws Exception {
+        mockMvc.perform(post("/users"))
+                .andExpect(status().isCreated());
+    }
+}
