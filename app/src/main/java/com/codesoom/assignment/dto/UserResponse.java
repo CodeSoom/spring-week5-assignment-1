bd1c2be54738ca0af29b2d65290ad4e5701639d1
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-02-25 18:15:21 +0900

    feat: Add id to 'UserResponse'

--- app/src/main/java/com/codesoom/assignment/dto/UserResponse.java
+++ app/src/main/java/com/codesoom/assignment/dto/UserResponse.java
@@ -1,24 +1,23 @@
 package com.codesoom.assignment.dto;
 
 import com.codesoom.assignment.domain.User;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@Builder
 @NoArgsConstructor
 public class UserResponse {
+    private Long id;
     private String name;
     private String email;
 
-    @Builder
-    public UserResponse(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
     public static UserResponse of(User user) {
         return UserResponse.builder()
+                .id(user.getId())
                 .name(user.getName())
                 .email(user.getEmail())
                 .build();
