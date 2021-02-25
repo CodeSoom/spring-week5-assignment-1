4f57d419b91e94a31813296d3d69eaca40971a1a
thkim <thkim2017@gmail.com>
2021-02-25 14:09:18 +0900

    Add a test for Applying UserService in UserController Apply MockBean and given occation with createUser() in UserService

--- app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
@@ -1,12 +1,16 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,8 +23,14 @@ class UserControllerTest {
     @Autowired
     private UserController userController;
 
+    @MockBean
+    private UserService userService;
+
     @Test
     void create() throws Exception {
+        User user = new User(1L,"홍길동", "test@gmail.com", "1234");
+        given(userService.createUser(any(User.class))).willReturn(user);
+
         mockMvc.perform(post("/users")
                 .accept(MediaType.APPLICATION_PROBLEM_JSON_UTF8)
         )
