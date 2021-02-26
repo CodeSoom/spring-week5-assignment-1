3afb88a2461bdeeb7e86e98e155c7a52af4d86b1
양효주 <yhyojoo@icloud.com>
2021-02-27 01:55:02 +0900

    dozerMapper를 사용하여 createProduct() 단순화

--- app/src/main/java/com/codesoom/assignment/application/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -4,6 +4,8 @@ import com.codesoom.assignment.ProductNotFoundException;
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.domain.ProductRepository;
 import com.codesoom.assignment.dto.ProductData;
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -12,9 +14,14 @@ import java.util.List;
 @Service
 @Transactional
 public class ProductService {
+    private final Mapper mapper;
     private final ProductRepository productRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(
+            Mapper dozerMapper,
+            ProductRepository productRepository
+    ) {
+        this.mapper = dozerMapper;
         this.productRepository = productRepository;
     }
 
@@ -23,21 +30,18 @@ public class ProductService {
     }
 
     public Product getProduct(Long id) {
-        return productRepository.findById(id)
-                .orElseThrow(() -> new ProductNotFoundException(id));
+        return findProduct(id);
     }
 
     public Product createProduct(ProductData productData) {
-        Product product = Product.builder()
-                .name(productData.getName())
-                .maker(productData.getMaker())
-                .price(productData.getPrice())
-                .build();
+        Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+        Product product = mapper.map(productData, Product.class);
+
         return productRepository.save(product);
     }
 
     public Product updateProduct(Long id, ProductData productData) {
-        Product product = getProduct(id);
+        Product product = findProduct(id);
 
         product.update(
                 productData.getName(),
@@ -50,10 +54,15 @@ public class ProductService {
     }
 
     public Product deleteProduct(Long id) {
-        Product product = getProduct(id);
+        Product product = findProduct(id);
 
         productRepository.delete(product);
 
         return product;
+    }
+
+    private Product findProduct(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException(id));
     }
 }
