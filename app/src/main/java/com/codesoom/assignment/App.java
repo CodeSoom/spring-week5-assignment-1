31b4d958a73e90ee028ac0240f5cb5437e12f9fc
양효주 <yhyojoo@icloud.com>
2021-02-27 02:05:51 +0900

    DozerMapper Bean 추가

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,12 +1,16 @@
 package com.codesoom.assignment;
 
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
-    public String getGreeting() {
-        return "Hello, world!";
+    @Bean
+    public Mapper dozerMapper() {
+        return DozerBeanMapperBuilder.buildDefault();
     }
 
     public static void main(String[] args) {
