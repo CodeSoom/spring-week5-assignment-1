7910845242b70e4c31dbfe180a52425acd427bc1
양효주 <yhyojoo@icloud.com>
2021-02-27 03:38:48 +0900

    ProductTest에 setUp() 추가

--- app/src/test/java/com/codesoom/assignment/domain/ProductTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/ProductTest.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,23 +8,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ProductTest {
     private static final String NAME = "뱀 장난감";
-
     private static final String MAKER = "야옹이네 장난감";
-
     private static final Integer PRICE = 3000;
-
     private static final String IMAGE = "https://bit.ly/3qzXRME";
 
-    @Test
-    @DisplayName("상품이 정상적으로 등록되었는지 확인합니다")
-    void creationWithBuilder() {
-        Product product = Product.builder()
+    private Product product;
+
+    @BeforeEach
+    void setUp() {
+        product = Product.builder()
                 .name(NAME)
                 .maker(MAKER)
                 .price(PRICE)
                 .image(IMAGE)
                 .build();
+    }
 
+    @Test
+    @DisplayName("상품이 정상적으로 등록되었는지 확인합니다")
+    void creationWithBuilder() {
         assertThat(product.getName()).isEqualTo(NAME);
         assertThat(product.getMaker()).isEqualTo(MAKER);
         assertThat(product.getPrice()).isEqualTo(PRICE);
@@ -33,13 +36,6 @@ class ProductTest {
     @Test
     @DisplayName("상품의 정보가 정상적으로 변경되었는지 확인합니다")
     void updateWith() {
-        Product product = Product.builder()
-                .name(NAME)
-                .maker(MAKER)
-                .price(PRICE)
-                .image(IMAGE)
-                .build();
-
         product.updateWith(Product.builder()
                 .name("물고기 장난감")
                 .maker("애옹이네 장난감")
