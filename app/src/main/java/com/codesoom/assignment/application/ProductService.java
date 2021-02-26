166345457c6f15b9362826b96d74e1c53e0e6794
newoo <newoo4297@naver.com>
2021-02-26 20:09:47 +0900

    ProductService의 메서드들에 JavaDoc추가

--- app/src/main/java/com/codesoom/assignment/application/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -24,19 +24,43 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    /**
+     * 저장된 장난감 리스트를 반환한다.
+     *
+     * @return List<Product>, 저장된 장난감 리스트
+     */
     public List<Product> getProducts() {
         return productRepository.findAll();
     }
 
+    /**
+     * 저장된 장난감을 반환한다.
+     *
+     * @param id 저장된 장난감의 id
+     * @return Product, 저장된 장난감
+     */
     public Product getProduct(Long id) {
         return findProduct(id);
     }
 
+    /**
+     * 장난감을 저장하고, 저장한 장난감을 반환한다.
+     *
+     * @param productData 저장할 장난감의 데이터
+     * @return Product, 저장한 장난감
+     */
     public Product createProduct(ProductData productData) {
         Product product = mapper.map(productData, Product.class);
         return productRepository.save(product);
     }
 
+    /**
+     * 저장된 장난감을 수정하고, 수정한 장난감을 반환한다.
+     *
+     * @param id 저장된 장난감의 id
+     * @param productData 수정할 장난감의 데이터
+     * @return Product, 수정한 장난감
+     */
     public Product updateProduct(Long id, ProductData productData) {
         Product product = findProduct(id);
 
@@ -45,6 +69,12 @@ public class ProductService {
         return product;
     }
 
+    /**
+     * 저장된 장난감을 삭제하고, 삭제한 장난감을 반환한다.
+     *
+     * @param id 저장된 장난감의 id
+     * @return Product, 삭제된 장난감
+     */
     public Product deleteProduct(Long id) {
         Product product = findProduct(id);
 
@@ -53,6 +83,12 @@ public class ProductService {
         return product;
     }
 
+    /**
+     * 장난감을 id 기준으로 찾고, 찾은 장난감을 반환한다.
+     *
+     * @param id 장난감의 id
+     * @return Product, 찾은 장난감
+     */
     private Product findProduct(Long id) {
         return productRepository.findById(id)
                 .orElseThrow(() -> new ProductNotFoundException(id));
