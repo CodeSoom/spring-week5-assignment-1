2642d9aefb837d0bb25c12f08e220257f933bd11
mikekang47 <gyeong0308@gmail.com>
2021-02-28 00:33:20 +0900

    Refactored about app.test

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -6,6 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class AppTest {
+    @Test
+    void Greeting() {
 
+    }
 
 }
