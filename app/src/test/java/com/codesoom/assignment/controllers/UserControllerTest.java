d35c3e5106ac8fcea263ee8465d6c42cc173e355
juunini <juuni.ni.i@gmail.com>
2021-02-27 19:47:47 +0900

    Add patch user success test [Red]

--- app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -102,12 +103,25 @@ class UserControllerTest {
     @DisplayName("[PATCH] /users/{id} 요청은")
     class Describe_modify {
         @Nested
-        @DisplayName("주어진 id의 유저가 존재할 때")
+        @DisplayName("주어진 id에 해당하는 유저가 존재할 때")
         class Context_when_exists_given_id_user {
-            @Test
-            @DisplayName("유저 정보가 변경되었음을 status ok 로 응답한다.")
-            void It_respond_modify_user_info() {
+            private final long givenID = 1;
+            private final String givenChangedName = "juuni Ni";
 
+            @Test
+            @DisplayName("변경된 유저 정보와 함께 status ok 를 응답한다.")
+            void It_respond_modify_user_info() throws Exception {
+                mockMvc.perform(
+                        patch("/users/{id}", givenID)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(userDataJSON(
+                                        givenEmail,
+                                        givenChangedName,
+                                        givenPassword
+                                ))
+                )
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(containsString(givenChangedName)));
             }
         }
 
