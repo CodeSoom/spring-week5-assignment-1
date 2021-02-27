4f34feccca64a6e46f22b3962eb691ebaef27279
juunini <juuni.ni.i@gmail.com>
2021-02-27 19:19:45 +0900

    Add create test checks contents
    and add test description

--- app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
@@ -13,8 +13,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
@@ -51,14 +53,15 @@ class UserControllerTest {
             }
 
             @Test
-            @DisplayName("생성된 유저 정보를 응답한다.")
+            @DisplayName("생성된 유저 정보와 함께 created 를 응답한다.")
             void It_respond_created_user_info() throws Exception {
                 mockMvc.perform(
                         post("/users")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(givenUserInfoJSON)
                 )
-                        .andExpect(status().isCreated());
+                        .andExpect(status().isCreated())
+                        .andExpect(content().string(containsString(givenEmail)));
             }
         }
 
@@ -80,7 +83,7 @@ class UserControllerTest {
         @DisplayName("주어진 id의 유저가 존재할 때")
         class Context_when_exists_given_id_user {
             @Test
-            @DisplayName("유저 정보가 변경되었음을 응답한다.")
+            @DisplayName("유저 정보가 변경되었음을 status ok 로 응답한다.")
             void It_respond_modify_user_info() {
 
             }
