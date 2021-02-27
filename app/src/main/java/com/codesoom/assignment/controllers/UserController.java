bcbe0338bb1e630efbc5c952f28d45d7131d8380
juunini <juuni.ni.i@gmail.com>
2021-02-27 19:22:48 +0900

    Add UserController description

--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -6,15 +6,24 @@ import com.codesoom.assignment.dto.UserData;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * 유저 정보를 관리하는 API.
+ */
 @RestController
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
-    
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
+    /**
+     * 주어진 데이터로 유저를 생성한다.
+     *
+     * @param userData 생성 할 유저의 데이터.
+     * @return 생성된 유저의 데이터.
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public UserData create(@RequestBody UserData userData) {
