3cc216a6cd4d37a5111d48a7a2add007b873b5fa
juunini <juuni.ni.i@gmail.com>
2021-02-27 20:35:32 +0900

    Add UserService create test [Green]

---
+++ app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
@@ -0,0 +1,36 @@
+package com.codesoom.assignment.application;
+
+import com.codesoom.assignment.domain.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("UserService 클래스")
+class UserServiceTest {
+    private final UserService userService = new UserService();
+    private final String givenEmail = "juuni.ni.i@gmail.com";
+    private final String givenName = "juunini";
+    private final String givenPassword = "secret";
+
+    private User makeUser(
+            String email,
+            String name,
+            String password
+    ) {
+        return new User(email, name, password);
+    }
+
+    @Nested
+    @DisplayName("create 메서드는")
+    class Describe_create {
+        @Test
+        @DisplayName("유저를 생성한 뒤 아무일도 일어나지 않는다.")
+        void It_makes_user_and_nothing() {
+            userService.create(makeUser(
+                    givenEmail,
+                    givenName,
+                    givenPassword
+            ));
+        }
+    }
+}
