1375c4d56376330ab7e7013255d4e154a1f64b26
juunini <juuni.ni.i@gmail.com>
2021-02-28 18:21:04 +0900

    Add app test

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -12,4 +12,9 @@ class AppTest {
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
         assertEquals(classUnderTest.getGreeting(), "Hello, world!");
     }
+
+    @Test
+    void mainTest() {
+        App.main(new String[]{});
+    }
 }
