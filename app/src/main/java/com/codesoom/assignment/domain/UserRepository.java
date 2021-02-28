c3e87c54897bfe04e84cfcb59f6cc53a97c4eb40
juunini <juuni.ni.i@gmail.com>
2021-02-28 10:54:45 +0900

    Make UserRepository Fake

--- app/src/main/java/com/codesoom/assignment/domain/UserRepository.java
+++ app/src/main/java/com/codesoom/assignment/domain/UserRepository.java
@@ -4,4 +4,16 @@ public interface UserRepository {
     User save(User user);
 
     void delete(User user);
+
+    public class Fake implements UserRepository {
+        @Override
+        public User save(User user) {
+            return null;
+        }
+
+        @Override
+        public void delete(User user) {
+
+        }
+    }
 }
--- app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
@@ -1,13 +1,15 @@
 package com.codesoom.assignment.application;
 
 import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.domain.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("UserService 클래스")
 class UserServiceTest {
-    private final UserService userService = new UserService();
+    private final UserRepository userRepository = new UserRepository.Fake();
+    private final UserService userService = new UserService(userRepository);
     private final String givenEmail = "juuni.ni.i@gmail.com";
     private final String givenName = "juunini";
     private final String givenPassword = "secret";
