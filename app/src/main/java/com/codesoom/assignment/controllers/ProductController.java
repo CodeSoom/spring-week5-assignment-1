b45a3b65925a91ceb0f98b03b40ba2aceec75f46
양효주 <yhyojoo@icloud.com>
2021-03-01 20:03:53 +0900

    Controller 클래스에 JavaDoc 주석 작성

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -2,42 +2,78 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.ProductService;
 import com.codesoom.assignment.domain.Product;
+import com.codesoom.assignment.dto.ProductData;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
+/**
+ * 상품 관련 요청을 처리합니다.
+ */
 @RestController
 @RequestMapping("/products")
 @CrossOrigin
+@RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
 
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
+    /**
+     * 등록된 상품 목록 조회 요청을 처리합니다.
+     *
+     * @return 등록된 상품 목록
+     */
 
     @GetMapping
     public List<Product> list() {
         return productService.getProducts();
     }
 
+    /**
+     * 상품 조회 요청을 처리하고, 해당 상품 정보를 반환합니다.
+     *
+     * @param id
+     * @return 해당 상품 정보
+     */
+
     @GetMapping("{id}")
     public Product detail(@PathVariable Long id) {
         return productService.getProduct(id);
     }
 
+    /**
+     * 상품 등록 요청을 처리하고, 등록된 상품 정보를 반환합니다.
+     *
+     * @param productData
+     * @return 등록된 상품 정보
+     */
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Product create(@RequestBody @Valid Product product) {
-        return productService.createProduct(product);
+    public Product create(@RequestBody @Valid ProductData productData) {
+        return productService.createProduct(productData);
     }
 
+    /**
+     * 상품 정보 수정 요청을 처리하고, 수정된 상품 정보를 반환합니다.
+     *
+     * @param id
+     * @param productData
+     * @return
+     */
+
     @RequestMapping(value = "{id}", method = { RequestMethod.PUT, RequestMethod.PATCH })
-    public Product update(@PathVariable Long id, @RequestBody @Valid Product product) {
-        return productService.updateProduct(id, product);
+    public Product update(@PathVariable Long id, @RequestBody @Valid ProductData productData) {
+        return productService.updateProduct(id, productData);
     }
+
+    /**
+     * 상품 삭제 요청을 처리합니다.
+     *
+     * @param id
+     */
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -3,31 +3,53 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.application.UserService;
 import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.dto.UserData;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+/**
+ * 사용자 관련 요청을 처리합니다.
+ */
 @RestController
 @RequestMapping("/users")
 @CrossOrigin
+@RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+    /**
+     * 사용자 등록 요청을 처리하고, 등록된 사용자 정보를 반환합니다.
+     *
+     * @param userData
+     * @return 등록된 사용자 정보
+     */
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public User createUser(@RequestBody @Valid UserData userData) {
-        return userService.createProduct(userData);
+        return userService.createUser(userData);
     }
+
+    /**
+     * 사용자 정보 수정 요청을 처리하고, 수정된 사용자 정보를 반환합니다.
+     *
+     * @param id
+     * @param userData
+     * @return 수정된 사용자 정보
+     */
 
     @PatchMapping("{id}")
     public User updateUser(@PathVariable Long id, @RequestBody @Valid UserData userData) {
         return userService.updateUser(id, userData);
     }
+
+    /**
+     * 사용자 삭제 요청을 처리합니다.
+     *
+     * @param id
+     */
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
