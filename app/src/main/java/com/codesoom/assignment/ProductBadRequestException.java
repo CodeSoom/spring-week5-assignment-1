474a2f3fcc7954a988921d6f365fee643de07a25
youdongkwan <dkyou7@naver.com>
2021-05-31 14:32:13 +0900

    mapper 이용 및 주석 작성

--- app/src/main/java/com/codesoom/assignment/ProductBadRequestException.java
+++ app/src/main/java/com/codesoom/assignment/ProductBadRequestException.java
@@ -4,4 +4,7 @@ public class ProductBadRequestException extends RuntimeException {
     public ProductBadRequestException(String variable, String status) {
         super(String.format("Product bad request: %s 는 %s 이어야 한다", variable, status));
     }
+    public ProductBadRequestException(String variable){
+        super(String.format("%s 값은 필수입니다", variable));
+    }
 }
