54c601c2bdeeaf6451ca95b136273810fc4ed830
Changsu Im <imcxsu@gmail.com>
2021-05-31 16:38:26 +0900

    Add javadoc for ProductNotFoundException

--- app/src/main/java/com/codesoom/assignment/ProductNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/ProductNotFoundException.java
@@ -1,5 +1,8 @@
 package com.codesoom.assignment;
 
+/**
+ * 상품을 찾을 수 없다는 예외.
+ */
 public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(Long id) {
         super("Product not found: " + id);
