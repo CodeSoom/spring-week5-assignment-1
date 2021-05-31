03b77d4139292fb62cee735215330aaaf954b4ce
Changsu Im <imcxsu@gmail.com>
2021-05-31 21:04:40 +0900

    Add description to `HelloControllerTest`

--- app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
@@ -1,11 +1,14 @@
 package com.codesoom.assignment.controllers;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("HelloController 클래스의")
 class HelloControllerTest {
     @Test
+    @DisplayName("sayHello 메서드는 인사말을 반환한다")
     void sayHello() {
         HelloController controller = new HelloController();
 
