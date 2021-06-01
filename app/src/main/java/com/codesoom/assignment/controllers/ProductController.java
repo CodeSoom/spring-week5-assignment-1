0672d36dac61857ce56be8d6c176e01b4f7a7679
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-06-01 10:18:17 +0900

    Product Controller  javadoc 작성

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -9,31 +9,58 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+/**
+ * 장난감 객체의 REST 통신을 담당하는 컨트롤러입니다.
+ */
 @RestController
 @RequestMapping("/products")
 public class ProductController {
     private final ProductService productService;
 
+    /**
+     * 서비스 객체를 주입하여 controller 객체를 생성합니다.
+     * @param productService 장난감의 서비스 객체
+     */
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
+    /**
+     * 전체 장난감 목록을 리턴합니다.
+     * @return 전체 장난감
+     */
     @GetMapping
     public List<Product> list() {
         return productService.getProducts();
     }
 
+    /**
+     * 주어진 id 값의 장난감 정보를 리턴합니다.
+     * @param id 찾고자 하는 장난감 id
+     * @return 찾은 장난감
+     */
     @GetMapping("{id}")
     public Product detail(@PathVariable Long id) {
         return productService.getProduct(id);
     }
 
+    /**
+     * 새로운 장난감을 생성합니다.
+     * @param productData 새로운 장난감의 정보를 담은 객체
+     * @return 새로운 장난감
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Product create(@RequestBody @Valid ProductData productData) {
         return productService.createProduct(productData);
     }
 
+    /**
+     * 기존 장난감의 정보를 수정합니다.
+     * @param id 수정할 장난감의 id
+     * @param productData 장난감 수정 내용
+     * @return 수정된 장난감
+     */
     @PatchMapping("{id}")
     public Product update(
             @PathVariable Long id,
@@ -42,6 +69,10 @@ public class ProductController {
         return productService.updateProduct(id, productData);
     }
 
+    /**
+     * 해당 id의 장난감을 삭제합니다.
+     * @param id 삭제할 장난감 id
+     */
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void destroy(@PathVariable Long id) {
