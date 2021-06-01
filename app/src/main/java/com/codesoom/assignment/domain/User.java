21d5b7058031c3b36fcba41831438293713a46d5
humblEgo <injun.woo30000@gmail.com>
2021-06-01 15:15:21 +0900

    User와 UserDto를 추가하라

---
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -0,0 +1,31 @@
+package com.codesoom.assignment.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    public void change(User source) {
+        this.name = source.getName();
+        this.email = source.getEmail();
+        this.password = source.getPassword();
+    }
+}
---
+++ app/src/main/java/com/codesoom/assignment/dto/UserDto.java
@@ -0,0 +1,34 @@
+package com.codesoom.assignment.dto;
+
+import com.github.dozermapper.core.Mapping;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private Long id;
+
+    @NotBlank
+    @Mapping("name")
+    private String name;
+
+    @NotBlank
+    @Mapping("email")
+    private String email;
+
+    @NotBlank
+    @Mapping("password")
+    private String password;
+}
