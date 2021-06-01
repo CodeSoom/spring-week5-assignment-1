1b98acaf82a5d5bfb4ae1d5b104b171ab2d54cc8
EHOTO <ktaewoong46@gmail.com>
2021-06-01 16:17:07 +0900

    Modified the Controller Advisor
    - Change the advisor name to "ExceptionAdvisor"
    
    - Add new exception handlers
      - MethodArgumentNotValidException
        - for when some of request parameters are invalid.
    
      - HttpMessageNotReadableException
        - for when required request body is missing.

--- app/src/main/java/com/codesoom/assignment/dto/ErrorResponse.java
+++ app/src/main/java/com/codesoom/assignment/dto/ErrorResponse.java
@@ -1,13 +1,14 @@
 package com.codesoom.assignment.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorResponse {
     private String message;
-
-    public ErrorResponse(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 }
