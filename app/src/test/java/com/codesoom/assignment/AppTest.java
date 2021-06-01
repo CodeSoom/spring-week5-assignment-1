5452831774270933a897f3d8defcfe4646c89871
humblEgo <injun.woo30000@gmail.com>
2021-06-01 16:12:52 +0900

    AppTest를 추가하라
    테스트 커버리지 100%를 위함

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -7,7 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class AppTest {
     @Test
-    void appHasAGreeting() {
-        App classUnderTest = new App();
+    void testMain() {
+        // 테스트 커버리지 100%를 위해 추가
+        App.main(new String[] {"test"});
     }
 }
