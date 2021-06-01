bafda8cf5e38ca10c43faf2ec59f53eeca40d03c
Changsu Im <imcxsu@gmail.com>
2021-06-01 15:02:11 +0900

    Add javadoc comment for error response

--- app/src/main/java/com/codesoom/assignment/dto/ErrorResponse.java
+++ app/src/main/java/com/codesoom/assignment/dto/ErrorResponse.java
@@ -1,13 +1,20 @@
 package com.codesoom.assignment.dto;
 
+import lombok.Getter;
+
+/**
+ * 응답을 위한 에러 메시지.
+ */
+@Getter
 public class ErrorResponse {
     private String message;
 
+    /**
+     * ErrorResponse 생성자.
+     *
+     * @param message 에러 메시지.
+     */
     public ErrorResponse(String message) {
         this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
     }
 }
