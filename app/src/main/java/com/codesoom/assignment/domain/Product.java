f38da9efa57e064affe9e02ba390d366c39f62a1
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-06-01 10:22:07 +0900

    import * 수정, 불필요 주석 제거

--- app/src/main/java/com/codesoom/assignment/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -8,8 +8,6 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Entity
 @Getter
