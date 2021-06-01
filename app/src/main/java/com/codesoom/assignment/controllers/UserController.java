129f68f73e6677fd29a7bf527fb1f11fd4c6cc54
humblEgo <injun.woo30000@gmail.com>
2021-06-01 15:26:57 +0900

    UserController를 구현하라

---
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -0,0 +1,48 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.application.UserService;
+import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.dto.UserDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@CrossOrigin
+@RequestMapping("/users")
+@RestController
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User create(@RequestBody @Valid UserDto source) {
+        return userService.createUser(source);
+    }
+
+    @PatchMapping("/{id}")
+    public User update(
+            @PathVariable Long id,
+            @RequestBody @Valid UserDto userDto) {
+        return userService.updateUser(id, userDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void destroy(@PathVariable Long id) {
+       userService.deleteUser(id);
+    }
+}
