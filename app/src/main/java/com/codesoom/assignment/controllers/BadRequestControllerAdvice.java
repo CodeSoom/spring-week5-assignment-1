e0dd3d7b77c81f0b48ef23a05ad7bff13e2069db
Changsu Im <imcxsu@gmail.com>
2021-06-01 19:36:49 +0900

    Add javedoc to controller advices

--- app/src/main/java/com/codesoom/assignment/controllers/BadRequestControllerAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/BadRequestControllerAdvice.java
@@ -3,14 +3,15 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+/**
+ * 400 Bad Request 상태와 관련된 예외를 처리한다.
+ */
+@RestControllerAdvice
 public class BadRequestControllerAdvice {
-    @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ErrorResponse handleInvalidProductData(MethodArgumentNotValidException ex) {
--- app/src/main/java/com/codesoom/assignment/controllers/NotFoundControllerAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/NotFoundControllerAdvice.java
@@ -4,15 +4,16 @@ import com.codesoom.assignment.ProductNotFoundException;
 import com.codesoom.assignment.UserNotFoundException;
 import com.codesoom.assignment.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
 
-@ControllerAdvice
+/**
+ * 404 Not Found 상태와 관련된 예외를 처리한다.
+ */
+@RestControllerAdvice
 public class NotFoundControllerAdvice {
-    @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler({ProductNotFoundException.class,
                        UserNotFoundException.class})
