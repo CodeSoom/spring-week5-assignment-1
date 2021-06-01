537fbe66aafc1bb0057935244ca61ffb0174cf0e
Changsu Im <imcxsu@gmail.com>
2021-06-01 12:38:50 +0900

    Update javadoc comment in exception classes

--- app/src/main/java/com/codesoom/assignment/UserNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/UserNotFoundException.java
@@ -1,7 +1,7 @@
 package com.codesoom.assignment;
 
 /**
- * 사용자를 찾을 수 없다는 예외.
+ * 사용자를 찾을 수 없는 경우에 던집니다.
  */
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Long id) {
