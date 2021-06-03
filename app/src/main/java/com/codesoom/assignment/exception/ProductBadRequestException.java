19a39d6ee238e64fbe900a765dc16ed50f7158cf
youdongkwan <dkyou7@naver.com>
2021-06-04 01:23:19 +0900

    변수명에 대한 고찰 및 피드백 반영 변경2

--- app/src/main/java/com/codesoom/assignment/exception/ProductBadRequestException.java
+++ app/src/main/java/com/codesoom/assignment/exception/ProductBadRequestException.java
@@ -1,10 +1,11 @@
 package com.codesoom.assignment.exception;
 
 public class ProductBadRequestException extends RuntimeException {
-    public ProductBadRequestException(String variable, String status) {
-        super(String.format("Product bad request: %s 는 %s 이어야 한다", variable, status));
+    public ProductBadRequestException(String value, String expectedValue) {
+        super(String.format("Product bad request: %s 는 %s 이어야 한다", value, expectedValue));
     }
-    public ProductBadRequestException(String variable){
-        super(String.format("%s 값은 필수입니다", variable));
+
+    public ProductBadRequestException(String value){
+        super(String.format("%s 값은 필수입니다", value));
     }
 }
