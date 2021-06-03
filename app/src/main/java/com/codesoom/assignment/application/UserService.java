04a1f40fc9f53e4f805e258fb79643fda575373c
Joowan Kim <rlawndhks217@gmail.com>
2021-06-03 22:42:35 +0900

    Add User CRUD

--- app/src/main/java/com/codesoom/assignment/application/UserService.java
+++ app/src/main/java/com/codesoom/assignment/application/UserService.java
@@ -1,9 +1,13 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
+import com.codesoom.assignment.dto.UserData;
+import com.codesoom.assignment.exception.UserNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -14,5 +18,47 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    // TODO: 메소드 이름 수정하고 제네릭을 이용하면 여기 메소드도 추상화가 가능할 것 같다.
 
+    public List<User> getUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public User getUser(Long id) {
+        return findUser(id);
+    }
+
+    public User createUser(UserData userData) {
+        User user = User.builder()
+                .name(userData.getName())
+                .email(userData.getEmail())
+                .password(userData.getPassword())
+                .build();
+        return this.userRepository.save(user);
+    }
+
+    public User updateUser(Long id, UserData userData) {
+        User user = findUser(id);
+
+        user.change(
+                userData.getName(),
+                userData.getEmail(),
+                userData.getPassword()
+        );
+
+        return user;
+    }
+
+    public User deleteUser(Long id) {
+        User user = findUser(id);
+
+        this.userRepository.delete(user);
+
+        return user;
+    }
+
+    private User findUser(Long id) {
+        return this.userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
 }
--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -1,8 +1,13 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.dto.UserData;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -14,4 +19,35 @@ public class UserController {
         this.userService = userService;
     }
 
+    // TODO: 추상클래스를 만들어서 Controller가 공통적으로 가지는 메소드 추상화하기
+    // 아마 제네릭도 필요할 듯
+
+    @GetMapping
+    public List<User> list() {
+        return this.userService.getUsers();
+    }
+
+    @GetMapping("{id}")
+    public User detail(@PathVariable Long id) {
+        return this.userService.getUser(id);
+    }
+
+    @PostMapping
+    public User create(@RequestBody @Valid UserData userData) {
+        return this.userService.createUser(userData);
+    }
+
+    @PatchMapping("{id}")
+    public User update(
+            @PathVariable Long id,
+            @RequestBody @Valid UserData userData
+    ) {
+        return this.userService.updateUser(id, userData);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void destroy(@PathVariable Long id) {
+        this.userService.deleteUser(id);
+    }
 }
