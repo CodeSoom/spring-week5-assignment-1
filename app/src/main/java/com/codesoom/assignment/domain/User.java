7fd4b035d6d522903982c5a3ed822b441590506e
Joowan Kim <rlawndhks217@gmail.com>
2021-06-03 10:35:10 +0900

    Add User domain skeletons

---
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -0,0 +1,35 @@
+package com.codesoom.assignment.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    public void change(String name,
+                       String email,
+                       String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
