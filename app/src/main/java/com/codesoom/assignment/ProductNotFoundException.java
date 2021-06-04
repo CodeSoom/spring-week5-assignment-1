89317dc0b93f6de9e143b49e136598e51398ffe7
humblEgo <injun.woo30000@gmail.com>
2021-06-04 20:54:06 +0900

    ProductNotFoundException의 javadoc을 추가하라

--- app/src/main/java/com/codesoom/assignment/ProductNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/ProductNotFoundException.java
@@ -1,6 +1,14 @@
 package com.codesoom.assignment;
 
+/**
+ * 상품을 찾지 못한 경우의 예외 객체
+ */
 public class ProductNotFoundException extends RuntimeException {
+    /**
+     * 찾지 못한 상품의 id를 조합한 메시지를 포함하여, 예외 객체를 생성합니다.
+     *
+     * @param id 찾지 못한 상품의 id
+     */
     public ProductNotFoundException(Long id) {
         super("Product not found: " + id);
     }
