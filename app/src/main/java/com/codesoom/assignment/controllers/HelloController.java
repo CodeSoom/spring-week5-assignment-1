6960366bc0023e2598fa2bfcc27d346d812cc031
humblEgo <injun.woo30000@gmail.com>
2021-06-04 20:48:21 +0900

    HelloController에 javadoc을 추가하라

--- app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
@@ -3,6 +3,9 @@ package com.codesoom.assignment.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * ROOT URL의 요청을 처리합니다.
+ */
 @RestController
 public class HelloController {
     @RequestMapping("/")
