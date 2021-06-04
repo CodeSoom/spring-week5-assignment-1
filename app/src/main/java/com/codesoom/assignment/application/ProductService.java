2c30afb5f6776449c06e774f09e0737ed44bb8cf
humblEgo <injun.woo30000@gmail.com>
2021-06-04 20:39:09 +0900

    ProductService에 javadoc을 추가하라

--- app/src/main/java/com/codesoom/assignment/application/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -9,23 +9,48 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
 
+/**
+ * 상품 데이터에 대한 요청을 처리합니다.
+ */
 @Service
 @Transactional
 public class ProductService {
     private final ProductRepository productRepository;
 
+    /**
+     * 상품 저장소 객체를 주입 받습니다.
+     *
+     * @param productRepository
+     */
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
+    /**
+     * 저장된 모든 상품 목록을 반환합니다.
+     *
+     * @return 저장된 모든 상품 목록
+     */
     public List<Product> getProducts() {
         return productRepository.findAll();
     }
 
+    /**
+     * 상품을 찾아서 반환합니다.
+     *
+     * @param id 찾을 상품 id
+     * @return 찾은 상품
+     */
     public Product getProduct(Long id) {
         return findProduct(id);
     }
 
+    /**
+     * 상품을 생성하고, 생성한 상품을 반환합니다.
+     *
+     * @param productData 생성할 상품 정보
+     * @return 생성한 상품
+     */
     public Product createProduct(ProductData productData) {
         Product product = Product.builder()
                 .name(productData.getName())
@@ -36,6 +61,13 @@ public class ProductService {
         return productRepository.save(product);
     }
 
+    /**
+     * 상품을 갱신하고, 갱신한 상품을 반환합니다.
+     *
+     * @param id 갱신할 상품 id
+     * @param productData 갱신할 내용
+     * @return 갱신한 상품
+     */
     public Product updateProduct(Long id, ProductData productData) {
         Product product = findProduct(id);
 
@@ -49,6 +81,12 @@ public class ProductService {
         return product;
     }
 
+    /**
+     * 상품을 제거하고, 제거한 상품을 반환합니다.
+     *
+     * @param id 제거할 상품의 id
+     * @return 제거한 상품
+     */
     public Product deleteProduct(Long id) {
         Product product = findProduct(id);
 
@@ -57,6 +95,14 @@ public class ProductService {
         return product;
     }
 
+    /**
+     * 상품을 찾아서 반환합니다.
+     *
+     * @param id 찾을 상품의 id
+     * @return 찾은 상품
+     * @throws ProductNotFoundException
+     *      상품을 찾지 못할 때 던지는 예외
+     */
     private Product findProduct(Long id) {
         return productRepository.findById(id)
                 .orElseThrow(() -> new ProductNotFoundException(id));
