f873ecf131b2516fb454b2b20833d9c30be29bd1
humblEgo <injun.woo30000@gmail.com>
2021-06-04 20:55:10 +0900

    NotFoundUserException의javadoc을 추가하라

--- app/src/main/java/com/codesoom/assignment/exception/NotFoundUserException.java
+++ app/src/main/java/com/codesoom/assignment/exception/NotFoundUserException.java
@@ -1,6 +1,14 @@
 package com.codesoom.assignment.exception;
 
+/**
+ * 유저를 찾지 못한 경우 던지는 예외 객체
+ */
 public class NotFoundUserException extends RuntimeException {
+    /**
+     * 찾지 못한 유저의 id를 조합한 메시지를 포함하여, 예외 객체를 생성합니다.
+     *
+     * @param id 찾지 못한 유저의 id
+     */
     public NotFoundUserException(Long id) {
         super("Cannot find user " + id);
     }
