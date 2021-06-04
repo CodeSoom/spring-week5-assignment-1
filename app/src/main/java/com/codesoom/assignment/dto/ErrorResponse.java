997f61c7e33cab7f868d4a7555a196423f025b75
humblEgo <injun.woo30000@gmail.com>
2021-06-04 21:00:28 +0900

    ErrorResponse에 javadoc을 추가하라

--- app/src/main/java/com/codesoom/assignment/dto/ErrorResponse.java
+++ app/src/main/java/com/codesoom/assignment/dto/ErrorResponse.java
@@ -1,12 +1,25 @@
 package com.codesoom.assignment.dto;
 
+/**
+ * HTTP 응답할 에러 내용을 저장하는 객체
+ */
 public class ErrorResponse {
     private String message;
 
+    /**
+     * 에러 내용을 저장하는 객체를 생성한다.
+     *
+     * @param message 에러 내용
+     */
     public ErrorResponse(String message) {
         this.message = message;
     }
 
+    /**
+     * 저장된 에러 내용을 반환한다.
+     *
+     * @return 저장된 에러 내용
+     */
     public String getMessage() {
         return message;
     }
