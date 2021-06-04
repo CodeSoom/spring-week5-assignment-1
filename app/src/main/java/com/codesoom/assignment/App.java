a9d03f9d4ccabbad99b62a0959b1e3f4fd4311a9
humblEgo <injun.woo30000@gmail.com>
2021-06-04 20:58:23 +0900

    App에 javadoc을 추가하라

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -6,12 +6,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * 스프링 부트 앱
+ */
 @SpringBootApplication
 public class App {
+    /**
+     * 스프링 부트 애플리케이션을 시동합니다.
+     *
+     * @param args 수프링 부트 애플리케이션에 주어진 인자
+     */
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
+    /**
+     * DozerMapper 객체를 Java Bean으로 만듭니다.
+     *
+     * @return Java Bean으로 쓸 DozerMapper 객체
+     */
     @Bean
     public Mapper dozerMapper() {
         return DozerBeanMapperBuilder.buildDefault();
