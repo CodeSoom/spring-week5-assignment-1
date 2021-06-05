9704b419b826248de19c0518612a66a65add3d57
humblEgo <injun.woo30000@gmail.com>
2021-06-05 16:50:28 +0900

    HelloController javadoc을 수정하라

--- app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
@@ -4,10 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * ROOT URL의 요청을 처리합니다.
+ * Hello API의 요청을 처리합니다.
  */
 @RestController
 public class HelloController {
+    /**
+     * 환영 인사를 리턴합니다.
+     *
+     * @return 환영 인사 문자열
+     */
     @RequestMapping("/")
     public String sayHello() {
         return "Hello, world!";
