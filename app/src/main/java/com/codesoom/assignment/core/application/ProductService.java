ebc0b9deb538da4e14f7b526fba72a0fbadf6880
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2021-06-05 22:11:11 +0900

    ProductService에 JavaDoc 추가.
    - 설명, 파라미터, 리턴값.

--- app/src/main/java/com/codesoom/assignment/core/application/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/core/application/ProductService.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
 
+/**
+ * 고양이 장난감 데이터를 가공하여 반환하거나 처리합니다.
+ */
 @Service
 @Transactional
 public class ProductService {
@@ -23,19 +26,38 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    /**
+     * 고양이 장난감 목록을 반환합니다.
+     * @return 고양이 장난감 목록
+     */
     public List<Product> getProducts() {
         return productRepository.findAll();
     }
 
+    /**
+     * ID에 해당하는 장난감을 반환합니다.
+     * @param id
+     * @return 고양이 장난감
+     */
     public Product getProduct(Long id) {
         return findProduct(id);
     }
 
+    /**
+     * 신규 고양이 장난감을 등록합니다.
+     * @param productData
+     * @return 등록한 고양이 장난감
+     */
     public Product createProduct(ProductData productData) {
         Product product = mapper.map(productData, Product.class);
         return productRepository.save(product);
     }
 
+    /**
+     * ID에 해당하는 장난감 정보를 갱신합니다.
+     * @param id
+     * @return 갱신한 고양이 장난감
+     */
     public Product updateProduct(Long id, ProductData productData) {
         Product product = findProduct(id);
 
@@ -44,6 +66,11 @@ public class ProductService {
         return product;
     }
 
+    /**
+     * ID에 해당하는 장난감을 삭제합니다.
+     * @param id
+     * @return 삭제한 고양이 장난감
+     */
     public Product deleteProduct(Long id) {
         Product product = findProduct(id);
 
@@ -52,6 +79,11 @@ public class ProductService {
         return product;
     }
 
+    /**
+     * ID에 해당하는 장난감을 반환합니다.
+     * @param id
+     * @return 고양이 장난감
+     */
     private Product findProduct(Long id) {
         return productRepository.findById(id)
                 .orElseThrow(() -> new ProductNotFoundException(id));
