7c09c5903ece4311d1059dfad9f6761cd8d743d9
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2021-06-06 02:49:53 +0900

    Make the tests.
    - ProductTest & MemberTest
      - Test for Lombok(`@Builder`, `@NoArgsConstructor`, `@AllArgsConstructor`).
      - Test for method that changeWith().

--- app/src/test/java/com/codesoom/assignment/core/domain/ProductTest.java
+++ app/src/test/java/com/codesoom/assignment/core/domain/ProductTest.java
@@ -1,33 +1,73 @@
 package com.codesoom.assignment.core.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ProductTest {
-    @Test
-    void creationWithBuilder() {
-        Product product = Product.builder()
-                .id(1L)
-                .name("쥐돌이")
-                .maker("냥이월드")
-                .price(5000)
-                .build();
+    private final Long DEFAULT_ID = 1L;
+    private final String NAME = "쥐인형";
+    private final String MAKER = "냥이월드";
+    private final Integer PRICE = 9900;
+    private final String IMAGE_URL = "http://localhost:8080/cat";
 
-        assertThat(product.getId()).isEqualTo(1L);
-        assertThat(product.getName()).isEqualTo("쥐돌이");
-        assertThat(product.getMaker()).isEqualTo("냥이월드");
-        assertThat(product.getPrice()).isEqualTo(5000);
-        assertThat(product.getImageUrl()).isNull();
+    @Nested
+    @DisplayName("Lombok 사용")
+    class contextLombokAnnotation {
+        @Test
+        @DisplayName("@Builder 테스트")
+        void creationWithBuilder() {
+            Product.ProductBuilder productBuilder = Product.builder()
+                    .id(DEFAULT_ID)
+                    .name(NAME)
+                    .maker(MAKER)
+                    .price(PRICE);
+
+            Product product = productBuilder.build();
+
+            assertThat(productBuilder.toString()).isEqualTo(String.valueOf(productBuilder));
+
+            assertThat(product.getId()).isEqualTo(DEFAULT_ID);
+            assertThat(product.getName()).isEqualTo(NAME);
+            assertThat(product.getMaker()).isEqualTo(MAKER);
+            assertThat(product.getPrice()).isEqualTo(PRICE);
+            assertThat(product.getImageUrl()).isNull();
+        }
+
+        @Test
+        @DisplayName("@AllArgsConstructor 테스트")
+        void creationWithAllArgsConstructor() {
+            Product product = new Product(DEFAULT_ID, NAME, MAKER, PRICE, IMAGE_URL);
+
+            assertThat(product.getId()).isEqualTo(DEFAULT_ID);
+            assertThat(product.getName()).isEqualTo(NAME);
+            assertThat(product.getMaker()).isEqualTo(MAKER);
+            assertThat(product.getPrice()).isEqualTo(PRICE);
+            assertThat(product.getImageUrl()).isEqualTo(IMAGE_URL);
+        }
+
+        @Test
+        @DisplayName("@NoArgsConstructor 테스트")
+        void creationWithNoArgsConstructor() {
+            Product product = new Product();
+
+            assertThat(product.getId()).isNull();
+            assertThat(product.getName()).isBlank();
+            assertThat(product.getMaker()).isBlank();
+            assertThat(product.getPrice()).isNull();
+            assertThat(product.getImageUrl()).isBlank();
+        }
     }
 
     @Test
     void changeWith() {
         Product product = Product.builder()
                 .id(1L)
-                .name("쥐돌이")
-                .maker("냥이월드")
-                .price(5000)
+                .name(NAME)
+                .maker(MAKER)
+                .price(PRICE)
                 .build();
 
         product.changeWith(Product.builder()
@@ -43,4 +83,5 @@ class ProductTest {
         assertThat(product.getImageUrl())
                 .isEqualTo("http://localhost:8080/rat");
     }
+
 }
