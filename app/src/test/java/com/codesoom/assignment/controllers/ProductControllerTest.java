c44ec2128b05f80268c846ef3a128c0231883e32
humblEgo <injun.woo30000@gmail.com>
2021-06-05 17:16:15 +0900

    테스트에서 한글이 깨지는 에러를 해결하라
    APPLICATION_JSON_VALUE로 수정했다가 한글이 깨지는
    현상이 있었습니다. 이에 UTF-8 인코딩 필터와 응답 인코딩
    설정을 추가했습니다.

--- app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
@@ -7,8 +7,10 @@ import com.codesoom.assignment.dto.ProductData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.servlet.HttpEncodingAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProductController.class)
+@Import(HttpEncodingAutoConfiguration.class)
 class ProductControllerTest {
     @Autowired
     private MockMvc mockMvc;
