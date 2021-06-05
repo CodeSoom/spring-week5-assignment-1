07dfca7b206f969898af11a69d5c105298813cd5
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2021-06-05 23:27:00 +0900

    Exception 에 대한 JavaDoc 작성.
    - `IndexOutOfBoundsException`를 참고하여 JavaDoc 작성.

--- app/src/main/java/com/codesoom/assignment/web/exception/MemberNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/web/exception/MemberNotFoundException.java
@@ -1,9 +1,13 @@
 package com.codesoom.assignment.web.exception;
 
+/**
+ * 회원 ID가 유효하지 않거나 찾을 수 없음을 나타냅니다.
+ */
 public class MemberNotFoundException extends RuntimeException {
     /**
-     * ID에 해당하는 회원을 찾을 수 없음.
-     * @param id
+     * {@code MemberNotFoundException} 생성자. 상세 메시지를 함께 출력합니다.
+     *
+     * @param id - 찾을 수 없는 회원 ID
      */
     public MemberNotFoundException(Long id) {
         super("Member not found: " + id);
--- app/src/main/java/com/codesoom/assignment/web/exception/ProductNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/web/exception/ProductNotFoundException.java
@@ -1,6 +1,14 @@
 package com.codesoom.assignment.web.exception;
 
+/**
+ * 고양이 장난감 ID가 유효하지 않거나 찾을 수 없음을 나타냅니다.
+ */
 public class ProductNotFoundException extends RuntimeException {
+    /**
+     * {@code ProductNotFoundException} 생성자. 상세 메시지를 함께 출력합니다.
+     *
+     * @param id - 찾을 수 없는 고양이 장난감 ID
+     */
     public ProductNotFoundException(Long id) {
         super("Product not found: " + id);
     }
