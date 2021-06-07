31307b7ebd3f09fdd190ac5d2e5c88b5a7827282
psjw <dlrudtn108@gmail.com>
2021-06-07 19:23:03 +0900

    UserTest Modifiy

--- app/src/test/java/com/codesoom/assignment/domain/UserTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/UserTest.java
@@ -3,8 +3,6 @@ package com.codesoom.assignment.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class UserTest {
 
     @Test
