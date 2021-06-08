9b70c8b80f4a360a1f3e676192b119a8f46833d5
EHOTO <ktaewoong46@gmail.com>
2021-06-08 16:09:22 +0900

    `ProductNotFoundException`에 대한 JavaDoc 수정.
    - 클래스 명칭과 매칭되는 주석으로 수정.

--- app/src/main/java/com/codesoom/assignment/web/exception/ProductNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/web/exception/ProductNotFoundException.java
@@ -1,13 +1,13 @@
 package com.codesoom.assignment.web.exception;
 
 /**
- * 고양이 장난감 ID가 유효하지 않거나 찾을 수 없음을 나타냅니다.
+ * 제품을 찾을 수 없는 경우 발생되는 Exception.
  */
 public class ProductNotFoundException extends RuntimeException {
     /**
      * {@code ProductNotFoundException} 생성자. 상세 메시지를 함께 출력합니다.
      *
-     * @param id - 찾을 수 없는 고양이 장난감 ID
+     * @param id - 찾을 수 없는 제품의 식별자
      */
     public ProductNotFoundException(Long id) {
         super("Product not found: " + id);
