32fff2ba2b718bf830479fcb6067b6f70078dc05
daeungkim <daeungkim93@gmail.com>
2021-09-06 06:55:04 +0900

    코드숨 5주차 강의 수강 및 반영
    코드숨 5주차 강의 수강 및 반영

--- app/src/main/java/com/codesoom/assignment/dto/ProductData.java
+++ app/src/main/java/com/codesoom/assignment/dto/ProductData.java
@@ -1,9 +1,15 @@
 package com.codesoom.assignment.dto;
 
-import lombok.*;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+
+import com.github.dozermapper.core.Mapping;
 
 @Setter
 @Getter
@@ -14,13 +20,17 @@ public class ProductData {
     private Long id;
 
     @NotBlank
+    @Mapping("name")
     private String name;
 
     @NotBlank
+    @Mapping("maker")
     private String maker;
 
     @NotNull
+    @Mapping("price")
     private Integer price;
 
+    @Mapping("imageUrl")
     private String imageUrl;
 }
