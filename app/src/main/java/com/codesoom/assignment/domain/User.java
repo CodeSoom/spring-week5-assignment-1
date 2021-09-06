8ee7e8619009bed6d031187221de03a53db13cfe
wjy5446 <wjy5446@gmail.com>
2021-09-06 22:53:06 +0900

    feat) User 클래스 추가

--- app/src/main/java/com/codesoom/assignment/domain/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -1,2 +1,35 @@
-package com.codesoom.assignment.domain;public class User {
+package com.codesoom.assignment.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String password;
+    private String email;
+
+    public void update(User user) {
+        this.name = user.getName();
+        this.password = user.getPassword();
+        this.email = user.getEmail();
+    }
+
 }
