bc61555cf287f998bc35856f7a2713068c451d1d
wjy5446 <wjy5446@gmail.com>
2021-09-07 00:38:26 +0900

    feat) UserService 추가

--- app/src/main/java/com/codesoom/assignment/application/UserService.java
+++ app/src/main/java/com/codesoom/assignment/application/UserService.java
@@ -1,2 +1,38 @@
-package com.codesoom.assignment.application;public class UserService {
+package com.codesoom.assignment.application;
+
+import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.domain.UserRepository;
+import com.codesoom.assignment.exception.UserNotFoundException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    public User update(Long id, User updateUser) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+        user.update(updateUser);
+        return userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        try {
+            userRepository.deleteById(id);
+        } catch(EmptyResultDataAccessException e) {
+            throw new UserNotFoundException(id);
+        }
+    }
 }
