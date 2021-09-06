c944c81d812738d32d33a85b2c204637becd48d6
HyoungUkJJang <guddnr1451@gmail.com>
2021-09-06 18:10:36 +0900

    [5주차] 고양이 장난감가게 만들기 심화과제
    1. User 도메인 클래스 작성
    2. UserTest 테스트 클래스 작성

---
+++ app/src/test/java/com/codesoom/assignment/domain/UserTest.java
@@ -0,0 +1,54 @@
+package com.codesoom.assignment.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    private String UPDATE_NAME = "updateName";
+    private String UPDATE_EMAIL = "updateEmail@xxxx.com";
+    private String UPDATE_PASSWORD = "13579";
+
+    User user;
+
+    @BeforeEach
+    void setUp() {
+
+        user = User.builder()
+                .id(1L)
+                .name("name")
+                .email("email@xxxx.com")
+                .password("12345")
+                .build();
+
+    }
+
+    @Test
+    @DisplayName("사용자 객체가 올바르게 생성되는지 테스트")
+    void user_create_test() {
+
+        Assertions.assertEquals(1L, user.getId());
+        Assertions.assertEquals("name", user.getName());
+        Assertions.assertEquals("email@xxxx.com", user.getEmail());
+        Assertions.assertEquals("12345", user.getPassword());
+
+    }
+
+    @Test
+    @DisplayName("사용자 객체가 올바르게 수정되는지 테스트")
+    void user_update_test() {
+
+        user.userUpdate(UPDATE_NAME, UPDATE_EMAIL, UPDATE_PASSWORD);
+
+        Assertions.assertEquals(1L, user.getId());
+        Assertions.assertEquals(UPDATE_NAME, user.getName());
+        Assertions.assertEquals(UPDATE_EMAIL, user.getEmail());
+        Assertions.assertEquals(UPDATE_PASSWORD, user.getPassword());
+
+    }
+
+}
