10bc6954ebfc07917a936d5ae68ea3afe4761db7
injun <ison@sfu.ca>
2021-09-06 19:15:05 +0900

    etc: 4주차 코드 이전

--- app/src/main/java/com/codesoom/assignment/domain/CatToy.java
+++ app/src/main/java/com/codesoom/assignment/domain/CatToy.java
@@ -1,39 +1,32 @@
 package com.codesoom.assignment.domain;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity
 @Getter
-@Builder
-@NoArgsConstructor
+@Setter
+@Entity
 @AllArgsConstructor
-public class Product {
+@NoArgsConstructor
+public class CatToy {
     @Id
     @GeneratedValue
     private Long id;
-
     private String name;
-
     private String maker;
-
     private Integer price;
-
     private String imageUrl;
 
-    public void change(String name,
-                       String maker,
-                       Integer price,
-                       String imageUrl) {
-        this.name = name;
-        this.maker = maker;
-        this.price = price;
-        this.imageUrl = imageUrl;
+    public void update(CatToy catToy) {
+        this.name = catToy.getName();
+        this.maker = catToy.getMaker();
+        this.price = catToy.getPrice();
+        this.imageUrl = catToy.getImageUrl();
     }
 }
