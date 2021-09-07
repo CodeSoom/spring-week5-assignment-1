1815b7c23a041c5c9554f04d3800f9f7ab03832d
wjy5446 <wjy5446@gmail.com>
2021-09-07 23:55:48 +0900

    fix) request url 수정

--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
