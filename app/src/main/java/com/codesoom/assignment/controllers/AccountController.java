deb2f8da8e9b6fff324902038583f59e5985aa7f
catsbi <killfine198@gmail.com>
2021-09-07 11:45:38 +0900

    feat: AccountController javadoc 작성

--- app/src/main/java/com/codesoom/assignment/controllers/AccountController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/AccountController.java
@@ -18,7 +18,6 @@ import javax.validation.Valid;
 
 /**
  * 회원 정보 HTTP Request 요청 API를 받아 처리한다.
- * TODO 구현해야 할 기능
  * 1. 회원 생성하기 - POST /user
  * 2. 회원 수정하기 - PATCH /user/{id}
  * 3. 회원 삭제하기 - DELETE /user/{id}
@@ -29,17 +28,32 @@ import javax.validation.Valid;
 public class AccountController {
     private final AccountService accountService;
 
+    /**
+     * 회원 정보를 등록한다.
+     * @param accountData 저장할 회원 정보
+     * @return 저장된 회원 상세 정보
+     */
     @PostMapping
     public AccountData createAccount(@RequestBody @Valid AccountData accountData) {
         return accountService.creation(accountData);
     }
 
+    /**
+     * 회원 정보를 수정한다.
+     * @param id 수정 할 회원 식별자
+     * @param accountData 수정 할 회원 정보
+     * @return 수정된 회원 정보
+     */
     @PatchMapping("{id}")
     public AccountData patchAccount(@PathVariable long id,
                                     @RequestBody @Valid AccountData accountData) {
         return accountService.patchAccount(id, accountData);
     }
 
+    /**
+     * 회원 정보를 삭제한다.
+     * @param id 삭제 할 회원 식별자
+     */
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteAccount(@PathVariable Long id) {
