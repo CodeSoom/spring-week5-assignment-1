7df9d53bf32dae40d9d73013adbb6e6f6e21f876
pangnem <sprnd645@gmail.com>
2021-09-07 23:26:47 +0900

    fix: User 생성자에서 id 제외

--- app/src/main/java/com/codesoom/assignment/domain/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -16,8 +16,7 @@ public class User {
     }
 
     @Builder
-    public User(Long id, String name, String email, String password) {
-        this.id = id;
+    public User(String name, String email, String password) {
         this.name = name;
         this.email = email;
         this.password = password;
