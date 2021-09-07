907213cb37b65959bf9aa57fa36eb7f96d8f8037
wjy5446 <wjy5446@gmail.com>
2021-09-07 23:49:42 +0900

    feat) UserController 추가

--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -1,2 +1,38 @@
-package com.codesoom.assignment.controllers;public class UserController {
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.application.UserService;
+import com.codesoom.assignment.domain.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    User create(@Valid @RequestBody User user) {
+        return userService.create(user);
+    }
+
+    @PatchMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    User update(@PathVariable Long id,
+                @Valid @RequestBody User user) {
+        return userService.update(id, user);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void delete(@PathVariable Long id) {
+        userService.delete(id);
+    }
 }
