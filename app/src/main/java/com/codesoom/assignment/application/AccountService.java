bcb0f988764b02da74db40d0e14c403dc9cbb08a
catsbi <killfine198@gmail.com>
2021-09-07 11:43:25 +0900

    feat: AccountService 테스트케이스 추가 및 AccountService 기능 구현

--- app/src/main/java/com/codesoom/assignment/application/AccountService.java
+++ app/src/main/java/com/codesoom/assignment/application/AccountService.java
@@ -1,32 +1,47 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.domain.Account;
 import com.codesoom.assignment.domain.AccountRepository;
 import com.codesoom.assignment.dto.AccountData;
+import com.codesoom.assignment.exceptions.AccountNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 회원 정보를 관리한다.
- * TODO 다음 내용을 구현해야 한다.
  * 1. 회원 생성하기 - creation(AccountData)
  * 2. 회원 수정하기 - patchAccount(AccountData)
  * 3. 회원 삭제하기 - deleteAccount(Long id)
  */
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class AccountService {
     private final AccountRepository accountRepository;
 
 
     public AccountData creation(AccountData data) {
-        return null;
+        final Account account = data.toAccount();
+        final Account savedAccount = accountRepository.save(account);
+
+        return AccountData.from(savedAccount);
     }
 
+    @Transactional
     public AccountData patchAccount(long id, AccountData data) {
-        return null;
+        final Account foundAccount = accountRepository.findById(id)
+                .orElseThrow(() -> new AccountNotFoundException(id));
+
+        foundAccount.change(data.toAccount());
+
+        return AccountData.from(foundAccount);
     }
 
     public void deleteAccount(Long id) {
+        final Account foundAccount = accountRepository.findById(id)
+                .orElseThrow(() -> new AccountNotFoundException(id));
 
+        accountRepository.delete(foundAccount);
     }
 }
--- app/src/test/java/com/codesoom/assignment/application/AccountServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/AccountServiceTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static com.codesoom.assignment.Constant.ACCOUNT_EMAIL;
 import static com.codesoom.assignment.Constant.ACCOUNT_NAME;
 import static com.codesoom.assignment.Constant.ACCOUNT_PASSWORD;
@@ -42,6 +44,10 @@ class AccountServiceTest {
 
     private void setUpFixture() {
         given(accountRepository.save(any(Account.class))).willReturn(account);
+
+        given(accountRepository.findById(account.getId())).willReturn(Optional.of(account));
+
+        given(accountRepository.findById(100L)).willThrow(new AccountNotFoundException(100L));
     }
 
     @DisplayName("회원 정보를 등록할 수 있다. ")
@@ -75,7 +81,6 @@ class AccountServiceTest {
     @DisplayName("존재하지 않는 식별자의 회원 정보를 수정하려 하면 예외가 발생한다.")
     @Test
     void patchWithInValidAccount() {
-        AccountData savedAccount = accountService.creation(accountData);
         final AccountData target = AccountData.of(OTHER_ACCOUNT_NAME, OTHER_ACCOUNT_NAME, OTHER_ACCOUNT_PASSWORD);
 
         assertThatThrownBy(() -> accountService.patchAccount(100L, target))
@@ -87,10 +92,17 @@ class AccountServiceTest {
     @Test
     void deleteWithValidAccount() {
         AccountData savedAccount = accountService.creation(accountData);
-        final Account account = savedAccount.toAccount();
 
         accountService.deleteAccount(savedAccount.getId());
 
         verify(accountRepository).delete(account);
+    }
+
+    @DisplayName("회원 정보를 삭제할 수 있다.")
+    @Test
+    void deleteWithInValidAccount() {
+        assertThatThrownBy(()->accountService.deleteAccount(100L))
+                .isInstanceOf(AccountNotFoundException.class)
+                .hasMessage(String.format(AccountNotFoundException.DEFAULT_MESSAGE, 100L));
     }
 }
