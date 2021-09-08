0eb744e1b426471002041923345ebb3cafbc37b9
daeungkim <daeungkim93@gmail.com>
2021-09-08 09:02:35 +0900

    ProductService getProducts 메서드 테스트 정의
    ProductService getProducts 메서드 테스트 정의

--- app/src/test/java/com/codesoom/assignment/application/ProductServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/ProductServiceTest.java
@@ -1,142 +1,85 @@
 package com.codesoom.assignment.application;
 
-import com.codesoom.assignment.ProductNotFoundException;
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.domain.ProductRepository;
-import com.codesoom.assignment.dto.ProductData;
-import com.github.dozermapper.core.DozerBeanMapperBuilder;
-import com.github.dozermapper.core.Mapper;
+import com.google.common.collect.Lists;
 
+import org.assertj.core.api.ListAssert;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.OngoingStubbing;
 
 import java.util.List;
-import java.util.Optional;
 
+import static com.codesoom.assignment.domain.ProductConstants.ID;
+import static com.codesoom.assignment.domain.ProductConstants.NAME;
+import static com.codesoom.assignment.domain.ProductConstants.MAKER;
+import static com.codesoom.assignment.domain.ProductConstants.PRICE;
+import static com.codesoom.assignment.domain.ProductConstants.IMAGE_URL;
+import static com.codesoom.assignment.domain.ProductConstants.PRODUCT;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@Nested
+@DisplayName("ProductService 클래스")
+@ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
+    @InjectMocks
     private ProductService productService;
 
-    private ProductRepository productRepository = mock(ProductRepository.class);
+    @Mock
+    private ProductRepository productRepository;
 
-    @BeforeEach
-    void setUp() {
-        Mapper dozerMapper = DozerBeanMapperBuilder.buildDefault();
-        productService = new ProductService(dozerMapper, productRepository);
-
-        Product product = Product.builder()
-                .id(1L)
-                .name("쥐돌이")
-                .maker("냥이월드")
-                .price(5000)
-                .build();
-
-        given(productRepository.findAll()).willReturn(List.of(product));
-
-        given(productRepository.findById(1L)).willReturn(Optional.of(product));
-
-        given(productRepository.save(any(Product.class))).will(invocation -> {
-            Product source = invocation.getArgument(0);
-            return Product.builder()
-                    .id(2L)
-                    .name(source.getName())
-                    .maker(source.getMaker())
-                    .price(source.getPrice())
-                    .build();
-        });
-    }
-
-    @Test
-    void getProductsWithNoProduct() {
-        given(productRepository.findAll()).willReturn(List.of());
-
-        assertThat(productService.getProducts()).isEmpty();
-    }
-
-    @Test
-    void getProducts() {
-        List<Product> products = productService.getProducts();
-
-        assertThat(products).isNotEmpty();
-
-        Product product = products.get(0);
-
-        assertThat(product.getName()).isEqualTo("쥐돌이");
-    }
-
-    @Test
-    void getProductWithExsitedId() {
-        Product product = productService.getProduct(1L);
-
-        assertThat(product).isNotNull();
-        assertThat(product.getName()).isEqualTo("쥐돌이");
-    }
-
-    @Test
-    void getProductWithNotExsitedId() {
-        assertThatThrownBy(() -> productService.getProduct(1000L))
-                .isInstanceOf(ProductNotFoundException.class);
-    }
-
-    @Test
-    void createProduct() {
-        ProductData productData = ProductData.builder()
-                .name("쥐돌이")
-                .maker("냥이월드")
-                .price(5000)
-                .build();
-
-        Product product = productService.createProduct(productData);
-
-        verify(productRepository).save(any(Product.class));
-
-        assertThat(product.getId()).isEqualTo(2L);
-        assertThat(product.getName()).isEqualTo("쥐돌이");
-        assertThat(product.getMaker()).isEqualTo("냥이월드");
-    }
-
-    @Test
-    void updateProductWithExistedId() {
-        ProductData productData = ProductData.builder()
-                .name("쥐순이")
-                .maker("냥이월드")
-                .price(5000)
-                .build();
-
-        Product product = productService.updateProduct(1L, productData);
-
-        assertThat(product.getId()).isEqualTo(1L);
-        assertThat(product.getName()).isEqualTo("쥐순이");
-    }
-
-    @Test
-    void updateProductWithNotExistedId() {
-        ProductData productData = ProductData.builder()
-                .name("쥐순이")
-                .maker("냥이월드")
-                .price(5000)
-                .build();
-
-        assertThatThrownBy(() -> productService.updateProduct(1000L, productData))
-                .isInstanceOf(ProductNotFoundException.class);
-    }
-
-    @Test
-    void deleteProductWithExistedId() {
-        productService.deleteProduct(1L);
-
-        verify(productRepository).delete(any(Product.class));
-    }
-
-    @Test
-    void deleteProductWithNotExistedId() {
-        assertThatThrownBy(() -> productService.deleteProduct(1000L))
-                .isInstanceOf(ProductNotFoundException.class);
+    @Nested
+    @DisplayName("getProducts 메서드는")
+    class Describe_getProducts {
+        ListAssert<Product> subject() {
+            return assertThat(productService.getProducts());
+        }
+        OngoingStubbing<List<Product>> mockSubject() {
+            return when(productRepository.findAll());
+        }
+        @AfterEach
+        void afterEach() {
+            verify(productRepository).findAll();
+        }
+        @Nested
+        @DisplayName("저장된 Product가 있다면")
+        class Context_product_exist {
+            @BeforeEach
+            void beforeEach() {
+                mockSubject()
+                    .thenReturn(Lists.newArrayList(PRODUCT));
+            }
+            @Test
+            @DisplayName("Product 목록을 리턴한다.")
+            void it_returns_a_product_list() {
+                subject()
+                    .isNotEmpty();
+            }
+        }
+        @Nested
+        @DisplayName("저장된 Product가 없다면")
+        class Context_product_empty {
+            @BeforeEach
+            void beforeEach() {
+                mockSubject()
+                    .thenReturn(Lists.newArrayList());
+            }
+            @Test
+            @DisplayName("빈 목록을 리턴한다.")
+            void it_returns_a_empty_list() {
+                subject()
+                    .isEmpty();
+            }
+        }
     }
 }
