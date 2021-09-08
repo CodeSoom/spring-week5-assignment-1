d68e8c47587bc7bacc66c5a5e494c772dfe38999
daeungkim <daeungkim93@gmail.com>
2021-09-08 14:22:12 +0900

    User 엔티티 setter 삭제
    User 엔티티 setter 삭제

--- app/src/main/java/com/codesoom/assignment/domain/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -8,11 +8,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Getter
-@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
