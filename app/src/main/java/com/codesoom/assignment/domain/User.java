4f2533043aae5db02f399cbc0509cbfc4b0a141e
daeungkim <daeungkim93@gmail.com>
2021-09-08 11:03:49 +0900

    User 모델 정의
    User 모델 정의

---
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -0,0 +1,29 @@
+package com.codesoom.assignment.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String email;
+
+    private String name;
+
+    private String password;
+}
