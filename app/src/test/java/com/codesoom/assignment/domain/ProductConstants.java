d0aa4de73dc10c56aa14f809001d26f8fead1b98
daeungkim <daeungkim93@gmail.com>
2021-09-08 16:16:48 +0900

    테스트 용도 Product ProductData 데이터 id 삭제
    테스트 용도 Product ProductData 데이터 id 삭제
    -> id 사용하지 않음
    <description>

--- app/src/test/java/com/codesoom/assignment/domain/ProductConstants.java
+++ app/src/test/java/com/codesoom/assignment/domain/ProductConstants.java
@@ -11,8 +11,18 @@ public interface ProductConstants {
     final String MAKER = "냥이월드";
     final Integer PRICE = 1000;
     final String IMAGE_URL = "http://localhost:8080/rat";
-    final Product PRODUCT = new Product(ID, NAME, MAKER, PRICE, IMAGE_URL);
-    final ProductData PRODUCT_DATA = new ProductData(ID, NAME, MAKER, PRICE, IMAGE_URL);
+    final Product PRODUCT = Product.builder()
+        .name(NAME)
+        .maker(MAKER)
+        .price(PRICE)
+        .imageUrl(IMAGE_URL)
+        .build();
+    final ProductData PRODUCT_DATA = ProductData.builder()
+        .name(NAME)
+        .maker(MAKER)
+        .price(PRICE)
+        .imageUrl(IMAGE_URL)
+        .build();
     final List<Product> PRODUCT_LIST = Lists.newArrayList(PRODUCT);
     final List<Product> EMPTY_LIST = Lists.newArrayList();
 }
