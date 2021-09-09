059a3b90174c5635406017243fdf729c21822b10
Sungeheon <elon4856@gmail.com>
2021-09-09 16:10:46 +0900

    fix: 빈 줄을 삭제하세요.

--- app/src/main/java/com/codesoom/assignment/UserNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/UserNotFoundException.java
@@ -3,6 +3,5 @@ package com.codesoom.assignment;
 public class UserNotFoundException extends RuntimeException {
   public UserNotFoundException(Long id) {
     super("User not found: " + id);
-
   }
 }
