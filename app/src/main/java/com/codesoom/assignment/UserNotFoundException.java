be1a41379b94495ab19d9b15d337c4a246729d27
pangnem <sprnd645@gmail.com>
2021-09-09 10:52:24 +0900

    docs: UserNotFoundException 문구 수정

--- app/src/main/java/com/codesoom/assignment/UserNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/UserNotFoundException.java
@@ -1,10 +1,15 @@
 package com.codesoom.assignment;
 
 /**
- * 회원을 찾지 못할 시 던집니다.
+ * 회원을 찾지 못할 시 나타내는 예외
  */
 public class UserNotFoundException extends RuntimeException {
 
+    /**
+     * id로 회원을 찾시 못할 시 던집니다.
+     *
+     * @param id 회원 아이디
+     */
     public UserNotFoundException(Long id) {
         super(String.format("id %s에 대한 회원을 찾을 수 없습니다.", id));
     }
