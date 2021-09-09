ba7059d573657609da81a38e992039473543715b
pangnem <sprnd645@gmail.com>
2021-09-10 05:45:59 +0900

    refactor: fixture 추출 - id, user

--- app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
@@ -32,46 +32,36 @@ public class UserServiceTest {
             .build());
     }
 
-    @Test
-    void create() {
-        User createdUser = userService.createUser(User.builder()
-            .name("name")
-            .email("email")
-            .password("password")
-            .build());
-
-        assertThat(createdUser.getName()).isEqualTo("name");
-        assertThat(createdUser.getEmail()).isEqualTo("email");
-        assertThat(createdUser.getPassword()).isEqualTo("password");
-    }
-
     @Nested
     @DisplayName("updateUser 메서드는")
     class Describe_updateUser {
+
+        private final User source = User.builder()
+            .name("name2")
+            .email("email2")
+            .password("password2")
+            .build();
+        private Long id;
+
+        @BeforeEach
+        void setUp() {
+            id = user.getId();
+        }
 
         @Nested
         @DisplayName("존재하는 id인 경우")
         class Context_existId {
 
-            private Long existId;
-
             @BeforeEach
             void setUp() {
-                assertThat(userRepository.existsById(user.getId()))
+                assertThat(userRepository.existsById(id))
                     .isTrue();
-
-                existId = user.getId();
             }
 
             @Test
             @DisplayName("수정한 유저를 리턴한다")
             void it_returns_updated_user() {
-                User source = User.builder()
-                    .name("name2")
-                    .email("email2")
-                    .password("password2")
-                    .build();
-                User updatedUser = userService.updateUser(existId, source);
+                User updatedUser = userService.updateUser(id, source);
 
                 assertThat(updatedUser.getName()).isEqualTo("name2");
                 assertThat(updatedUser.getEmail()).isEqualTo("email2");
@@ -83,25 +73,19 @@ public class UserServiceTest {
         @DisplayName("존재하지 않는 id인 경우")
         class Context_notExistId {
 
-            private Long notExistId;
-
             @BeforeEach
             void setUp() {
                 userRepository.deleteAll();
 
-                assertThat(userRepository.existsById(user.getId()))
+                assertThat(userRepository.existsById(id))
                     .isFalse();
-
-                notExistId = user.getId();
             }
 
             @Test
             @DisplayName("에러를 던진다")
             void it_throws() {
-                User source = User.builder().build();
-
                 assertThatThrownBy(() -> {
-                    userService.updateUser(notExistId, source);
+                    userService.updateUser(id, source);
                 }).isInstanceOf(UserNotFoundException.class);
             }
         }
@@ -111,27 +95,28 @@ public class UserServiceTest {
     @DisplayName("deleteUser 메서드는")
     class Describe_deleteUser {
 
+        private Long id;
+
+        @BeforeEach
+        void setUp() {
+            id = user.getId();
+        }
+
         @Nested
         @DisplayName("존재하는 회원인 경우")
         class Context_findUser {
 
-            private Long findUserId;
-
             @BeforeEach
             void setUp() {
-                Long id = user.getId();
-
                 assertThat(userRepository.existsById(id)).isTrue();
-
-                findUserId = id;
             }
 
             @Test
             @DisplayName("삭제한다")
             void it_delete() {
-                userService.deleteUser(findUserId);
+                userService.deleteUser(id);
 
-                assertThat(userRepository.existsById(findUserId))
+                assertThat(userRepository.existsById(id))
                     .isFalse();
             }
         }
@@ -140,20 +125,16 @@ public class UserServiceTest {
         @DisplayName("회원을 찾지 못한 경우")
         class Context_notFoundUser {
 
-            private Long notFoundUserId;
-
             @BeforeEach
             void setUp() {
                 userRepository.deleteAll();
-
-                notFoundUserId = user.getId();
             }
 
             @Test
             @DisplayName("에러를 던진다")
             void it_throws() {
                 assertThatThrownBy(() ->
-                    userService.deleteUser(notFoundUserId)
+                    userService.deleteUser(id)
                 ).isInstanceOf(UserNotFoundException.class);
             }
         }
