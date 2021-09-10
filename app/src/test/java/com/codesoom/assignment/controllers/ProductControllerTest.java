0ad51c74f6a7eab68ae8e74826006f321f040807
daeungkim <daeungkim93@gmail.com>
2021-09-10 10:30:25 +0900

    ProductController @WebMvcTest 어노테이션 삭제
    ProductController @WebMvcTest 어노테이션 삭제 및 테스트 반영
    -> WebMvcTest의 경우 테스트하려는 Controller의 메서드만 테스트 하지 않음
    -> 예를들어 Http Status 코드 리턴값 또는 ControllerAdvice 등
    -> 이러한 경우 유닛테스트의 목적에 맞지 않음
    <description>

--- app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
@@ -1,85 +1,74 @@
 package com.codesoom.assignment.controllers;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static com.codesoom.assignment.constants.ProductConstants.PRODUCT_LIST;
+
+import java.util.Arrays;
+import java.util.List;
+
 import com.codesoom.assignment.application.ProductService;
 import com.codesoom.assignment.domain.Product;
-import com.codesoom.assignment.utils.Parser;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.stubbing.OngoingStubbing;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
-import java.util.List;
-
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static com.codesoom.assignment.constants.ProductConstants.PRODUCT_LIST;
-
-@WebMvcTest(ProductController.class)
+@Nested
+@ExtendWith(MockitoExtension.class)
 @DisplayName("ProductController 클래스")
-class ProductControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
+public class ProductControllerTest {
+    @InjectMocks
+    private ProductController productController;
 
-    @MockBean
+    @Mock
     private ProductService productService;
-
-    @AfterEach
-    private void afterEach() {
-        reset(productService);
-    }
 
     @Nested
     @DisplayName("list 메서드는")
     public class Describe_list {
-        private ResultActions subject() throws Exception {
-            return mockMvc.perform(
-                get("/products")
-                    .accept(MediaType.APPLICATION_JSON_UTF8)
-            );
+        private List<Product> subject() {
+            return productController.list();
         }
 
-        private OngoingStubbing<List<Product>> mockGetProducts() {
+        private OngoingStubbing<List<Product>> mockFindAll() {
             return when(productService.getProducts());
         }
 
-        private ProductService verifyGetProducts(final int invokeCounts) {
+        private ProductService verifyFindAll(final int invokeCounts) {
             return verify(productService, times(invokeCounts));
         }
 
         @BeforeEach
         private void beforeEach() {
-            mockGetProducts()
+            mockFindAll()
                 .thenReturn(PRODUCT_LIST);
         }
 
         @AfterEach
         private void afterEach() {
-            verifyGetProducts(1)
+            verifyFindAll(1)
                 .getProducts();
         }
 
         @Test
         @DisplayName("ProductService getProducts 메서드의 리턴값을 리턴한다.")
-        public void it_returns_a_product_list() throws Exception {
-            subject()
-                .andExpect(status().isOk())
-                .andExpect(content().string(
-                    Parser.toJson(PRODUCT_LIST)
-                ));
+        public void it_returns_a_productService_getProducts_return_value() {
+            assertThat(subject())
+                .matches(
+                    outputList -> Arrays.deepEquals(
+                        PRODUCT_LIST.toArray(), outputList.toArray()
+                    )
+                );
         }
     }
 }
