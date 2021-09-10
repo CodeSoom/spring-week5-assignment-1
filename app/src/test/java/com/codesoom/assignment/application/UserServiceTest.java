7b5e63ea145594f32bec1509b8a27c9e3fdfbcb3
daeungkim <daeungkim93@gmail.com>
2021-09-10 17:55:51 +0900

    UserService updateUser 메서드 테스트 작성
    UserService updateUser 메서드 테스트 작성

--- app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
@@ -8,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -20,7 +21,6 @@ import com.codesoom.assignment.domain.UserRepository;
 import com.codesoom.assignment.dto.UserData;
 import com.github.dozermapper.core.Mapper;
 
-import org.assertj.core.api.ObjectAssert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -45,7 +45,7 @@ public class UserServiceTest {
     @Mock
     private Mapper dozerMapper;
 
-    private OngoingStubbing<User> whenMapper() {
+    private OngoingStubbing<User> mockMapper() {
         return when(dozerMapper.map(any(UserData.class), eq(User.class)));
     }
 
@@ -57,42 +57,57 @@ public class UserServiceTest {
         return when(userRepository.findById(anyLong()));
     }
 
-    private void verifyFindById(final int invokeNumber) {
-        verify(userRepository, times(invokeNumber))
-            .findById(anyLong());
+    private OngoingStubbing<User> mockSave() {
+        return when(userRepository.save(any(User.class)));
+    }
+
+    private UserRepository verifyRepository(final int invokeNumber) {
+        return verify(userRepository, times(invokeNumber));
     }
 
     @Nested
     @DisplayName("createUser 메서드는")
     public class Describe_createUser {
-        private ObjectAssert<User> subject() {
-            return assertThat(userService.createUser(USER_DATA));
+        private User subject() {
+            return userService.createUser(USER_DATA);
         }
 
-        private OngoingStubbing<User> mockSubject() {
-            return when(userRepository.save(any(User.class)));
-        }
+        @Nested
+        @DisplayName("Mapper map메서드가 저장하지 않은 User를 생성하고")
+        public class Context_mapper_creates_an_unsaved_user {
+            @BeforeEach
+            private void beforeEach() {
+                mockMapper()
+                    .thenReturn(USER);
+            }
 
-        @BeforeEach
-        public void beforeEach() {
-            whenMapper()
-                .thenReturn(USER);
-            mockSubject()
-                .thenReturn(USER);
-        }
+            @AfterEach
+            private void afterEach() {
+                verifyMapper();
+            }
 
-        @AfterEach
-        public void afterEach() {
-            verifyMapper();
-            verify(userRepository)
-                .save(any(User.class));
-        }
+            @Nested
+            @DisplayName("UserRepository save메서드가 User를 저장하면")
+            public class Context_repository_saves_user {
+                @BeforeEach
+                private void beforeEach() {
+                    mockSave()
+                        .thenReturn(USER);
+                }
 
-        @Test
-        @DisplayName("User를 생성하고 리턴한다.")
-        public void it_creates_a_user() {
-            subject()
-                .isInstanceOf(User.class);
+                @AfterEach
+                private void afterEach() {
+                    verifyRepository(1)
+                        .save(any(User.class));
+                }
+
+                @Test
+                @DisplayName("저장한 User를 리턴한다.")
+                public void it_returns_a_saved_user() {
+                    assertThat(subject())
+                        .isInstanceOf(User.class);
+                }
+            }
         }
     }
 
@@ -103,53 +118,155 @@ public class UserServiceTest {
             userService.deleteUser(ID);
         }
 
-        private void verifyDelete(final int invokeNumber) {
-            verify(userRepository, times(invokeNumber))
-                .delete(any(User.class));
-        }
-
         @Nested
-        @DisplayName("User를 찾을 수 없는 경우")
-        public class Context_find_fail {
+        @DisplayName("UserRepository findById메서드가 예외를 던지면")
+        public class Context_repository_throws_an_exception {
             @BeforeEach
-            void beforeEach() {
+            private void beforeEach() {
                 mockFindById()
                     .thenThrow(new NotFoundException(ID, User.class.getSimpleName()));
             }
 
             @AfterEach
-            void afterEach() {
-                verifyFindById(1);
-                verifyDelete(0);
+            private void afterEach() {
+                verifyRepository(1)
+                    .findById(anyLong());
+                verifyRepository(0)
+                    .delete(any(User.class));
             }
 
             @Test
-            @DisplayName("NotFoundException을 던진다.")
-            void it_throws_a_notFoundException() {
+            @DisplayName("예외를 캐치하지 않는다.")
+            public void it_throws_a_notFoundException() {
                 assertThatThrownBy(() -> subject())
                     .isInstanceOf(NotFoundException.class);
             }
         }
 
         @Nested
-        @DisplayName("User를 찾을 수 있는 경우")
-        public class Context_find_success {
+        @DisplayName("UserRepository findById메서드가 User를 찾으면")
+        public class Context_repository_finds_a_user {
             @BeforeEach
-            void beforeEach() {
+            private void beforeEach() {
                 mockFindById()
                     .thenReturn(Optional.of(USER));
             }
 
             @AfterEach
-            void afterEach() {
-                verifyFindById(1);
-                verifyDelete(1);
+            private void afterEach() {
+                verifyRepository(1)
+                    .findById(anyLong());
+                verifyRepository(1)
+                    .delete(any(User.class));
             }
 
             @Test
-            @DisplayName("찾은 User를 삭제한다.")
+            @DisplayName("UserRepository delete 메서드를 호출한다.")
             public void it_deletes_a_user() {
                 subject();
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("updateUser 메서드는")
+    public class Describe_updateUser {
+        private final User user = mock(User.class);
+
+        private User subject() {
+            return userService.updateUser(ID, USER_DATA);
+        }
+
+        private void verifyDomain(final int invokeCounts) {
+            verify(user, times(invokeCounts)).update(any(User.class));
+        }
+
+        @Nested
+        @DisplayName("UserRepository findById 메서드가 예외를 던지면")
+        public class Context_repository_throws_an_exception {
+            @BeforeEach
+            private void beforeEach() {
+                mockFindById()
+                    .thenThrow(new NotFoundException(ID, User.class.getSimpleName()));
+            }
+
+            @AfterEach
+            private void afterEach() {
+                verifyRepository(1)
+                    .findById(anyLong());
+                verifyDomain(0);
+                verifyRepository(0)
+                    .save(any(User.class));
+            }
+
+            @Test
+            @DisplayName("예외를 캐치하지 않는다.")
+            public void it_does_not_catch_exceptions() {
+                assertThatThrownBy(() -> subject())
+                    .isInstanceOf(NotFoundException.class);
+            }
+        }
+
+        @Nested
+        @DisplayName("UserRepository findById가 수정할 User를 찾은 경우")
+        public class Context_repository_finds_a_user {
+            @BeforeEach
+            private void beforeEach() {
+                mockFindById()
+                    .thenReturn(Optional.of(user));
+            }
+
+            @AfterEach
+            private void afterEach() {
+                verifyRepository(1)
+                    .findById(anyLong());
+            }
+
+            @Nested
+            @DisplayName("Mapper map메서드가 수정할 데이터를 생성하고")
+            public class Context_mapper_generates_data_to_be_updated {
+                @BeforeEach
+                private void beforeEach() {
+                    mockMapper()
+                        .thenReturn(USER);
+                }
+
+                @AfterEach
+                private void afterEach() {
+                    verifyMapper();
+                }
+
+                @Nested
+                @DisplayName("User update메서드가 데이터를 수정하면")
+                public class Context_update_user {
+                    @AfterEach
+                    private void afterEach() {
+                        verifyDomain(1);
+                    }
+
+                    @Nested
+                    @DisplayName("UserRepository save메서드가 수정한 User를 저장하고")
+                    public class Context_repository_saves_user {
+                        @BeforeEach
+                        private void beforeEach() {
+                            mockSave()
+                                .thenReturn(USER);
+                        }
+
+                        @AfterEach
+                        private void afterEach() {
+                            verifyRepository(1)
+                                .save(any(User.class));
+                        }
+
+                        @Test
+                        @DisplayName("업데이트한 User를 리턴한다.")
+                        public void it_returns_a_updated_user() {
+                            assertThat(subject())
+                                .isInstanceOf(User.class);
+                        }
+                    }
+                }
             }
         }
     }
