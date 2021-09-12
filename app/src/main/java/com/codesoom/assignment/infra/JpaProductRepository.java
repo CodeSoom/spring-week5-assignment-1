cb33685b0d9f12adccca1421f4f38664ba199396
daeungkim <daeungkim93@gmail.com>
2021-09-12 19:01:57 +0900

    불필요한 javadoc 삭제
    불필요한 javadoc 삭제

--- app/src/main/java/com/codesoom/assignment/infra/JpaProductRepository.java
+++ app/src/main/java/com/codesoom/assignment/infra/JpaProductRepository.java
@@ -8,12 +8,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 import java.util.Optional;
 
-/**
- * Product 리소스 비즈니스 로직과 데이터베이스를 연결한다.
- *
- * @see Product
- * @see ProductRepository
- */
 @Primary
 public interface JpaProductRepository
         extends ProductRepository, CrudRepository<Product, Long> {
--- app/src/main/java/com/codesoom/assignment/infra/JpaUserRepository.java
+++ app/src/main/java/com/codesoom/assignment/infra/JpaUserRepository.java
@@ -6,12 +6,6 @@ import com.codesoom.assignment.domain.UserRepository;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * User 리소스 비즈니스 로직과 데이터베이스를 연결한다.
- *
- * @see User
- * @see UserRepository
- */
 @Primary
 public interface JpaUserRepository
     extends UserRepository, CrudRepository<User, Long> {
