8fce53cad1508e511e07b3aabbac12083aa8e059
bloomspes <bloomspes@gmail.com>
2021-09-12 19:30:24 +0900

    [과제] test : POST /users 간단한 테스트 로직 설계 중

--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -5,5 +5,19 @@
 
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.dto.UserData;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    UserData userData createAccount() {
+        return userData.getId()
+    }
 }
--- app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
@@ -6,7 +6,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -20,9 +22,13 @@ class UserControllerTest {
         mockMvc.perform(
                 post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{}")
+                        .content("{\"name\":\"슭\",\n" +
+                                "\"email\":\"bloomspes@gmail.com\"}")
         )
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(content().string(
+                        containsString("\"email\":\"bloomspes@gmail.com\"")
+                        ));
     }
 
 }
