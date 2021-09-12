b39d570a1959d721b8cbb4e5a7df82e3f2907f27
bloomspes <bloomspes@gmail.com>
2021-09-12 18:56:21 +0900

    [과제] add : POST /users 테스트코드 작성 완료

--- app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
@@ -1,15 +1,18 @@
 package com.codesoom.assignment.controllers;
 
-import jdk.internal.jshell.tool.ConsoleIOContext;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @WebMvcTest(UserController.class)
 class UserControllerTest {
+    @Autowired
     private MockMvc mockMvc;
 
     @Test
@@ -18,6 +21,8 @@ class UserControllerTest {
                 post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{}")
-        );
+        )
+                .andExpect(status().isCreated());
     }
+
 }
