7165688bcb4e0d34f5e79c88962cd17e35b34282
bloomspes <bloomspes@gmail.com>
2021-09-12 18:48:33 +0900

    [과제] add : POST /users 테스트 로직 작성
    - post 통신 할 url path 작성
    - contentType 명시
    - content 형식 명시

--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -1,2 +1,9 @@
-package com.codesoom.assignment.controllers;public class UserController {
+// TODO
+// 1. 회원 생성하기 - POST /user
+// 2. 회원 수정하기 - PUT, PATCH /user/{id}
+// 3. 회원 삭제하기 - DELETE /user/{id}
+
+package com.codesoom.assignment.controllers;
+
+public class UserController {
 }
--- app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
@@ -3,14 +3,21 @@ package com.codesoom.assignment.controllers;
 import jdk.internal.jshell.tool.ConsoleIOContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(UserController.class)
 class UserControllerTest {
     private MockMvc mockMvc;
-    
+
     @Test
-    void registerUser() {
-        mockMvc.perform();
+    void registerUser() throws Exception {
+        mockMvc.perform(
+                post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{}")
+        );
     }
 }
