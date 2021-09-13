bc2745075a4af616c555fe7b2e5537b45880215d
bloomspes <bloomspes@gmail.com>
2021-09-13 16:06:30 +0900

    [과제] fix : user model 수정

--- app/src/main/java/com/codesoom/assignment/domain/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -3,15 +3,14 @@ package com.codesoom.assignment.domain;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-
-@Entity
 @Getter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class User {
-    private String Email;
+    private Long id;
+
+    private String name;
+
+    private String email;
 }
