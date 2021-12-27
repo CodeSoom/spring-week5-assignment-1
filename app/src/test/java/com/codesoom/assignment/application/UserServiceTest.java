22f63c5604d421082bb234dd6da300493ad82f4d
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2021-12-27 22:16:58 +0900

    [생성] - 유저를 저장하는 createUser에 대한 테스트

---
+++ app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
@@ -0,0 +1,22 @@
+package com.codesoom.assignment.application;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings({"InnerClassMayBeStatic", "NonAsciiCharacters"})
+@DisplayName("UserService 클래스")
+class UserServiceTest {
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class createUser_메소드는 {
+
+        @Test
+        void 유저를_저장한다() {
+
+        }
+    }
+}
