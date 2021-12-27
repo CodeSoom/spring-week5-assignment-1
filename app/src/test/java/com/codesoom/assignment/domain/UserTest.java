eb38e06a219f13545496169cbef1ca94ef97217a
kyuwon53 <lkw2v@naver.com>
2021-12-27 16:23:45 +0900

    [ADD] 사용자 정보를 바꾸는 메서드를 테스트하라

--- app/src/test/java/com/codesoom/assignment/domain/UserTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/UserTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DisplayName("User 클래스")
 class UserTest {
     @Nested
@@ -18,6 +20,35 @@ class UserTest {
                     .password("1234")
                     .email("codesoom@gmail.com")
                     .build();
+            assertThat(user.getId()).isEqualTo(1L);
+            assertThat(user.getName()).isEqualTo("코드숨");
+            assertThat(user.getPassword()).isEqualTo("1234");
+            assertThat(user.getEmail()).isEqualTo("codesoom@gmail.com");
+        }
+    }
+
+    @Nested
+    @DisplayName("change 메서드는")
+    class Decribe_change {
+        @Test
+        @DisplayName("사용자 정보를 바꿔준다.")
+        void it_change_user() {
+            User user = User.builder()
+                    .id(1L)
+                    .name("코드숨")
+                    .password("1234")
+                    .email("codesoom@gmail.com")
+                    .build();
+
+            user.changeWith(User.builder()
+                    .name("코드숨1")
+                    .password("5678")
+                    .email("codesoom1@gmail.com")
+                    .build());
+
+            assertThat(user.getName()).isEqualTo("코드숨1");
+            assertThat(user.getPassword()).isEqualTo("5678");
+            assertThat(user.getEmail()).isEqualTo("codesoom1@gmail.com");
         }
     }
 }
