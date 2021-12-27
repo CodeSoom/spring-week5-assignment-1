21dee392acb8189945aff85b05c33373ee06d1b4
hyuk <pjh08190819@naver.com>
2021-12-28 03:11:26 +0900

    User 클래스 구현

---
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -0,0 +1,32 @@
+package com.codesoom.assignment.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    private String email;
+
+    @Builder
+    public User(String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+}
