a067ab429199468cb432b773e1008e429cc4150e
kyuwon53 <lkw2v@naver.com>
2021-12-28 10:09:06 +0900

    [Add] 사용자 생성 함수를 테스트하라

--- app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.codesoom.assignment.application;
 import com.codesoom.assignment.UserNotFoundException;
 import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
+import com.codesoom.assignment.dto.UserData;
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,8 +16,10 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 @DisplayName("UserService 클래스")
 class UserServiceTest {
@@ -37,6 +40,16 @@ class UserServiceTest {
         Mapper mapper = DozerBeanMapperBuilder.buildDefault();
 
         userService = new UserService(mapper, userRepository);
+
+        User user = User.builder()
+                .id(1L)
+                .name(USER_NAME)
+                .password(USER_PASSWORD)
+                .email(USER_EMAIL)
+                .build();
+
+        given(userRepository.findAll()).willReturn(List.of(user));
+        given(userRepository.findById(1L)).willReturn(Optional.of(user));
     }
 
     @Nested
@@ -45,17 +58,6 @@ class UserServiceTest {
         @Nested
         @DisplayName("등록된 사용자가 있으면")
         class Context_has_user {
-            @BeforeEach
-            void setUp() {
-                User user = User.builder()
-                        .id(1L)
-                        .name(USER_NAME)
-                        .password(USER_PASSWORD)
-                        .email(USER_EMAIL)
-                        .build();
-
-                given(userRepository.findAll()).willReturn(List.of(user));
-            }
 
             @Test
             @DisplayName("전체 목록을 리턴한다.")
@@ -65,6 +67,7 @@ class UserServiceTest {
                 assertThat(users).isNotEmpty();
             }
         }
+
         @Nested
         @DisplayName("등록된 사용자가 없으면")
         class Context_hasnot_user {
@@ -87,27 +90,16 @@ class UserServiceTest {
         @Nested
         @DisplayName("id에 해당하는 사용자가 있으면")
         class Context_With_Exist_userId {
-            @BeforeEach
-            void setUp() {
-                User user = User.builder()
-                        .id(1L)
-                        .name(USER_NAME)
-                        .password(USER_PASSWORD)
-                        .email(USER_EMAIL)
-                        .build();
-
-                given(userRepository.findById(1L)).willReturn(Optional.of(user));
-            }
 
             @Test
             @DisplayName("사용자 정보를 리턴한다.")
             void it_return_user() {
                 User user = userService.getUser(1L);
 
-                assertThat(user).isNotNull();
                 assertThat(user.getName()).isEqualTo(USER_NAME);
             }
         }
+
         @Nested
         @DisplayName("등록된 사용자가 없으면")
         class Context_hasnot_user {
@@ -116,6 +108,46 @@ class UserServiceTest {
             void it_return_users() {
                 assertThatThrownBy(() -> userService.getUser(1000L))
                         .isInstanceOf(UserNotFoundException.class);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("createUser 메소드는")
+    class Describe_createUser {
+        @Nested
+        @DisplayName("사용자 정보를 입력받으면")
+        class Context_when_user_info {
+            @BeforeEach
+            void setUp() {
+                given(userRepository.save(any(User.class))).will(invocation -> {
+                    User source = invocation.getArgument(0);
+                    return User.builder()
+                            .id(2L)
+                            .name(source.getName())
+                            .password(source.getPassword())
+                            .email(source.getEmail())
+                            .build();
+                });
+            }
+
+            @Test
+            @DisplayName("사용자를 생성하고 리턴한다.")
+            void it_return_user() {
+                UserData userData = UserData.builder()
+                        .name(NEW_NAME)
+                        .password(NEW_PASSWORD)
+                        .email(NEW_EMAIL)
+                        .build();
+
+                User user = userService.createUser(userData);
+
+                verify(userRepository).save(any(User.class));
+
+                assertThat(user.getId()).isEqualTo(2L);
+                assertThat(user.getName()).isEqualTo(NEW_NAME);
+                assertThat(user.getPassword()).isEqualTo(NEW_PASSWORD);
+                assertThat(user.getEmail()).isEqualTo(NEW_EMAIL);
             }
         }
     }
