4469516286e9a2edc30d6795b6ed9658270e4bc2
kyuwon53 <lkw2v@naver.com>
2021-12-28 10:51:36 +0900

    [Add] javadoc 추가

--- app/src/main/java/com/codesoom/assignment/application/UserService.java
+++ app/src/main/java/com/codesoom/assignment/application/UserService.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
 
+/**
+ * 사용자를 관리합니다.
+ */
 @Service
 @Transactional
 public class UserService {
@@ -24,19 +27,31 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    /**
+     * 사용자 목록을 반환합니다.
+     *
+     * @return 사용자 목록
+     */
     public List<User> getUsers() {
         return userRepository.findAll();
     }
 
+    /**
+     * id에 해당하는 사용자를 반환합니다
+     *
+     * @param id 사용자 id
+     * @return 해당하는 사용자
+     */
     public User getUser(long id) {
         return findUser(id);
     }
 
-    private User findUser(long id) {
-        return userRepository.findById(id)
-                .orElseThrow(() -> new UserNotFoundException(id));
-    }
-
+    /**
+     * 사용자를 저장하고 반환합니다.
+     *
+     * @param userData 사용자 정보
+     * @return 저장된 사용자
+     */
     public User createUser(UserData userData) {
         Mapper mapper = DozerBeanMapperBuilder.buildDefault();
         User user = mapper.map(userData, User.class);
@@ -44,6 +59,12 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    /**
+     * id에 해당하는 사용자를 수정하고 반환합니다.
+     *
+     * @param userData 사용자 정보
+     * @return 저장된 사용자
+     */
     public User updateUser(long id, UserData userData) {
         User user = findUser(id);
 
@@ -52,10 +73,28 @@ public class UserService {
         return user;
     }
 
+    /**
+     * id에 해당하는 사용자를 삭제하고 반환합니다.
+     *
+     * @param id 사용자 id
+     * @return 저장된 사용자
+     */
     public User deleteUser(long id) {
         User user = findUser(id);
         userRepository.delete(user);
 
         return user;
+    }
+
+    /**
+     * id에 해당하는 사용자를 찾고 없으면 예외를 던집니다.
+     *
+     * @param id 사용자 id
+     * @return 사용자
+     * @throws UserNotFoundException (사용자를 찾을 수 없다는 예외)
+     */
+    public User findUser(long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
     }
 }
