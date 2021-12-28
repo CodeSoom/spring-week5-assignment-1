0c9be841287a6fcda590b11cb6753ab795edf56b
kyuwon53 <lkw2v@naver.com>
2021-12-28 13:40:16 +0900

    [Add] javadoc 설명 추가

--- app/src/main/java/com/codesoom/assignment/domain/UserRepository.java
+++ app/src/main/java/com/codesoom/assignment/domain/UserRepository.java
@@ -3,7 +3,11 @@ package com.codesoom.assignment.domain;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * 사용자 데이터를 관리합니다.
+ */
 public interface UserRepository {
+
     List<User> findAll();
 
     Optional<User> findById(Long id);
