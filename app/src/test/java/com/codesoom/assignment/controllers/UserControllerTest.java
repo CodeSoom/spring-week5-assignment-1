f6f7e3a42c9bc7c893808fde9f8566c84a317e1f
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2021-12-28 22:18:49 +0900

    [수정] - 테스트 경고창 제거

--- app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SuppressWarnings({"InnerClassMayBeStatic", "NonAsciiCharacters"})
 @WebMvcTest(UserController.class)
 @DisplayName("UserController 클래스")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
