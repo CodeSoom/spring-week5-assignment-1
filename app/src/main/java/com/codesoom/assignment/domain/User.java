acacae8b4bdd22b33b55c9fcba0d38cdfc6dc8c0
hyuk <pjh08190819@naver.com>
2021-12-29 00:20:49 +0900

    import 정리

--- app/src/main/java/com/codesoom/assignment/domain/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -3,7 +3,6 @@ package com.codesoom.assignment.domain;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- app/src/main/java/com/codesoom/assignment/dto/UserData.java
+++ app/src/main/java/com/codesoom/assignment/dto/UserData.java
@@ -3,7 +3,6 @@ package com.codesoom.assignment.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
