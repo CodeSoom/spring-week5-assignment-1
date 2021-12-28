642645c70abd9f0155137fb94872cece5aa7dcc1
ririti <xodnzlzl@naver.com>
2021-12-29 00:38:48 +0900

    UserServiceTest 구현

--- app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.codesoom.assignment.application;
 import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
 import com.codesoom.assignment.dto.UserData;
+import com.codesoom.assignment.infra.UserNotFoundException;
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,7 +14,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
@@ -81,6 +82,75 @@ class UserServiceTest {
                 assertThat(user.getName()).isEqualTo(USER_NAME);
                 assertThat(user.getPassword()).isEqualTo(USER_PASSWORD);
                 assertThat(user.getEmail()).isEqualTo(USER_EMAIL);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("updateUser 메서드는")
+    class Describe_updateUser{
+        @Nested
+        @DisplayName("user의 id가 주어진다면")
+        class Context_with_userId{
+
+            @Test
+            @DisplayName("user을 수정하고 리턴한다.")
+            void it_return_user(){
+                UserData userData = UserData.builder()
+                        .name("코드숨")
+                        .password("1597")
+                        .email("codesoom@gmail.com")
+                        .build();
+
+                User user = userService.updateUser(1L, userData);
+
+                assertThat(user.getName()).isEqualTo("코드숨");
+                assertThat(user.getPassword()).isEqualTo("1597");
+                assertThat(user.getEmail()).isEqualTo("codesoom@gmail.com");
+            }
+        }
+
+        @Nested
+        @DisplayName("등록되지 않은 User의 id가 주어진다면")
+        class Context_withOut_userId{
+
+            @Test
+            @DisplayName("User를 찾을 수 없다는 예외를 던진다.")
+            void it_return_user(){
+                UserData source = new UserData();
+                source.setName("코드숨");
+
+                assertThatThrownBy(()-> userService.updateUser(100L, source)).isInstanceOf(UserNotFoundException.class);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("delete 메서드는")
+    class Describe_deleteUser{
+        @Nested
+        @DisplayName("user의 id가 주어진다면")
+        class Context_with_userId{
+
+            @Test
+            @DisplayName("user를 삭제하고 리턴한다")
+            void it_return_user(){
+                userService.deleteUser(USER_ID);
+
+                verify(userRepository).delete(any(User.class));
+            }
+        }
+
+        @Nested
+        @DisplayName("등록되지 않은 User의 id가 주어진다면")
+        class Context_withOut_userId{
+
+            @Test
+            @DisplayName("User를 찾을 수 없다는 예외를 던진다.")
+            void it_return_error(){
+                assertThatThrownBy(()-> userService.deleteUser(100L)).isInstanceOf(UserNotFoundException.class);
+
+                verify(userRepository).findById(100L);
             }
         }
     }
