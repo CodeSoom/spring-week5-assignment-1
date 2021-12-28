122687cc7630625587fecc76c20f4ef72534a8e9
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2021-12-28 22:15:29 +0900

    [수정] - 회원 저장 요청에 대한 구현

--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -1,10 +1,30 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.UserService;
+import com.codesoom.assignment.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 회원의 요청을 관리합니다.
+ */
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserService userService;
+
+    /**
+     * 저장된 회원을 리턴한다.
+     *
+     * @param user 저장할 회원
+     * @return 저장된 회원
+     */
+    @PostMapping
+    public User create(User user) {
+        return userService.createUser(user);
+    }
 }
--- app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
@@ -1,16 +1,29 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
 @DisplayName("UserController 클래스")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
 
     @Nested
     class 회원을_저장하는_핸들러는 {
@@ -18,8 +31,9 @@ class UserControllerTest {
         class 유효한_회원_파라미터인_경우 {
 
             @Test
-            void 회원을_저장한다() {
-
+            void 회원을_저장한다() throws Exception {
+                mockMvc.perform(post("/users", "{name:\"test\", email:\"test@naver.com\", password:\"1234\"}"))
+                        .andExpect(status().isOk());
             }
         }
 
