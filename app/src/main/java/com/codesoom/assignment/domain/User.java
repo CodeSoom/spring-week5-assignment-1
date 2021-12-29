d1ac6168336ecae150fe841789f4d4e1e9643905
Kwakcena <rhkrgudwh@gmail.com>
2021-12-29 09:41:52 +0900

    User model을 만들어라
    회원에 대한 model entity를 만들었습니다

---
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -0,0 +1,49 @@
+package com.codesoom.assignment.domain;
+
+public class User {
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
