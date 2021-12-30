55f8ccbcbb9fd90f4ebfa42da7ce0e74246ca57f
Seungchul Jang <jangsc0000@gmail.com>
2021-12-30 19:19:08 +0900

    [추가] User Not Found에 대한 ExceptionHandler 추가

--- app/src/main/java/com/codesoom/assignment/controllers/NotFoundErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/NotFoundErrorAdvice.java
@@ -1,10 +1,12 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.ProductNotFoundException;
+import com.codesoom.assignment.UserNotFoundException;
 import com.codesoom.assignment.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -15,5 +17,12 @@ public class NotFoundErrorAdvice {
     @ExceptionHandler(ProductNotFoundException.class)
     public ErrorResponse handleProductTaskNotFound() {
         return new ErrorResponse("Product not found");
+    }
+    
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public ErrorResponse handleUserNotFound() {
+        return new ErrorResponse("User not found");
     }
 }
