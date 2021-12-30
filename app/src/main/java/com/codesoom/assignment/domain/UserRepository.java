ae713c4b6f6f1c3975516f8d427972598b3297eb
Kwakcena <rhkrgudwh@gmail.com>
2021-12-31 02:21:31 +0900

    CrudRepository를 상속하라
    UserRepository가 CrudRepository를 상속하지 않아서 서버가 제대로 실행되지 않아
    CI가 깨집니다. 이를 수정하였습니다.

--- app/src/main/java/com/codesoom/assignment/domain/UserRepository.java
+++ app/src/main/java/com/codesoom/assignment/domain/UserRepository.java
@@ -1,8 +1,10 @@
 package com.codesoom.assignment.domain;
 
+import org.springframework.data.repository.CrudRepository;
+
 import java.util.Optional;
 
-public interface UserRepository {
+public interface UserRepository extends CrudRepository<User, Long> {
     User save(User user);
 
     Optional<User> findById(Long id);
