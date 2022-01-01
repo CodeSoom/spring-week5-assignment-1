fb1b69ad25fb35361d2d2447c966abf3c0706775
Seungchul Jang <jangsc0000@gmail.com>
2022-01-02 05:21:59 +0900

    [리팩토링] Setter 테스트 제거

--- app/src/test/java/com/codesoom/assignment/domain/UserTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/UserTest.java
@@ -11,16 +11,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class UserTest {
     @Nested
     @DisplayName("User 빌더는")
-    class Describe_UserBuilder{
+    class Describe_UserBuilder {
         @Test
         @DisplayName("User를 생성한다.")
-        void it_returns_user(){
+        void it_returns_user() {
             User user = User.builder()
-                            .id(1L)
-                            .email("honggd@gmail.com")
-                            .name("홍길동")
-                            .password("password")
-                            .build();
+                    .id(1L)
+                    .email("honggd@gmail.com")
+                    .name("홍길동")
+                    .password("password")
+                    .build();
 
             assertThat(user.getId()).isEqualTo(1L);
             assertThat(user.getEmail()).isEqualTo("honggd@gmail.com");
@@ -28,35 +28,4 @@ public class UserTest {
             assertThat(user.getPassword()).isEqualTo("password");
         }
     }
-
-    @Nested
-    @DisplayName("setter는")
-    class Describe_setter{
-        User user;
-
-        @BeforeEach
-        void prepare(){
-            user = User.builder()
-                    .id(1L)
-                    .email("honggd@gmail.com")
-                    .name("홍길동")
-                    .password("password")
-                    .build();
-        }
-
-        @Test
-        @DisplayName("User의 속성을 변경한다")
-        void it_changes_attribute(){
-            user.setId(2L);
-            user.setEmail("new_honggd@gmail.com");
-            user.setName("new_홍길동");
-            user.setPassword("new_password");
-
-            assertThat(user.getId()).isEqualTo(2L);
-            assertThat(user.getEmail()).isEqualTo("new_honggd@gmail.com");
-            assertThat(user.getName()).isEqualTo("new_홍길동");
-            assertThat(user.getPassword()).isEqualTo("new_password");
-        }
-    }
-
 }
