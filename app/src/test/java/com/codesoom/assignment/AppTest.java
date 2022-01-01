874a8ce4008e6488067d20af6e51fa8025012be5
Kwakcena <rhkrgudwh@gmail.com>
2022-01-01 20:07:17 +0900

    사용하지 않는 코드 제거
    사용하지 않는 코드를 제거합니다

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -1,15 +1,5 @@
 package com.codesoom.assignment;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 class AppTest {
-    @Test
-    void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-        assertEquals(classUnderTest.getGreeting(), "Hello, world!");
-    }
+    // TODO: 언젠가는 다시 돌아온다.
 }
