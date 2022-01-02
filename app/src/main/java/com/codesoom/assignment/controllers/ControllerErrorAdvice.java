45faafeb5a16abe69ef9908f37dbd826c779b316
kyuwon53 <lkw2v@naver.com>
2022-01-02 21:45:22 +0900

    [Add] 이메일 중복 예외를 추가하라

---
+++ app/src/main/java/com/codesoom/assignment/controllers/ControllerErrorAdvice.java
@@ -0,0 +1,35 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.dto.ErrorResponse;
+import com.codesoom.assignment.errors.ProductNotFoundException;
+import com.codesoom.assignment.errors.UserEmailDuplicationException;
+import com.codesoom.assignment.errors.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ControllerErrorAdvice {
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({ProductNotFoundException.class})
+    public ErrorResponse handleProductTaskNotFound() {
+        return new ErrorResponse("Product not found");
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({UserNotFoundException.class})
+    public ErrorResponse handleUserTaskNotFound() {
+        return new ErrorResponse("User not found");
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({UserEmailDuplicationException.class})
+    public ErrorResponse handleUserEmailIsAlreadyExisted() {
+        return new ErrorResponse("User's email address is already existed");
+    }
+}
