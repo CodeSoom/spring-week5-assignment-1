c6950218beaa54eed559428436444b00937196b0
shineoov <shineoov88@gmail.com>
2022-04-19 18:43:12 +0900

    test: 회원 등록 API 실패 테스트

--- app/src/test/java/com/codesoom/assignment/controllers/WebUserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/WebUserControllerTest.java
@@ -2,9 +2,13 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.dto.UserSaveDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -39,11 +43,16 @@ public class WebUserControllerTest {
         @DisplayName("회원 등록에 필요한 데이터로 요청을 한다면")
         class Context_valid {
 
-            UserSaveDto source = UserSaveDto.builder()
-                    .email(TEST_USER_EMAIL)
-                    .name(TEST_USER_NAME)
-                    .password(TEST_USER_PASSWORD)
-                    .build();
+            private UserSaveDto source;
+
+            @BeforeEach
+            void setUp() {
+                source = UserSaveDto.builder()
+                        .email(TEST_USER_EMAIL)
+                        .name(TEST_USER_NAME)
+                        .password(TEST_USER_PASSWORD)
+                        .build();
+            }
 
             @Test
             @DisplayName("회원을 등록하고, 등록 정보를 응답한다. [201]")
@@ -57,6 +66,88 @@ public class WebUserControllerTest {
                         .andExpect(jsonPath("id").exists())
                         .andExpect(jsonPath("email").value(TEST_USER_EMAIL))
                         .andExpect(jsonPath("name").value(TEST_USER_NAME));
+            }
+        }
+
+        @Nested
+        @DisplayName("유효하지 않은 이메일을 요청 한다면")
+        class Context_invalidEmail {
+
+            private UserSaveDto source;
+
+            void setUp(String givenEmail) {
+                source = UserSaveDto.builder()
+                        .email(givenEmail)
+                        .name(TEST_USER_NAME)
+                        .password(TEST_USER_PASSWORD)
+                        .build();
+            }
+
+            @ParameterizedTest(name = "(\"{0}\") - Bad Request 를 응답한다. [400]")
+            @ValueSource(strings = {"testnaver.com", "testemail"})
+            @NullAndEmptySource
+            void it_response_400(String invalidEmail) throws Exception {
+
+                setUp(invalidEmail);
+
+                mockMvc.perform(post("/users")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(source)))
+                        .andExpect(status().isBadRequest());
+            }
+        }
+
+        @Nested
+        @DisplayName("유효하지 않은 이름으로 요청 한다면")
+        class Context_invalidName {
+
+            private UserSaveDto source;
+
+            void setUp(String name) {
+                source = UserSaveDto.builder()
+                        .email(TEST_USER_EMAIL)
+                        .name(name)
+                        .password(TEST_USER_PASSWORD)
+                        .build();
+            }
+
+            @ParameterizedTest(name = "(\"{0}\") - Bad Request 를 응답한다. [400]")
+            @NullAndEmptySource
+            void it_response_400(String givenName) throws Exception {
+
+                setUp(givenName);
+
+                mockMvc.perform(post("/users")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(source)))
+                        .andExpect(status().isBadRequest());
+            }
+        }
+
+        @Nested
+        @DisplayName("유효하지 않은 비밀번호로 요청 한다면")
+        class Context_invalidPassword {
+
+            private UserSaveDto source;
+
+            void setUp(String givenPassword) {
+                source = UserSaveDto.builder()
+                        .email(TEST_USER_EMAIL)
+                        .name(TEST_USER_NAME)
+                        .password(givenPassword)
+                        .build();
+            }
+
+            @ParameterizedTest(name = "(\"{0}\") - Bad Request 를 응답한다. [400]")
+            @NullAndEmptySource
+            void it_response_400(String givenPassword) throws Exception {
+
+                setUp(givenPassword);
+
+                mockMvc.perform(post("/users")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(source)))
+                        .andExpect(status().isBadRequest());
             }
         }
     }
