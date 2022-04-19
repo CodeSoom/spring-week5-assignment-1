1048a20c338fab42e383418eff7a740c0161949d
subin606 <gaemilee606@gmail.com>
2022-04-19 17:24:34 +0900

    feat: User 엔티티, 리포지토리 작성
    사용자를 관리하기 위한 User 도메인을 작성합니다.

---
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -0,0 +1,37 @@
+package com.codesoom.assignment.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    protected User() {
+    }
+
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+}
---
+++ app/src/main/java/com/codesoom/assignment/domain/UserRepository.java
@@ -0,0 +1,12 @@
+package com.codesoom.assignment.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Override
+    List<User> findAll();
+
+}
