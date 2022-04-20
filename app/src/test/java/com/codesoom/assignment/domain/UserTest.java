426b47cd7f30a30c26697f95cf5409654d609f78
Jeon Gyedong <jeongyedong@gmail.com>
2022-04-21 05:44:35 +0900

    TST: User 테스트 추가

---
+++ app/src/test/java/com/codesoom/assignment/domain/UserTest.java
@@ -0,0 +1,37 @@
+package com.codesoom.assignment.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserTest {
+    @Test
+    void creationWithBuilder() {
+        User user = User.builder()
+                .id(1L)
+                .name("johndoe")
+                .email("johndoe@gmail.com")
+                .password("verysecret")
+                .build();
+
+        assertThat(user.getId()).isEqualTo(1L);
+        assertThat(user.getName()).isEqualTo("johndoe");
+        assertThat(user.getEmail()).isEqualTo("johndoe@gmail.com");
+        assertThat(user.getPassword()).isEqualTo("verysecret");
+    }
+
+    @Test
+    void change() {
+        User user = User.builder()
+                .id(1L)
+                .name("johndoe")
+                .email("johndoe@gmail.com")
+                .password("verysecret")
+                .build();
+
+        user.change("janedoe", "janedoe@gmail.com", "verysecret!");
+        assertThat(user.getName()).isEqualTo("janedoe");
+        assertThat(user.getEmail()).isEqualTo("janedoe@gmail.com");
+        assertThat(user.getPassword()).isEqualTo("verysecret!");
+    }
+}
