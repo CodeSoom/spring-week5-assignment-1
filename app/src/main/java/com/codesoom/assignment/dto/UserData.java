2bf22b3cf0ff31e243233905ad7c7a2efa23046d
davidyang2149 <davidyang2149@gmail.com>
2022-04-20 21:02:29 +0900

    UserDTO 클래스를 생성합니다

---
+++ app/src/main/java/com/codesoom/assignment/dto/UserData.java
@@ -0,0 +1,29 @@
+package com.codesoom.assignment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserData {
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+}
