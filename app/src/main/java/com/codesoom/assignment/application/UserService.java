45245eeb79f6edf260f3952a9a7fea3cde6f16e9
shineoov <shineoov88@gmail.com>
2022-04-20 17:49:36 +0900

    refactor: 회원객체 생성 위치 변경 - UserRepository save() 오버로딩

--- app/src/main/java/com/codesoom/assignment/application/UserService.java
+++ app/src/main/java/com/codesoom/assignment/application/UserService.java
@@ -4,7 +4,6 @@ import com.codesoom.assignment.domain.users.User;
 import com.codesoom.assignment.domain.users.UserRepository;
 import com.codesoom.assignment.domain.users.UserSaveRequest;
 import com.codesoom.assignment.domain.users.UserUpdateRequest;
-import com.codesoom.assignment.dto.UserUpdateDto;
 import com.codesoom.assignment.exception.UserNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,14 +23,7 @@ public class UserService {
      * @param saveRequest 회원 등록에 필요한 데이터
      */
     public User save(final UserSaveRequest saveRequest) {
-
-        final User user = User.builder()
-                .email(saveRequest.getEmail())
-                .name(saveRequest.getName())
-                .password(saveRequest.getPassword())
-                .build();
-
-        return userRepository.save(user);
+        return userRepository.save(saveRequest);
     }
 
     /**
@@ -47,8 +39,8 @@ public class UserService {
     /**
      * 회원을 수정하고 리턴합니다.
      *
-     * @param user         수정 대상 회원
-     * @param updateSource 수정할 회원 데이터
+     * @param user          수정 대상 회원
+     * @param updateRequest 수정할 회원 데이터
      */
     public User updateUser(final User user, final UserUpdateRequest updateRequest) {
         return user.replace(updateRequest);
