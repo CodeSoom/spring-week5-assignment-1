ce52251e68e8262aa7889d008b89e619403913eb
Lee Byoung Seong <dev.bslee@gmail.com>
2022-04-21 19:06:41 +0900

    refactor:application layer내 도메인 별로 분리
    각 도메인 마다 패키지 생성 후 분리

--- app/src/main/java/com/codesoom/assignment/application/product/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/product/ProductService.java
@@ -1,4 +1,4 @@
-package com.codesoom.assignment.application;
+package com.codesoom.assignment.application.product;
 
 import com.codesoom.assignment.ProductNotFoundException;
 import com.codesoom.assignment.domain.product.Product;
