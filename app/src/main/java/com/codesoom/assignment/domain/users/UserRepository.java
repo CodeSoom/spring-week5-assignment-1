c4a2a2778c375f5e75c2ac92c94fdfc58c0ebb8f
shineoov <shineoov88@gmail.com>
2022-04-21 18:18:28 +0900

    UserRepository 주석 개선 - @return 추가

--- app/src/main/java/com/codesoom/assignment/domain/users/UserRepository.java
+++ app/src/main/java/com/codesoom/assignment/domain/users/UserRepository.java
@@ -7,6 +7,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     /**
      * 회원을 저장하고 리턴합니다.
      * @param saveRequest 회원 등록에 필요한 데이터
+     * @return 회원 엔티티
      */
     default User save(UserSaveRequest saveRequest) {
 
