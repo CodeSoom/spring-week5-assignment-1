8515b08c4b32fbebd72c74b5e7ccf9d393105785
Jeon Gyedong <jeongyedong@gmail.com>
2022-04-22 21:40:07 +0900

    UserNotFoundException에 ResponseStatus 추가

--- app/src/main/java/com/codesoom/assignment/UserNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package com.codesoom.assignment;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User Not Found")
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Long id) {
         super("User not found: " + id);
