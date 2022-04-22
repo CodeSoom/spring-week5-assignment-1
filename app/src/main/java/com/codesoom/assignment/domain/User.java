f2455b1265aadabe6b9fe34f6c07d091abf1f6bc
n00nietzsche <n00nietzsche@gmail.com>
2022-04-22 21:31:20 +0900

    User 의 Setter 를 PRIVATE 레벨로 변경

--- app/src/main/java/com/codesoom/assignment/domain/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -7,7 +7,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-@Getter @Setter
+// 라이브러리 사용을 위해 Setter 를 무작정 공개하는 것은 좋지 않은 전략이기 때문에 엑세스 레벨을 Private 으로 둔다.
+// 라이브러리는 reflection API 로 동작하기 때문에, 접근 권한이 Public 일 필요가 없다.
+@Getter @Setter(AccessLevel.PRIVATE)
 @NoArgsConstructor @AllArgsConstructor @Builder
 public class User {
     @Id
