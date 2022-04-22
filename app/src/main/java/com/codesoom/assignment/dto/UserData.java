cb2442b4bb8d5aefc345fb943223fb7c78212c82
Jeon Gyedong <jeongyedong@gmail.com>
2022-04-22 20:46:11 +0900

    UserData 추가

---
+++ app/src/main/java/com/codesoom/assignment/dto/UserData.java
@@ -0,0 +1,24 @@
+package com.codesoom.assignment.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserData {
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String email;
+
+    @NotNull
+    private String password;
+}
