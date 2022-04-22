38d5606051f38e99e92df26a60e947606075660d
n00nietzsche <n00nietzsche@gmail.com>
2022-04-22 21:01:12 +0900

    UserService 유저 생성 테스트 통과 코드 작성

---
+++ app/src/main/java/com/codesoom/assignment/application/UserService.java
@@ -0,0 +1,18 @@
+package com.codesoom.assignment.application;
+
+import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.domain.UserRepository;
+import com.codesoom.assignment.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+}
