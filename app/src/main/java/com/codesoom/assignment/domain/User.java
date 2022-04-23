3e0deb94b6938c93d375e0c5cfbc6ca833118e0d
SUBIN606 <gaemilee606@gmail.com>
2022-04-23 20:31:14 +0900

    User클래스의 의미 없는 코드를 제거하라

--- app/src/main/java/com/codesoom/assignment/domain/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -28,7 +28,6 @@ public class User {
     }
 
     public User(String name, String email, String password) {
-        this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
