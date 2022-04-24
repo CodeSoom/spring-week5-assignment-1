fdf3ffa9ed9b6c8a0af4ebaaa96a7312fda23fc8
n00nietzsche <n00nietzsche@gmail.com>
2022-04-24 20:26:28 +0900

    userService get() 메서드 테스트 추가

--- app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
@@ -2,7 +2,6 @@ package com.codesoom.assignment.application;
 
 import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -10,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @DisplayName("UserService 는")
@@ -19,18 +17,24 @@ class UserServiceTest {
     UserService userService;
     @Autowired
     UserRepository userRepository;
-    private final User userWithoutId = new User(null, "김갑생", "gabseng@naver.com", "12341234");
 
-    @BeforeEach
-    void setUp() {
-        userRepository.deleteAll();
-    }
+    private final User userWithoutId = User.builder()
+            .id(null)
+            .name("김갑생")
+            .email("gabseng@naver.com")
+            .password("12341234")
+            .build();
 
     @Nested
     @DisplayName("create() 메서드는")
     class Context_create_method {
+
+        public Context_create_method() {
+            userRepository.deleteAll();
+        }
+
         @Nested
-        @DisplayName("id가 null인 User 객체를 받았을 때")
+        @DisplayName("id가 null 인 User 객체를 받았을 때")
         class Context_with_user {
             @Test
             @DisplayName("리포지토리에 저장하고, User 객체를 리턴한다.")
@@ -39,6 +43,38 @@ class UserServiceTest {
                 assertThat(user).isNotNull();
                 assertThat(user).isInstanceOf(User.class);
                 assertThat(userRepository.count()).isEqualTo(1);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("get() 메서드는")
+    class Context_get_method {
+        @Nested
+        @DisplayName("존재하는 id 를 받았을 때")
+        class Context_valid_id {
+            private final User user;
+
+            // 한번만 수행은 생성자
+            public Context_valid_id() {
+                userRepository.deleteAll();
+                user = userRepository.save(User.builder()
+                        .password("password")
+                        .name("name")
+                        .email("email")
+                        .build());
+            }
+
+            @Test
+            @DisplayName("id 를 가진 User 를 반환한다.")
+            void it_returns_user_having_that_id() {
+                User resultUser = userService.get(user.getId());
+
+                assertThat(resultUser).isNotNull();
+                assertThat(resultUser.getId()).isEqualTo(user.getId());
+                assertThat(resultUser.getEmail()).isEqualTo(user.getEmail());
+                assertThat(resultUser.getName()).isEqualTo(user.getName());
+                assertThat(resultUser.getPassword()).isEqualTo(user.getPassword());
             }
         }
     }
