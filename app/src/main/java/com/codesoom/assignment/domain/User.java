47d04d6700832559739b79f5b24c8d603b0b74a2
n00nietzsche <n00nietzsche@gmail.com>
2022-04-24 20:28:38 +0900

    User Setter 등 접근권한 변경

--- app/src/main/java/com/codesoom/assignment/domain/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.domain;
 
+import com.google.common.base.MoreObjects;
 import lombok.*;
 
 import javax.persistence.Entity;
@@ -10,7 +11,7 @@ import javax.persistence.Id;
 // 라이브러리 사용을 위해 Setter 를 무작정 공개하는 것은 좋지 않은 전략이기 때문에 엑세스 레벨을 Private 으로 둔다.
 // 라이브러리는 reflection API 로 동작하기 때문에, 접근 권한이 Public 일 필요가 없다.
 @Getter @Setter(AccessLevel.PRIVATE)
-@NoArgsConstructor @AllArgsConstructor @Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor(access = AccessLevel.PRIVATE) @Builder
 public class User {
     @Id
     @GeneratedValue
@@ -18,4 +19,14 @@ public class User {
     String name;
     String email;
     String password;
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("id", id)
+                .add("name", name)
+                .add("email", email)
+                .add("password", password)
+                .toString();
+    }
 }
