2bb7370e933b80a880d98e3b605eeb2f8a4419ab
seunglo <devseung9056@gmail.com>
2022-05-30 16:55:05 +0900

    UserData가 올바른 데이터인지 검증하는 애노테이션을 추가해라

--- app/src/main/java/com/codesoom/assignment/dto/UserData.java
+++ app/src/main/java/com/codesoom/assignment/dto/UserData.java
@@ -5,15 +5,21 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserData {
 
+    @NotBlank
     private String email;
 
+    @NotBlank
     private String name;
 
+    @NotBlank
     private String password;
 }
