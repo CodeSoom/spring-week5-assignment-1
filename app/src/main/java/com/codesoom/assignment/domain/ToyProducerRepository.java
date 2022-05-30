3707fda69ecf7473577b680835ec1f9dac25f1d2
MJbae <bmj5208@gmail.com>
2022-05-30 16:06:34 +0900

    feat: ToyProducerRepository 인터페이스 정의

---
+++ app/src/main/java/com/codesoom/assignment/domain/ToyProducerRepository.java
@@ -0,0 +1,18 @@
+package com.codesoom.assignment.domain;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ToyProducerRepository extends CrudRepository<ToyProducer, Long> {
+    List<ToyProducer> findAll();
+
+    Optional<ToyProducer> findById(Long id);
+
+    ToyProducer save(ToyProducer producer);
+
+    void delete(ToyProducer producer);
+}
