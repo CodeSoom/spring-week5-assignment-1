26f4982debcc25ba99ab1749772b110e800fce24
MJbae <bmj5208@gmail.com>
2022-05-30 22:36:58 +0900

    feat: ToyCrudController의 갱신 요청에 대한 책임 구현

--- app/src/main/java/com/codesoom/assignment/controllers/ToyCrudController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ToyCrudController.java
@@ -5,6 +5,7 @@ import com.codesoom.assignment.controllers.dtos.ToyRequestData;
 import com.codesoom.assignment.controllers.dtos.ToyResponseData;
 import com.codesoom.assignment.controllers.interfaces.*;
 import com.codesoom.assignment.domain.Toy;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,7 +15,8 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/products")
 @CrossOrigin(origins = "http://localhost:3000")
-public class ToyCrudController implements ToyDetailController, ToyListController {
+public class ToyCrudController implements ToyCreateController, ToyDetailController,
+        ToyListController, ToyUpdateController, ToyDeleteController {
     private final ToyCrudService service;
 
     public ToyCrudController(ToyCrudService service) {
@@ -34,5 +36,27 @@ public class ToyCrudController implements ToyDetailController, ToyListController
     public ToyResponseData detail(@PathVariable Long id) {
         Toy toy = service.showById(id);
         return new ToyResponseData().toDto(toy);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @Override
+    public ToyResponseData create(@RequestBody ToyRequestData requestDto) {
+        Toy toy = requestDto.toEntity();
+        return new ToyResponseData().toDto(toy);
+    }
+
+    @PatchMapping("{id}")
+    @Override
+    public ToyResponseData update(@PathVariable Long id, @RequestBody ToyRequestData requestDto) {
+        Toy toy = service.update(id, requestDto.toEntity());
+        return new ToyResponseData().toDto(toy);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Override
+    public void delete(@PathVariable Long id) {
+        service.deleteBy(id);
     }
 }
