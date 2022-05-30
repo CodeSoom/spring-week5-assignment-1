956b4b522d8186938e512c49249a5585d23f839f
MJbae <bmj5208@gmail.com>
2022-05-30 16:05:51 +0900

    feat: ToyProducer 클래스 구현

---
+++ app/src/main/java/com/codesoom/assignment/domain/ToyProducer.java
@@ -0,0 +1,18 @@
+package com.codesoom.assignment.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ToyProducer extends Producer{
+    private String name;
+}
