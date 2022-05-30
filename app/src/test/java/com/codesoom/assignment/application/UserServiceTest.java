baf8ace1c50055d4092b3c84fb9a53297a72979c
seunglo <devseung9056@gmail.com>
2022-05-30 19:16:25 +0900

    유저 정보를 변경하는 테스트 케이스를 추가해라

--- app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
@@ -3,7 +3,6 @@ package com.codesoom.assignment.application;
 import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
 import com.codesoom.assignment.dto.UserData;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -51,6 +50,38 @@ class UserServiceTest {
             assertThat(user.getEmail()).isEqualTo(EMAIl);
             assertThat(user.getName()).isEqualTo(NAME);
             assertThat(user.getPassword()).isEqualTo(PASSWORD);
+        }
+    }
+
+    @Nested
+    @DisplayName("updateInfo 메소드는")
+    class Describe_updateInfo {
+
+        private final UserData userDataForUpdate = UserData.builder()
+                .name("배추김치")
+                .password("1234")
+                .build();
+
+        @DataJpaTest
+        @Nested
+        @DisplayName("존재하는 아이디가 주어지면")
+        class Context_existed_id {
+            private Long id;
+
+            @BeforeEach
+            void setUp() {
+                User user = userService.signUp(userData);
+                id = user.getId();
+            }
+
+            @Test
+            @DisplayName("변경된 user를 반환한다.")
+            void it_returns_updated_user() {
+                User user = userService.updateInfo(id, userDataForUpdate);
+
+                assertThat(user.getName()).isEqualTo("배추김치");
+                assertThat(user.getPassword()).isEqualTo("1234");
+            }
         }
     }
 }
