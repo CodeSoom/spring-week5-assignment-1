66677374d007ffe40d813f5524a5b28918f04ade
MJbae <bmj5208@gmail.com>
2022-05-30 22:35:51 +0900

    feat: ErrorResponse 구현

---
+++ app/src/main/java/com/codesoom/assignment/controllers/dtos/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.codesoom.assignment.controllers.dtos;
+
+public class ErrorResponse {
+    private final String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
