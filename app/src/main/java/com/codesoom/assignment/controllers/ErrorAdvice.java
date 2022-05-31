5e751f546cc1ce3020c0d43dce0438f41eed1974
HanJiyoung <jiyounghaan@gmail.com>
2022-05-31 10:40:06 +0900

    ErrorAdvice에 BadRequest에 대한 handler메서드를 추가하라
    한 개의 ErrorAdvice에서 모든 에러를 관리하기 위해 기존 클래스 명인 NotFoundErrorAdvice를 ErrorAdvice로 변경한 후 handleBadRequest 메서드를 추가했습니다.

--- app/src/main/java/com/codesoom/assignment/controllers/ErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ErrorAdvice.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.BadRequestException;
 import com.codesoom.assignment.ProductNotFoundException;
 import com.codesoom.assignment.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
@@ -9,11 +10,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class NotFoundErrorAdvice {
-    @ResponseBody
+@ResponseBody
+public class ErrorAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ProductNotFoundException.class)
-    public ErrorResponse handleProductTaskNotFound() {
-        return new ErrorResponse("Product not found");
+    public ErrorResponse handleProductNotFound() {
+        return new ErrorResponse("Product Not found");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BadRequestException.class)
+    public ErrorResponse handleBadRequest() {
+        return new ErrorResponse("Bad Request");
     }
 }
