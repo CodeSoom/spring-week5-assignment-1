1f78e5af10f981ede6c8d4f7862b43601969a95d
MJbae <bmj5208@gmail.com>
2022-05-31 10:07:40 +0900

    fix: VO에서 불필요한 빌더 어노테이션 제거

--- app/src/main/java/com/codesoom/assignment/domain/ImageDemo.java
+++ app/src/main/java/com/codesoom/assignment/domain/ImageDemo.java
@@ -1,14 +1,12 @@
 package com.codesoom.assignment.domain;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Getter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Embeddable
--- app/src/main/java/com/codesoom/assignment/domain/Won.java
+++ app/src/main/java/com/codesoom/assignment/domain/Won.java
@@ -1,7 +1,6 @@
 package com.codesoom.assignment.domain;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,7 +8,6 @@ import javax.persistence.Embeddable;
 import java.math.BigDecimal;
 
 @Getter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Embeddable
