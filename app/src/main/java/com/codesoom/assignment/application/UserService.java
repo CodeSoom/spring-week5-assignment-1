24481589401e18319e19e713d8d1b85c3b0979e2
minjoon_lee <minjoon1995@naver.com>
2022-05-31 14:14:06 +0900

    refactoring : mapper 객체 지역 변수에서 클래스 변수로 변경

--- app/src/main/java/com/codesoom/assignment/application/UserService.java
+++ app/src/main/java/com/codesoom/assignment/application/UserService.java
@@ -1,8 +1,10 @@
 package com.codesoom.assignment.application;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
-import com.codesoom.assignment.ProductNotFoundException;
 import com.codesoom.assignment.UserNotFoundException;
 import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
@@ -13,24 +15,31 @@ import com.github.dozermapper.core.Mapper;
 @Service
 public class UserService {
 	private UserRepository userRepository;
+	private Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+
 
 	public UserService(UserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
 
 	public UserDTO.Response createUser(UserDTO.CreateUser source) {
-		Mapper mapper = DozerBeanMapperBuilder.buildDefault();
 		User user = userRepository.save(mapper.map(source, User.class));
 		return mapper.map(user, UserDTO.Response.class);
 	}
 
 	public UserDTO.Response getUser(int id) {
-		Mapper mapper = DozerBeanMapperBuilder.buildDefault();
 		User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(1));
 		return mapper.map(user, UserDTO.Response.class);
 	}
 
 	public void deleteUser(int id) {
 		userRepository.deleteById(id);
+	}
+
+	public List<UserDTO.Response> getUsers() {
+		List<User> users = userRepository.findAll();
+		return users.stream().map(user -> mapper.map(user, UserDTO.Response.class))
+			.collect(Collectors.toList());
+
 	}
 }
