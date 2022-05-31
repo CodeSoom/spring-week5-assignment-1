be1e73daaecbb3151bc9897fc2fe6d7ee5a64ec6
hy43 <hyde0401@gmail.com>
2022-06-01 07:01:13 +0900

    feat : controller에서 전체 조회시 200 ok 반환하도록 구현.

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -1,4 +1,15 @@
 package com.codesoom.assignment.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    @GetMapping
+    public void list() {
+
+    }
 }
