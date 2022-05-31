e73978cbd47f3d6aa4db3d649a96ceb132abdef5
hy43 <hyde0401@gmail.com>
2022-06-01 06:44:47 +0900

    docs : Controller Testcase TODO 정리.

---
+++ app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
@@ -0,0 +1,28 @@
+package com.codesoom.assignment.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("ProductController Web Test")
+class ProductControllerTest {
+
+    // TODO 구현 목록
+    // 1. GET /products
+    //  - 장난감 목록이 비어있는 경우 -> 200 ok
+    //  - 장난감 목록이 있는 경우 -> 200 ok
+    // 2. GET /products/{id}
+    //  - id가 유효한 경우 -> 200 ok
+    //  - id가 유효하지 않은 경우 -> 404 Not Found
+    // 3. POST /products
+    //  - 입력한 정보가 유효한 경우 -> 201 Created
+    // 4. PATCH /products/{id}
+    //  - id가 유효한 경우 -> 200 ok
+    //  - id가 유효하지 않은 경우 -> 404 Not Found
+    // 5. PUT /products/{id}
+    //  - id가 유효한 경우 -> 200 ok
+    //  - id가 유효하지 않은 경우 -> 404 Not Found
+    // 6. DELETE /products/{id}
+    //  - id가 유효한 경우 -> No Content
+    //  - id가 유효하지 않은 경우 -> 404 Not Found
+}
