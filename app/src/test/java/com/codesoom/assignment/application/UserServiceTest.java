d713cd66056f4abc87a1ecadcf2d51940f195d8b
minjoon_lee <minjoon1995@naver.com>
2022-05-31 12:52:18 +0900

    feature : getUsers 실패하는 코드 생성

--- app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
@@ -3,6 +3,8 @@ package com.codesoom.assignment.application;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -28,8 +30,13 @@ public class UserServiceTest {
 				.password(source.getPassword())
 				.build();
 		});
-		User user = new User(1,"get name test","get email test","get password test");
-		given(userRepository.findById(1)).willReturn(Optional.of(user));
+		User user1 = new User(1, "get name test", "get email test", "get password test");
+		User user2 = new User(2, "get name test 2", "get email test 2", "get password test 2");
+
+		List<User> users = Arrays.asList(user1, user2);
+
+		given(userRepository.findAll()).willReturn(users);
+		given(userRepository.findById(1)).willReturn(Optional.of(user1));
 	}
 
 	@Test
@@ -39,15 +46,24 @@ public class UserServiceTest {
 		verify(userRepository).save(any(User.class));
 		assertThat(response.getEmail()).isEqualTo("email test");
 	}
+
 	@Test
 	void getUserTest() {
 		UserDTO.Response response = userService.getUser(1);
 		verify(userRepository).findById(1);
 		assertThat(response.getEmail()).isEqualTo("get email test");
 	}
+
 	@Test
 	void deleteUserTest() {
 		userService.deleteUser(1);
 		verify(userRepository).deleteById(1);
+	}
+
+	@Test
+	void getUsersTest() {
+		List<User> users = userService.getUsers();
+		verify(userRepository).findAll();
+		assertThat(users.size()).isEqualTo(2);
 	}
 }
