9a5df16ed508c0f08a475c99e7fc9358511f4775
caoyu-dev <choyumin01@gmail.com>
2022-06-01 02:43:24 +0900

    feat: UserController::createUser 구현

---
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -0,0 +1,23 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.application.UserService;
+import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.dto.UserCreateData;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@RequestBody UserCreateData userCreateData) {
+        return userService.createUser(userCreateData);
+    }
+}
