c115fa66bccb26b89b238b672e53c8a2562bf6f9
caoyu-dev <choyumin01@gmail.com>
2022-06-01 02:39:31 +0900

    feat: User 클래스 구현

---
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -0,0 +1,31 @@
+package com.codesoom.assignment.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+}
