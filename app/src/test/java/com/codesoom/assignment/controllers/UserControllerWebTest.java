e1be8c3f356b996af22506a5034b8b0d96717d25
minjoon_lee <minjoon1995@naver.com>
2022-05-31 15:59:48 +0900

    feature: UserControllerWebTest 의 updateUser 실패하는 테스트 코드 작성

--- app/src/test/java/com/codesoom/assignment/controllers/UserControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/UserControllerWebTest.java
@@ -2,6 +2,7 @@ package com.codesoom.assignment.controllers;
 
 import static org.hamcrest.Matchers.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.eq;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -17,8 +18,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.codesoom.assignment.application.UserService;
 import com.codesoom.assignment.dto.UserDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.dozermapper.core.DozerBeanMapperBuilder;
-import com.github.dozermapper.core.Mapper;
 
 @WebMvcTest(UserController.class)
 public class UserControllerWebTest {
@@ -38,6 +37,14 @@ public class UserControllerWebTest {
 			"create email test", "create password test");
 		given(userService.createUser(any(UserDTO.CreateUser.class)))
 			.willReturn(response);
+
+		UserDTO.UpdateUser updateUser = new UserDTO.UpdateUser("update name test", "update email test",
+			"update password test");
+		given(userService.updateUsers(eq(1), any(UserDTO.UpdateUser.class)))
+			.will(invocation -> {
+				UserDTO.UpdateUser source = invocation.getArgument(0);
+				return new UserDTO.Response(1, source.getName(), source.getEmail(), source.getPassword());
+			});
 
 		//TODO 아래의 코드에서 createUser 의 attribute 가 null 로 뽑히는데 이유가 무엇인지 알아내기.
 
@@ -66,5 +73,18 @@ public class UserControllerWebTest {
 			.andExpect(status().isCreated())
 			.andExpect(content().string(containsString("create email test")));
 		verify(userService).createUser(any(UserDTO.CreateUser.class));
+	}
+
+	@Test
+	void updateUser() throws Exception {
+		mockMvc.perform(
+				put("/users")
+					.accept(MediaType.APPLICATION_JSON)
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(
+						"{\"name\":\"update name test\",\"email\":\"update email test\",\"password\":\"update password test\"}"))
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("update email test")));
+		verify(userService).updateUsers(eq(1), any(UserDTO.UpdateUser.class));
 	}
 }
