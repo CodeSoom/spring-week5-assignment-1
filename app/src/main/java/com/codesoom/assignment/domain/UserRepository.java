a87e578c2da725b5c9c0c422fcc84faea15ae7b5
caoyu-dev <choyumin01@gmail.com>
2022-06-02 00:11:22 +0900

    feat: UserRepository 인터페이스 수정 delete 추가

--- app/src/main/java/com/codesoom/assignment/domain/UserRepository.java
+++ app/src/main/java/com/codesoom/assignment/domain/UserRepository.java
@@ -4,6 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository {
+
     User save(User user);
+
     Optional<User> findById(Long id);
+
+    void delete(Long id);
 }
