b8f359d0352bf43f5ec59297379a4c2ae98ab4f7
HanJiyoung <jiyounghaan@gmail.com>
2022-06-01 16:37:02 +0900

    UserNotFoundException 클래스를 만들고, ErrorAdvice에 추가하라
    - 유효하지 않은 id로 요청 시 UserNotFoundException을 발생시키기 위해 예외 클래스를 만들었습니다.
    - ErrorAdvice에서 해당 예외를 관리할 수 있도록 handleUserNotFound() 메서드를 추가하였습니다.

--- app/src/main/java/com/codesoom/assignment/controllers/ErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ErrorAdvice.java
@@ -2,6 +2,7 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.BadRequestException;
 import com.codesoom.assignment.ProductNotFoundException;
+import com.codesoom.assignment.UserNotFoundException;
 import com.codesoom.assignment.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -22,5 +23,11 @@ public class ErrorAdvice {
     @ExceptionHandler(BadRequestException.class)
     public ErrorResponse handleBadRequest() {
         return new ErrorResponse("Bad Request");
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public ErrorResponse handleUserNotFound() {
+        return new ErrorResponse("User Not found");
     }
 }
