936331f5e5ba2e05be637099b03d99c42482f6d2
caoyu-dev <choyumin01@gmail.com>
2022-06-01 23:25:49 +0900

    feat: UserController::update 구현 및 메서드명 변경 createUser -> create

--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.application.UserService;
 import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.dto.UserCreateData;
+import com.codesoom.assignment.dto.UserUpdateData;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,7 +20,16 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public User createUser(@RequestBody @Valid UserCreateData userCreateData) {
+    public User create(@RequestBody @Valid UserCreateData userCreateData) {
         return userService.createUser(userCreateData);
+    }
+
+    @RequestMapping(value = "/{id}",
+            method = {RequestMethod.PATCH, RequestMethod.PUT})
+    public User update(
+            @PathVariable Long id,
+            @RequestBody @Valid UserUpdateData userUpdateData
+    ) {
+        return userService.updateUser(id, userUpdateData);
     }
 }
