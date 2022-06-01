2abe1b263dc8ae22dd0b125c94e0ef1ad512cce1
hy43 <hyde0401@gmail.com>
2022-06-01 10:32:43 +0900

    feat : GET /products 기능 구현

---
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -0,0 +1,11 @@
+package com.codesoom.assignment.application;
+
+import com.codesoom.assignment.domain.Product;
+
+import java.util.List;
+
+public class ProductService {
+    public List<Product> findProducts() {
+        return null;
+    }
+}
