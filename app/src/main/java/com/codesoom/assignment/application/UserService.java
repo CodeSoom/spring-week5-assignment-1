d916cd065c5dac6b681cdc1f3344727eaff6d7ca
caoyu-dev <choyumin01@gmail.com>
2022-06-01 23:27:15 +0900

    feat: UserService::updateUser 구현

--- app/src/main/java/com/codesoom/assignment/application/UserService.java
+++ app/src/main/java/com/codesoom/assignment/application/UserService.java
@@ -1,13 +1,16 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.UserNotFoundException;
 import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
 import com.codesoom.assignment.dto.UserCreateData;
+import com.codesoom.assignment.dto.UserUpdateData;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private User user;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -21,6 +24,24 @@ public class UserService {
                 .build();
 
         return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, UserUpdateData userUpdateData) {
+        User user = getUser(id);
+
+        user.update(
+                userUpdateData.getName(),
+                userUpdateData.getEmail(),
+                userUpdateData.getPassword()
+        );
+
+        return user;
+    }
+
+    private User getUser(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+        return user;
     }
 
 }
