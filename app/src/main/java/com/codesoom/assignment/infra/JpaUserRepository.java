3f4a642a22f78a7512272347f787867134c53e94
HanJiyoung <jiyounghaan@gmail.com>
2022-06-01 16:54:23 +0900

    JpaUserRepository에서 save()의 매개변수와 반환값을 User로 수정하라
    save() 메서드가 매개변수로 Product를 받고 Product를 반환하고 있었다는 것을 발견하여 수정했습니다.

--- app/src/main/java/com/codesoom/assignment/infra/JpaUserRepository.java
+++ app/src/main/java/com/codesoom/assignment/infra/JpaUserRepository.java
@@ -1,6 +1,5 @@
 package com.codesoom.assignment.infra;
 
-import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
 import org.springframework.data.repository.CrudRepository;
@@ -9,7 +8,7 @@ import java.util.Optional;
 
 public interface JpaUserRepository
         extends UserRepository, CrudRepository<User, Long> {
-    Product save(Product product);
+    User save(User user);
 
     Optional<User> findById(Long id);
 }
