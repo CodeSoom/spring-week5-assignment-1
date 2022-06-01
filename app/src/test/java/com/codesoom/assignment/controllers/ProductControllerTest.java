6dd9b195e1c10c61efb6414544cfdf04afb03ffd
hy43 <hyde0401@gmail.com>
2022-06-01 11:34:58 +0900

    test : GET /product/{id} 유효한 id, 유효하지 않은 id에 대한 테스트 케이스 작성

--- app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.jmx.export.naming.IdentityNamingStrategy;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -29,8 +30,8 @@ class ProductControllerTest {
 
     // TODO 구현 목록
     // 1. GET /products
-    //  - 장난감 목록이 비어있는 경우 -> 200 ok
-    //  - 장난감 목록이 있는 경우 -> 200 ok
+    //  - 장난감 목록이 비어있는 경우 -> 200 ok -> 완료
+    //  - 장난감 목록이 있는 경우 -> 200 ok -> 완료
     // 2. GET /products/{id}
     //  - id가 유효한 경우 -> 200 ok
     //  - id가 유효하지 않은 경우 -> 404 Not Found
@@ -53,6 +54,7 @@ class ProductControllerTest {
     ProductSearchService productSearchService;
 
     private final Long ID = 1L;
+    private final Long INVALID_ID = 1000L;
     private final String NAME = "캣타워";
     private final String MAKER = "(주)애옹이네";
     private final BigDecimal PRICE = BigDecimal.valueOf(100000);
@@ -74,7 +76,7 @@ class ProductControllerTest {
         class when_product_list_is_empty {
 
             @Test
-            @DisplayName("200 ok를 반환한다.")
+            @DisplayName("200 ok로 응답한다.")
             void list_200_ok() throws Exception {
                 mockMvc.perform(get("/products"))
                         .andExpect(status().isOk());
@@ -86,7 +88,7 @@ class ProductControllerTest {
         class when_product_list_is_not_empty {
 
             @Test
-            @DisplayName("200 ok를 반환한다.")
+            @DisplayName("200 ok로 응답한다.")
             void list_200_ok() throws Exception {
                 mockMvc.perform(get("/products"))
                         .andExpect(status().isOk());
@@ -100,6 +102,54 @@ class ProductControllerTest {
                                 .accept(MediaType.APPLICATION_JSON_UTF8)
                         )
                         .andExpect(content().string(containsString("캣타워")));
+            }
+        }
+
+    }
+
+    @Nested
+    @DisplayName("GET /products/{id}")
+    class detail {
+
+        @BeforeEach
+        void setUp() {
+            Product product = new ProductDto(ID, NAME, MAKER, PRICE);
+
+            given(productSearchService.findProduct(ID))
+                    .willReturn(product);
+
+            given(productSearchService.findProduct(INVALID_ID))
+                    .willThrow(ProductNotFoundException(INVALID_ID));
+        }
+
+        @Nested
+        @DisplayName("입력한 id가 유효하다면")
+        class when_id_is_valid {
+
+            @Test
+            @DisplayName("200 ok로 응답한다.")
+            void detail_200_ok() throws Exception {
+                mockMvc.perform(get("/products/" + ID))
+                        .andExpect(status().isOk());
+            }
+
+            @Test
+            @DisplayName("장난감 정보를 반환한다.")
+            void detail() throws Exception {
+                mockMvc.perform(get("/products/" + ID))
+                        .andExpect(content().string(containsString(NAME)));
+            }
+        }
+
+        @Nested
+        @DisplayName("입력한 id가 유효하지 않다면")
+        class when_id_is_invalid {
+
+            @Test
+            @DisplayName("404 Not Found로 응답한다.")
+            void detail_404_not_found() throws Exception {
+                mockMvc.perform(get("/products/" + INVALID_ID))
+                        .andExpect(status().isNotFound());
             }
         }
 
