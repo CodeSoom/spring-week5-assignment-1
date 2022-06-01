15c0b6d010afdbb342fb54c3685238ea3335b1fe
caoyu-dev <choyumin01@gmail.com>
2022-06-01 23:33:06 +0900

    feat: JpaUserRepository 인터페이스 수정 findById 추가

--- app/src/main/java/com/codesoom/assignment/infra/JpaUserRepository.java
+++ app/src/main/java/com/codesoom/assignment/infra/JpaUserRepository.java
@@ -5,8 +5,11 @@ import com.codesoom.assignment.domain.UserRepository;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 @Primary
 public interface JpaUserRepository
         extends UserRepository, CrudRepository<User, Long> {
     User save(User user);
+    Optional<User> findById(Long id);
 }
