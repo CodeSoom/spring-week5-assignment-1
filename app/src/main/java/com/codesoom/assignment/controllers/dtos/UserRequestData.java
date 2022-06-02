40a113899ffc87cf1732a250487ea0a9db295186
MJbae <bmj5208@gmail.com>
2022-06-02 12:37:23 +0900

    feat: UserRequestData 구현

---
+++ app/src/main/java/com/codesoom/assignment/controllers/dtos/UserRequestData.java
@@ -0,0 +1,35 @@
+package com.codesoom.assignment.controllers.dtos;
+
+import com.codesoom.assignment.domain.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequestData {
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+
+    public User toEntity() {
+        return User.builder()
+                .name(name)
+                .email(email)
+                .password(password)
+                .build();
+    }
+
+
+}
