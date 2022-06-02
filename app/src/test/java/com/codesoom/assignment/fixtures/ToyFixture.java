62771293798c737b2dcaee03158c954b854c8ffe
MJbae <bmj5208@gmail.com>
2022-06-02 22:36:34 +0900

    test: ToyFixture 내 producer를 toyproducerfixture로 대체

--- app/src/test/java/com/codesoom/assignment/fixtures/ToyFixture.java
+++ app/src/test/java/com/codesoom/assignment/fixtures/ToyFixture.java
@@ -4,18 +4,15 @@ import com.codesoom.assignment.domain.entities.Toy;
 import com.codesoom.assignment.domain.entities.ToyProducer;
 import com.codesoom.assignment.domain.vos.ImageDemo;
 import com.codesoom.assignment.domain.vos.Won;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
 @Component
 public class ToyFixture {
-    private final Long TOY_PRODUCER_ID = 1L;
-    private final String PRODUCER_NAME = "Test Producer";
-    private final ToyProducer producer = ToyProducer.builder()
-            .id(TOY_PRODUCER_ID)
-            .name(PRODUCER_NAME)
-            .build();
+    @Autowired
+    private ToyProducerFixture producerFixture;
     private final Won money = new Won(new BigDecimal(1000));
     private final ImageDemo demo = new ImageDemo("https://metacode.biz/@test/avatar.jpg");
     private final Long TOY_ID = 1L;
@@ -26,7 +23,7 @@ public class ToyFixture {
                 .id(TOY_ID)
                 .name(PRODUCT_NAME)
                 .price(money)
-                .producer(producer)
+                .producer(producerFixture.toyProducer())
                 .demo(demo)
                 .build();
     }
@@ -36,7 +33,7 @@ public class ToyFixture {
                 .id(TOY_ID)
                 .name(" ")
                 .price(money)
-                .producer(producer)
+                .producer(producerFixture.toyProducer())
                 .demo(demo)
                 .build();
     }
@@ -45,7 +42,7 @@ public class ToyFixture {
         return Toy.builder()
                 .name(PRODUCT_NAME)
                 .price(money)
-                .producer(producer)
+                .producer(producerFixture.toyProducer())
                 .demo(demo)
                 .build();
     }
@@ -54,7 +51,7 @@ public class ToyFixture {
         return Toy.builder()
                 .name(PRODUCT_NAME + "UPDATED")
                 .price(money)
-                .producer(producer)
+                .producer(producerFixture.toyProducer())
                 .demo(demo)
                 .build();
     }
@@ -64,7 +61,7 @@ public class ToyFixture {
                 .id(TOY_ID)
                 .name(PRODUCT_NAME + "UPDATED")
                 .price(money)
-                .producer(producer)
+                .producer(producerFixture.toyProducer())
                 .demo(demo)
                 .build();
     }
