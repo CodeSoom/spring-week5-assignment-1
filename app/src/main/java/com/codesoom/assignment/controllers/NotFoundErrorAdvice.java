2a0f18664cbd135c5232981c543b0f8d50420522
MJbae <bmj5208@gmail.com>
2022-06-02 11:59:21 +0900

    feat: UserNotFoundException 핸들링 Advice 추가

--- app/src/main/java/com/codesoom/assignment/controllers/NotFoundErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/NotFoundErrorAdvice.java
@@ -1,6 +1,7 @@
 package com.codesoom.assignment.controllers;
 
-import com.codesoom.assignment.ProductNotFoundException;
+import com.codesoom.assignment.application.exceptions.ProductNotFoundException;
+import com.codesoom.assignment.application.exceptions.UserNotFoundException;
 import com.codesoom.assignment.controllers.dtos.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,5 +16,12 @@ public class NotFoundErrorAdvice {
     @ExceptionHandler(ProductNotFoundException.class)
     public ErrorResponse handleProductTaskNotFound() {
         return new ErrorResponse("Product not found");
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public ErrorResponse handleUserTaskNotFound() {
+        return new ErrorResponse("User not found");
     }
 }
