0cf9c0e2d12d33ddba568b208760f4c0cdcb9307
caoyu-dev <choyumin01@gmail.com>
2022-06-03 00:23:55 +0900

    refactor: NotFoundException 수정 NotFoundErrorAdvice 클래스에서 @ResponseStatus(HttpStatus.NOT_FOUND) 를 받고 있으므로 제거

--- app/src/main/java/com/codesoom/assignment/UserNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/UserNotFoundException.java
@@ -1,9 +1,5 @@
 package com.codesoom.assignment;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Long id) {
         super("User not found: " + id);
