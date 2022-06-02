db7d5775ee18bae5918304aac18c6654476757e9
MJbae <bmj5208@gmail.com>
2022-06-02 12:38:58 +0900

    feat: UserCrudController 내 create 메소드 구현

--- app/src/main/java/com/codesoom/assignment/controllers/UserCrudController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserCrudController.java
@@ -1,11 +1,14 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.UserCrudService;
+import com.codesoom.assignment.controllers.dtos.UserRequestData;
 import com.codesoom.assignment.controllers.dtos.UserResponseData;
 import com.codesoom.assignment.controllers.interfaces.*;
 import com.codesoom.assignment.domain.entities.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,7 +16,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/users")
 @CrossOrigin(origins = "http://localhost:3000")
-public class UserCrudController implements UserListController, UserDetailController {
+public class UserCrudController implements UserListController, UserDetailController, UserCreateController {
     private final UserCrudService service;
 
     public UserCrudController(UserCrudService service) {
@@ -33,6 +36,14 @@ public class UserCrudController implements UserListController, UserDetailControl
     @Override
     public UserResponseData detail(@PathVariable Long id) {
         User user = service.showById(id);
+        return UserResponseData.from(user);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @Override
+    public UserResponseData create(@RequestBody @Valid UserRequestData requestData) {
+        User user = service.create(requestData.toEntity());
         return UserResponseData.from(user);
     }
 }
