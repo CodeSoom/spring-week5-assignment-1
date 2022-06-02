1defcf91e1e9d8731c07151d42a71840d9f33cd7
HanJiyoung <jiyounghaan@gmail.com>
2022-06-03 01:44:22 +0900

    DTO의 toString() 메서드에 대한 테스트 코드를 작성하라

---
+++ app/src/test/java/com/codesoom/assignment/dto/UserDataTest.java
@@ -0,0 +1,24 @@
+package com.codesoom.assignment.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserDataTest {
+
+    @Test
+    void testToString() {
+        UserData userData = UserData.builder()
+                .name("김철수")
+                .email("kim@gmail.com")
+                .password("1111")
+                .build();
+
+        assertThat(userData.toString())
+                .isEqualTo("UserData{" +
+                        "name='" + userData.getName() + '\'' +
+                        ", email='" + userData.getEmail() + '\'' +
+                        ", password='" + userData.getPassword() + '\'' +
+                        '}');
+    }
+}
