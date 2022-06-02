ba942f6573ba0dbea7fb4e02f53a07b5d3f201e9
MJbae <bmj5208@gmail.com>
2022-06-02 12:38:16 +0900

    feat: UserCrudService 내 create 메소드 구현

--- app/src/main/java/com/codesoom/assignment/application/UserCrudService.java
+++ app/src/main/java/com/codesoom/assignment/application/UserCrudService.java
@@ -3,13 +3,14 @@ package com.codesoom.assignment.application;
 import com.codesoom.assignment.application.exceptions.UserNotFoundException;
 import com.codesoom.assignment.application.interfaces.*;
 import com.codesoom.assignment.domain.UserRepository;
+import com.codesoom.assignment.domain.entities.Toy;
 import com.codesoom.assignment.domain.entities.User;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class UserCrudService implements UserShowService {
+public class UserCrudService implements UserShowService, UserCreateService {
     private final UserRepository repository;
 
     public UserCrudService(UserRepository repository) {
@@ -28,6 +29,15 @@ public class UserCrudService implements UserShowService {
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
 
+    @Override
+    public User create(User user) {
+        User userSaving = User.builder()
+                .name(user.getName())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .build();
+        return repository.save(userSaving);
+    }
 }
 
 
