fc64c26031488a2b36cf588750bbd3ab55101763
HanJiyoung <jiyounghaan@gmail.com>
2022-06-05 02:27:45 +0900

    App에 대한 테스트 코드를 작성하라

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -2,8 +2,17 @@ package com.codesoom.assignment;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class AppTest {
+    @Test
+    void main() {
+        App.main(new String[] {});
+    }
+
+    @Test
+    void appHasAGreeting() {
+        App app = new App();
+        assertThat(app.getGreeting()).isEqualTo("Hello, world!");
+    }
 }
