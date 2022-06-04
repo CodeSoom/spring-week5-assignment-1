5b20e6ca1c16d2dfed83a42ee16070d94e15d867
HanJiyoung <jiyounghaan@gmail.com>
2022-06-05 02:12:54 +0900

    ErrorAdvice에서 handleBadRequest() 메서드를 제거하라
    BadRequestException은 컨트롤러에서 직접 던져주는 예외가 아니므로 제거했습니다.

--- app/src/main/java/com/codesoom/assignment/controllers/ErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ErrorAdvice.java
@@ -1,14 +1,10 @@
 package com.codesoom.assignment.controllers;
 
-import com.codesoom.assignment.BadRequestException;
 import com.codesoom.assignment.ProductNotFoundException;
 import com.codesoom.assignment.UserNotFoundException;
 import com.codesoom.assignment.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 @ControllerAdvice
 @ResponseBody
@@ -17,12 +13,6 @@ public class ErrorAdvice {
     @ExceptionHandler(ProductNotFoundException.class)
     public ErrorResponse handleProductNotFound() {
         return new ErrorResponse("Product Not found");
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(BadRequestException.class)
-    public ErrorResponse handleBadRequest() {
-        return new ErrorResponse("Bad Request");
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
