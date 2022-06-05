b2cabaa2cffb84203f298837e9a0b52a444e75d6
minjoon_lee <minjoon1995@naver.com>
2022-06-05 14:05:56 +0900

    refactoring : UserServiceInterface 를 주입 받도록 수정

--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -16,44 +16,45 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codesoom.assignment.application.UserServiceImpl;
+import com.codesoom.assignment.application.UserServiceInterface;
 import com.codesoom.assignment.dto.UserDTO;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-	private final UserServiceImpl userServiceImpl;
+	private final UserServiceInterface userService;
 
-	public UserController(UserServiceImpl userServiceImpl) {
-		this.userServiceImpl = userServiceImpl;
+	public UserController(UserServiceImpl userService) {
+		this.userService = userService;
 	}
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public UserDTO.Response createUser(@Valid @RequestBody UserDTO.CreateUser createUser) {
-		return userServiceImpl.createUser(createUser);
+		return userService.createUser(createUser);
 	}
 
 	@PatchMapping("/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public UserDTO.Response updateUser(@PathVariable("id") int id, @Valid @RequestBody UserDTO.UpdateUser updateUser) {
-		return userServiceImpl.updateUsers(id, updateUser);
+		return userService.updateUsers(id, updateUser);
 	}
 
 	@GetMapping("/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public UserDTO.Response getUser(@PathVariable("id") int id) {
-		return userServiceImpl.getUser(id);
+		return userService.getUser(id);
 	}
 
 	@GetMapping()
 	@ResponseStatus(HttpStatus.OK)
 	public List<UserDTO.Response> getUsers() {
-		return userServiceImpl.getUsers();
+		return userService.getUsers();
 	}
 
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteUser(@PathVariable("id") int id) {
-		userServiceImpl.deleteUser(id);
+		userService.deleteUser(id);
 	}
 }
