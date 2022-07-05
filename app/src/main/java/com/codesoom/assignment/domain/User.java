49f8853ea2794bbc6e4bd48a27dd88bcc2019075
unajun <bjyoon513@gmail.com>
2022-07-06 01:47:50 +0900

    feat: add lombok annotation to User domain for constructor and getter

--- app/src/main/java/com/codesoom/assignment/domain/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -1,9 +1,20 @@
 package com.codesoom.assignment.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue
@@ -19,9 +30,9 @@ public class User {
     private String password;
 
 
-    public User update(final User source){
+    public void update(final User source) {
         email = source.email;
         name = source.name;
         password = source.password;
-    };
+    }
 }
