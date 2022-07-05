10be3801c3b11ac5628f22400c9fffc2f66d3c3f
Noh Jinu <yes.jinu@gmail.com>
2022-07-05 21:07:17 +0900

    test: add test validate UserService::register()

--- app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/UserServiceTest.java
@@ -1,21 +1,22 @@
 package com.codesoom.assignment.application;
 
 import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.infra.JpaUserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@SpringBootTest
 @DisplayName("UserService 클래스")
 public class UserServiceTest {
+    @Autowired
     private UserService service;
 
-    @BeforeEach
-    void setUp() {
-        this.service = new UserService();
-    }
 
     @Nested
     @DisplayName("register 메소드는 ")
@@ -31,6 +32,9 @@ public class UserServiceTest {
                 User user = service.register("쥐돌이", "mouse@gmail.com", "1234");
 
                 assertThat(user).isNotNull();
+                assertThat(user.getName()).isEqualTo("쥐돌이");
+                assertThat(user.getEmail()).isEqualTo("mouse@gmail.com");
+                assertThat(user.getPassword()).isEqualTo("1234");
             }
         }
     }
