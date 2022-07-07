9a1f13d8526e982a90c8bfa686f4ba199b07c30f
unajun <bjyoon513@gmail.com>
2022-07-08 01:54:26 +0900

    chore: add sql query for creating schema

---
+++ app/src/main/resources/schema-h2.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+     `id` INT(11) NOT NULL AUTO_INCREMENT,
+     'name' VARCHAR(20) NOT NULL,
+     `email` VARCHAR(50) NOT NULL,
+     password VARCHAR(20) NOT NULL,
+     PRIMARY KEY (`id`)
+);
