3a6c08205e702ab5c30d3bde81399b9dbfa634d3
Noh Jinu <yes.jinu@gmail.com>
2022-07-08 18:23:13 +0900

    docs: 주석을 간단하게 변경함

--- app/src/main/java/com/codesoom/assignment/services/user/UserRegisterService.java
+++ app/src/main/java/com/codesoom/assignment/services/user/UserRegisterService.java
@@ -14,9 +14,9 @@ public class UserRegisterService {
 
     /**
      * 사용자를 생성합니다.
-     * @param name 사용자의 새로운 이름
-     * @param email 사용자의 새로운 이메일
-     * @param password 사용자의 새로운 비밀번호
+     * @param name 새로운 이름
+     * @param email 새로운 이메일
+     * @param password 새로운 비밀번호
      * @return 새롭게 생성된 사용자
      */
     public User execute(String name, String email, String password) {
