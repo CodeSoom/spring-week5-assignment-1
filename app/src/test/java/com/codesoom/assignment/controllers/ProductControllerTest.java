8a361b6b472c894ab3c42f260ac88666ba0ba9ab
BEOMKING <qjawlsqjacks@naver.com>
2022-08-01 21:50:46 +0900

    상속을 활용해 Normal input과 결과값의 관계를 표현

--- app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
@@ -15,9 +15,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,24 +23,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @DisplayName("ProductController 클래스의")
 public class ProductControllerTest {
-    public static final String NORMAL_NAME = "고영희";
-    public static final String NORMAL_MAKER = "코드숨";
-    public static final int NORMAL_PRICE = 2200000;
-    public static final String NORMAL_URL = "www.picture.com";
-
     @Autowired
     private MockMvc mockMvc;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
-
-    private Map<String, Object> normalInput() {
-        HashMap<String, Object> input = new HashMap<>();
-        input.put("name", NORMAL_NAME);
-        input.put("maker", NORMAL_MAKER);
-        input.put("price", NORMAL_PRICE);
-        input.put("imageUrl", NORMAL_URL);
-        return input;
-    }
 
     private ResultActions create(Map<String, Object> input) throws Exception {
         return mockMvc.perform(post("/products")
@@ -50,33 +34,51 @@ public class ProductControllerTest {
                 .content(objectMapper.writeValueAsString(input)));
     }
 
+    abstract class Normal {
+        public static final String NAME = "고영희";
+        public static final String MAKER = "코드숨";
+        public static final int PRICE = 2200000;
+        public static final String URL = "www.picture.com";
+
+        Map<String, Object> input() {
+            HashMap<String, Object> input = new HashMap<>();
+
+            input.put("name", NAME);
+            input.put("maker", MAKER);
+            input.put("price", PRICE);
+            input.put("imageUrl", URL);
+
+            return input;
+        }
+    }
+
     @Nested
     @DisplayName("/products 요청은")
     class Describe_create {
         @Nested
         @DisplayName("상품 정보가 주어지면")
-        class Context_with_productData {
+        class Context_with_productData extends Normal {
             @Test
             @DisplayName("상품을 리턴한다")
             void It_returns_product() throws Exception {
-                create(normalInput())
-                        .andExpect(jsonPath("$.name").value(NORMAL_NAME))
-                        .andExpect(jsonPath("$.maker").value(NORMAL_MAKER))
-                        .andExpect(jsonPath("$.price").value(NORMAL_PRICE))
-                        .andExpect(jsonPath("$.imageUrl").value(NORMAL_URL))
+                create(input())
+                        .andExpect(jsonPath("$.name").value(NAME))
+                        .andExpect(jsonPath("$.maker").value(MAKER))
+                        .andExpect(jsonPath("$.price").value(PRICE))
+                        .andExpect(jsonPath("$.imageUrl").value(URL))
                         .andExpect(status().isCreated());
             }
         }
 
         @Nested
         @DisplayName("상품 정보의 이름이 주어지지 않았다면")
-        class Context_without_Name_Maker_Price {
+        class Context_without_Name_Maker_Price extends Normal {
             Map<String, Object> prepare() {
                 Map<String, Object> inValidInput = new HashMap<>();
                 inValidInput.put("name", "");
-                inValidInput.put("maker", NORMAL_MAKER);
-                inValidInput.put("price", NORMAL_PRICE);
-                inValidInput.put("imageUrl", NORMAL_URL);
+                inValidInput.put("maker", MAKER);
+                inValidInput.put("price", PRICE);
+                inValidInput.put("imageUrl", URL);
 
                 return inValidInput;
             }
