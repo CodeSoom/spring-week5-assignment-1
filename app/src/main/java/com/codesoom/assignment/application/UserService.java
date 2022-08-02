5d0a8be89339d115324bb1c62b3c7a5b47536145
eunice0035 <eunice7357@gmail.com>
2022-08-03 01:04:55 +0900

    UserService create 기능 추가

--- app/src/main/java/com/codesoom/assignment/application/UserService.java
+++ app/src/main/java/com/codesoom/assignment/application/UserService.java
@@ -1,9 +1,20 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.domain.Product;
+import com.codesoom.assignment.domain.User;
+import com.codesoom.assignment.domain.UserRepository;
+import com.codesoom.assignment.dto.ProductData;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
 }
--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
     }
 
     @PostMapping
-    public User createUser(@RequestBody User user){
+    public User create(@RequestBody User user){
         return user;
     }
 
