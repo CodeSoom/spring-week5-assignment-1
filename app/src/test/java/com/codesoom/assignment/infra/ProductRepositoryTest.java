a82285407926ed3cf6ae068eb04e705a05f12012
BEOMKING <qjawlsqjacks@naver.com>
2022-08-02 22:17:33 +0900

    상품 Repository 저장 메서드 테스트

---
+++ app/src/test/java/com/codesoom/assignment/infra/ProductRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.codesoom.assignment.infra;
+
+import com.codesoom.assignment.domain.Product;
+import com.codesoom.assignment.domain.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+@DisplayName("ProductRepository 인터페이스의")
+public class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Nested
+    @DisplayName("save 메서드는")
+    class Describe_save {
+        @Nested
+        @DisplayName("상품이 주어지면")
+        class Context_with_product {
+            Product givenProduct = new Product("캣타워", "코드숨", 12000, "url");
+
+            Product expectProduct(Long id) {
+                return new Product(id, "캣타워", "코드숨", 12000, "url");
+            }
+
+            @Test
+            @DisplayName("상품을 저장하고 리턴한다")
+            void It_returns_productAndSave() {
+                Product resultProduct = productRepository.save(givenProduct);
+
+                assertThat(resultProduct)
+                        .isEqualTo(expectProduct(resultProduct.getId()));
+            }
+        }
+    }
+}
