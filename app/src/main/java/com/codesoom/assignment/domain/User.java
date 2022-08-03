fc74f3acca86b6da9f1c7469cae35a2a0d149cf8
Ernest Hong <hsh3592@gmail.com>
2022-08-03 22:23:28 +0900

    fix: User Entity 지정

--- app/src/main/java/com/codesoom/assignment/domain/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -6,12 +6,19 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
 public class User {
+    @Id
+    @GeneratedValue
     private Long id;
     private String name;
     private String email;
--- app/src/main/java/com/codesoom/assignment/infra/JpaUserRepository.java
+++ app/src/main/java/com/codesoom/assignment/infra/JpaUserRepository.java
@@ -4,5 +4,5 @@ import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface JpaUserRepository extends UserRepository, CrudRepository<Long, User> {
+public interface JpaUserRepository extends UserRepository, CrudRepository<User, Long> {
 }
