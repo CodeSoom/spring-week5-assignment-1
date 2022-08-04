fded5a437b4bafbdf2aa567ee2e1be9f7f47e813
박범진 <qjawlsqjacks@fasoo.com>
2022-08-05 08:19:41 +0900

    AppTest에서 애플리케이션이 정상 작동을 확인하는 테스트 작성

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -1,14 +1,18 @@
 package com.codesoom.assignment;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@SpringBootTest
 class AppTest {
     @Test
-    void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertEquals(classUnderTest.getGreeting(), "안녕하세요! 잘 부탁드립니다.");
+    @DisplayName("애플리케이션은 정상적으로 작동한다")
+    void appWorksFine() {
+        App app = new App();
+
+       assertNotNull(app.getGreeting());
     }
 }
