2d9496f851d2af27e6b0bdde17b912ea6f5f1da9
BEOMKING <qjawlsqjacks@naver.com>
2022-08-05 23:18:13 +0900

    스프링부트 정상 동작 테스트 리팩터링

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -2,17 +2,19 @@ package com.codesoom.assignment;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class AppTest {
+    @Autowired
+    private App app;
+
     @Test
     @DisplayName("애플리케이션은 정상적으로 작동한다")
     void appWorksFine() {
-        App app = new App();
-
-       assertNotNull(app.getGreeting());
+       assertNotNull(app);
     }
 }
