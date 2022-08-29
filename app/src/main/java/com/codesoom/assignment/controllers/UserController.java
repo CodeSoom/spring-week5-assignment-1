e92a0a914d1cff56b1153e92520b630f78497773
정현준 <jeongdalma@gmail.com>
2022-08-29 18:05:19 +0900

    UserController @RequestBody 추가

--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -2,15 +2,18 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.UserService;
 import com.codesoom.assignment.domain.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -27,12 +30,12 @@ public class UserController {
     }
 
     @PostMapping
-    public User create(User user){
+    public User create(@RequestBody User user){
         return service.save(user);
     }
 
     @PostMapping("{id}")
-    public User update(@PathVariable Long id , User user){
+    public User update(@PathVariable Long id , @RequestBody User user){
         return service.update(id , user);
     }
 
