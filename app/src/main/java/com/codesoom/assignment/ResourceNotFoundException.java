33857700fbff07354603f52706505d97b0ab6c82
정현준 <jeongdalma@gmail.com>
2022-08-30 17:56:19 +0900

    예외 메세지 수정

--- app/src/main/java/com/codesoom/assignment/ResourceNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/ResourceNotFoundException.java
@@ -2,6 +2,6 @@ package com.codesoom.assignment;
 
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(Long id) {
-        super("Product not found: " + id);
+        super("식별자에 해당하는 자원이 존재하지 않습니다. : " + id);
     }
 }
