b87be63e501e38e3476119485e09b6a8e6945919
luckycw87 <blissful.pcw@gmail.com>
2022-10-10 21:19:40 +0900

    Fix: `@Repository` 애너테이션 추가

--- app/src/main/java/com/codesoom/assignment/member/infrastructure/JpaMemberRepository.java
+++ app/src/main/java/com/codesoom/assignment/member/infrastructure/JpaMemberRepository.java
@@ -3,6 +3,8 @@ package com.codesoom.assignment.member.infrastructure;
 import com.codesoom.assignment.member.domain.Member;
 import com.codesoom.assignment.member.domain.MemberRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JpaMemberRepository extends MemberRepository, JpaRepository<Member, Long> {
 }
