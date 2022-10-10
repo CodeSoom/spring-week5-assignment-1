9800e7167c01da40dff45e8bb4c8a16ddb2ff6de
luckycw87 <blissful.pcw@gmail.com>
2022-10-10 21:42:03 +0900

    Add: MemberCommand Dto 추가

---
+++ app/src/main/java/com/codesoom/assignment/member/application/MemberCommand.java
@@ -0,0 +1,50 @@
+package com.codesoom.assignment.member.application;
+
+import com.codesoom.assignment.member.domain.Member;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+public class MemberCommand {
+    @Getter
+    @Builder
+    @ToString
+    public static class Register {
+        private final String name;
+
+        private final String password;
+
+        private final String email;
+
+        public Member toEntity() {
+            return Member.builder()
+                    .name(name)
+                    .password(password)
+                    .email(email)
+                    .build();
+        }
+
+    }
+
+    @Getter
+    @Builder
+    @ToString
+    public static class UpdateReq {
+        private final Long id;
+
+        private final String name;
+
+        private final String password;
+
+        private final String email;
+
+        public Member toEntity() {
+            return Member.builder()
+                    .id(id)
+                    .name(name)
+                    .password(password)
+                    .email(email)
+                    .build();
+        }
+    }
+}
