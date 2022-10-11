0ef2e1c081fd5e37f9b995e0fbec90d831cb298e
luckycw87 <blissful.pcw@gmail.com>
2022-10-11 18:24:59 +0900

    Fix: 샘플데이터 생성 메소드 추가

--- app/src/test/java/com/codesoom/assignment/product/common/ProductFactory.java
+++ app/src/test/java/com/codesoom/assignment/product/common/ProductFactory.java
@@ -1,16 +1,24 @@
 package com.codesoom.assignment.product.common;
 
+import com.codesoom.assignment.product.application.ProductCommand;
 import com.codesoom.assignment.product.controller.ProductDto;
 import com.codesoom.assignment.product.domain.Product;
-import com.codesoom.assignment.product.application.ProductCommand;
 
+import java.util.Random;
 import java.util.UUID;
 
+import static com.codesoom.assignment.product.common.ProductFactory.FieldName.MAKER;
+import static com.codesoom.assignment.product.common.ProductFactory.FieldName.NAME;
+import static com.codesoom.assignment.product.common.ProductFactory.FieldName.PRICE;
+
 public class ProductFactory {
+
+    private static final Random random = new Random();
+
     public static Product createProduct() {
         return Product.builder()
-                .name("고양이 장난감")
-                .maker("제조사")
+                .name("고양이 장난감" + random.nextInt(100))
+                .maker("제조사" + random.nextInt(100))
                 .price(randomPrice())
                 .imageUrl(UUID.randomUUID().toString() + ".png")
                 .build();
@@ -22,8 +30,8 @@ public class ProductFactory {
         System.out.println(product.toString());
 
         product.id(id)
-                .name("고양이 장난감" + id)
-                .maker("제조사" + id)
+                .name("고양이 장난감" + random.nextInt(100))
+                .maker("제조사" + random.nextInt(100))
                 .price(randomPrice())
                 .imageUrl(UUID.randomUUID().toString() + ".png")
                 .build();
@@ -33,10 +41,29 @@ public class ProductFactory {
 
     public static ProductDto.RequestParam createRequestParam() {
         ProductDto.RequestParam request = new ProductDto.RequestParam();
-        request.setName("고양이 장난감");
-        request.setMaker("제조사");
+        request.setName("고양이 장난감" + random.nextInt(100));
+        request.setMaker("제조사" + random.nextInt(100));
         request.setPrice(randomPrice());
         request.setImageUrl(UUID.randomUUID().toString() + ".png");
+
+        return request;
+    }
+
+    public static ProductDto.RequestParam createRequestParamWith(ProductFactory.FieldName fieldName, ProductFactory.ValueType valueType) {
+        final ProductDto.RequestParam request = new ProductDto.RequestParam();
+        String testValue;
+
+        if (valueType == ProductFactory.ValueType.NULL) {
+            testValue = null;
+        } else if (valueType == ProductFactory.ValueType.EMPTY) {
+            testValue = "";
+        } else {
+            testValue = "  ";
+        }
+
+        request.setName(fieldName == NAME ? testValue : "고양이 장난감" + random.nextInt(100));
+        request.setMaker(fieldName == MAKER ? testValue : UUID.randomUUID().toString());
+        request.setPrice(fieldName == PRICE ? null : randomPrice());
 
         return request;
     }
@@ -65,5 +92,31 @@ public class ProductFactory {
 
     public static Long randomPrice() {
         return (long) (Math.ceil((Math.random() * 10000 + 10000) / 1000) * 1000);
+    }
+
+    public enum FieldName {
+        NAME("name", "상품명"),
+        MAKER("maker", "제조사"),
+        PRICE("price", "금액");
+
+        private String name;
+        private String description;
+
+        FieldName(String name, String description) {
+            this.name = name;
+            this.description = description;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+    }
+
+    public enum ValueType {
+        NULL, EMPTY, BLANK
     }
 }
