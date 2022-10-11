e238ef172bfe08f04ff662fc179412910a8c1837
Eunha Jeong <eunhaay@gmail.com>
2022-10-11 14:21:28 +0900

    feat: UserNotFoundException 예외시 응답 추가
    - @RestControllerAdvice로 변경
    - @CrossOrigin 적용

--- app/src/main/java/com/codesoom/assignment/controllers/NotFoundErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/NotFoundErrorAdvice.java
@@ -2,18 +2,21 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.ProductNotFoundException;
 import com.codesoom.assignment.dto.ErrorResponse;
+import com.codesoom.assignment.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+/**
+ * 조회 실패시 응답을 관리합니다.
+ */
+@RestControllerAdvice
 public class NotFoundErrorAdvice {
-    @ResponseBody
+
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(ProductNotFoundException.class)
-    public ErrorResponse handleProductTaskNotFound() {
-        return new ErrorResponse("Product not found");
+    @ExceptionHandler({ProductNotFoundException.class, UserNotFoundException.class})
+    public ErrorResponse handleProductTaskNotFound(Exception e) {
+        return new ErrorResponse(e.getMessage());
     }
 }
