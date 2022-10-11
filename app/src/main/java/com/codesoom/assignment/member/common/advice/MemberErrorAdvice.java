036b2cb1b45563e3c6b20dbb5486d7b8c321c910
luckycw87 <blissful.pcw@gmail.com>
2022-10-11 16:58:30 +0900

    Add: Validation error message를 handle하기위해서 handleValidation메소드 추가

--- app/src/main/java/com/codesoom/assignment/member/common/advice/MemberErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/member/common/advice/MemberErrorAdvice.java
@@ -3,12 +3,17 @@ package com.codesoom.assignment.member.common.advice;
 import com.codesoom.assignment.member.common.exception.MemberNotFoundException;
 import com.codesoom.assignment.product.common.ErrorResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ControllerAdvice
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice(basePackages = "com.codesoom.assignment.member")
 public class MemberErrorAdvice {
 
     @ResponseBody
@@ -23,5 +28,15 @@ public class MemberErrorAdvice {
     @ExceptionHandler(IllegalArgumentException.class)
     public ErrorResponse handleBadRequest(IllegalArgumentException e) {
         return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidation(MethodArgumentNotValidException e) {
+        Map<String, String> responseBody = new HashMap<>();
+
+        e.getBindingResult().getFieldErrors()
+                .forEach(fieldError -> responseBody.put(fieldError.getField(), fieldError.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(responseBody);
     }
 }
