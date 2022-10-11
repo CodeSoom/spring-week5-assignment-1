4fa527fa304702854338b6f20898d78e0d7d3dd2
hautblis <hautblis@gmail.com>
2022-10-11 20:52:57 +0900

    feat : POST /users 에 대한 유효성 검증 구현

--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -4,14 +4,13 @@ import com.codesoom.assignment.application.UserCommandService;
 import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.dto.UserRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
@@ -24,7 +23,7 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public User createUser(@RequestBody UserRequest userRequest) {
+    public User createUser(@RequestBody @Valid UserRequest userRequest) {
         return userCommandService.createUser(userRequest);
     }
 }
--- app/src/main/java/com/codesoom/assignment/dto/UserRequest.java
+++ app/src/main/java/com/codesoom/assignment/dto/UserRequest.java
@@ -4,12 +4,17 @@ import com.github.dozermapper.core.Mapping;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Builder
 public class UserRequest {
+
+    @NotBlank
     @Mapping("email")
     private String email;
 
+    @NotBlank
     @Mapping("password")
     private String password;
 }
