659a033185eaef41db14cfb37c706fe943a7a7b5
hautblis <hautblis@gmail.com>
2022-10-12 19:24:41 +0900

    feat : PATCH /users/{id} 에 대한 구현

--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -4,6 +4,8 @@ import com.codesoom.assignment.application.UserCommandService;
 import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.dto.UserRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +27,10 @@ public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     public User createUser(@RequestBody @Valid UserRequest userRequest) {
         return userCommandService.createUser(userRequest);
+    }
+
+    @PatchMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody @Valid UserRequest userRequest) {
+        return userCommandService.updateUser(id, userRequest);
     }
 }
