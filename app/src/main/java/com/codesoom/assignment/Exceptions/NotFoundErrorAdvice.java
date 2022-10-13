7dbdf57a060d38dec53faee64845434d27ee1d07
kckoh <kyungcheolkoh@gmail.com>
2022-10-13 15:21:37 -0700

    fix: messagesource 에러 수정
    messages.properties/UserData에 typo 수정

--- app/src/main/java/com/codesoom/assignment/Exceptions/NotFoundErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/Exceptions/NotFoundErrorAdvice.java
@@ -1,14 +1,14 @@
-package com.codesoom.assignment.controllers;
+package com.codesoom.assignment.Exceptions;
 
 import com.codesoom.assignment.ProductNotFoundException;
 import com.codesoom.assignment.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class NotFoundErrorAdvice {
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -2,13 +2,17 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.UserService;
 import com.codesoom.assignment.dto.UserData;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
     private UserService userservice;
@@ -17,9 +21,10 @@ public class UserController {
         this.userservice = userservice;
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public UserData createUser(@RequestBody UserData userData) {
-        return null;
+    public UserData createUser(@RequestBody @Valid UserData userData) {
+        userservice.create(userData);
     }
 
 
