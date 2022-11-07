9932b65341fb6bd17004e21ac1ec1d697383dd47
giibeom <kpmyung@gmail.com>
2022-11-07 20:54:43 +0900

    feat: 공통 예외 구축

--- app/src/main/java/com/codesoom/assignment/exceptions/product/ProductNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/exceptions/product/ProductNotFoundException.java
@@ -1,7 +1,19 @@
 package com.codesoom.assignment.exceptions.product;
 
-public class ProductNotFoundException extends RuntimeException {
+import com.codesoom.assignment.exceptions.CommonException;
+import org.springframework.http.HttpStatus;
+
+public class ProductNotFoundException extends CommonException {
+    private static final String MESSAGE = "상품이 존재하지 않습니다.";
+
+    /**
+     * 상품을 찾지 못했을 때 던집니다.
+     */
+    public ProductNotFoundException() {
+        super(MESSAGE, HttpStatus.NOT_FOUND);
+    }
+
     public ProductNotFoundException(Long id) {
-        super("Product not found: " + id);
+        super(MESSAGE + " Id: " + id, HttpStatus.NOT_FOUND);
     }
 }
