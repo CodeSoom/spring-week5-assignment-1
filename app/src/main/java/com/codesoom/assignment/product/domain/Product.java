4a58f4550866851d8b69179b449d541f2622e996
giibeom <kpmyung@gmail.com>
2022-11-08 03:55:11 +0900

    feat: Jackson 라이브러리를 사용한 JsonUtil 추가

--- app/src/main/java/com/codesoom/assignment/product/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/product/domain/Product.java
@@ -1,7 +1,8 @@
 package com.codesoom.assignment.product.domain;
 
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,20 +13,25 @@ import javax.persistence.Id;
 @Entity
 @Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
 public class Product {
     @Id
     @GeneratedValue
     private Long id;
-
     private String name;
-
     private String maker;
-
     private Integer price;
-
     private String imageUrl;
+
+    @Builder
+    public Product(Long id, String name, String maker, Integer price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.maker = maker;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
 
     public void change(String name,
                        String maker,
