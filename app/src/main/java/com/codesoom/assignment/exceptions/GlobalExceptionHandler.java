7f38d9659f10cb0d2136d4e739a5252bae88d4bd
giibeom <kpmyung@gmail.com>
2022-11-10 20:17:06 +0900

    fix: ProductNotFoundException 에러 시 반환코드 404 설정 추가

--- app/src/main/java/com/codesoom/assignment/exceptions/GlobalExceptionHandler.java
+++ app/src/main/java/com/codesoom/assignment/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.codesoom.assignment.exceptions;
 
 import com.codesoom.assignment.exceptions.product.ProductNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -9,6 +11,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {ProductNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     protected ErrorResponse handleProductNotFoundException(final ProductNotFoundException exception) {
         return ErrorResponse.from(exception);
     }
