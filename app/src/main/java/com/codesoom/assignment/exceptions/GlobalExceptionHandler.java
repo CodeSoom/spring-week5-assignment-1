d617bbc62091974c3ac7e8454a7dabf15e619bec
giibeom <kpmyung@gmail.com>
2022-11-10 19:33:14 +0900

    refactor: Exception 객체에서의 HTTP 의존성 분리 작업

--- app/src/main/java/com/codesoom/assignment/exceptions/GlobalExceptionHandler.java
+++ app/src/main/java/com/codesoom/assignment/exceptions/GlobalExceptionHandler.java
@@ -1,28 +1,15 @@
 package com.codesoom.assignment.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import com.codesoom.assignment.exceptions.product.ProductNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.servlet.http.HttpServletRequest;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = {HttpBusinessException.class})
-    protected ResponseEntity<ErrorResponse> handleHttpBusinessException(final HttpBusinessException exception) {
-        return ResponseEntity
-                .status(exception.getStatus())
-                .body(ErrorResponse.from(exception));
-    }
-
-    @ExceptionHandler(value = {Exception.class})
-    protected ResponseEntity<ErrorResponse> handleException(final HttpServletRequest request,
-                                                            final Exception exception) {
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ErrorResponse.from(request, exception));
+    @ExceptionHandler(value = {ProductNotFoundException.class})
+    protected ErrorResponse handleProductNotFoundException(final ProductNotFoundException exception) {
+        return ErrorResponse.from(exception);
     }
 }
--- app/src/main/java/com/codesoom/assignment/exceptions/product/ProductNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/exceptions/product/ProductNotFoundException.java
@@ -1,19 +1,13 @@
 package com.codesoom.assignment.exceptions.product;
 
-import com.codesoom.assignment.exceptions.HttpBusinessException;
-import org.springframework.http.HttpStatus;
-
 /**
  * 상품을 찾지 못했을 때 던지는 예외입니다.
+ * 기본적으로 RuntimeException을 상속받기에, 예외 발생 시 roll-back을 수행합니다.
  */
-public class ProductNotFoundException extends HttpBusinessException {
+public class ProductNotFoundException extends RuntimeException {
     private static final String MESSAGE = "상품이 존재하지 않습니다.";
 
-    public ProductNotFoundException() {
-        super(MESSAGE, HttpStatus.NOT_FOUND);
-    }
-
     public ProductNotFoundException(Long id) {
-        super(MESSAGE + " Id: " + id, HttpStatus.NOT_FOUND);
+        super(MESSAGE + " Id: " + id);
     }
 }
