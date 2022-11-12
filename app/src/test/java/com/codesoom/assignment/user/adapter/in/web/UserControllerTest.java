03ed4664de9ca538503dfa0c80f411431ac1131d
giibeom <kpmyung@gmail.com>
2022-11-13 02:25:08 +0900

    test: (user-presentation) 회원 수정 성공 케이스

--- app/src/test/java/com/codesoom/assignment/user/adapter/in/web/UserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/user/adapter/in/web/UserControllerTest.java
@@ -1,6 +1,8 @@
 package com.codesoom.assignment.user.adapter.in.web;
 
+import com.codesoom.assignment.user.adapter.in.web.dto.response.UserCreateResponse;
 import com.codesoom.assignment.utils.JsonUtil;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -13,7 +15,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static com.codesoom.assignment.support.UserFixture.USER_1;
+import static com.codesoom.assignment.support.UserFixture.USER_2;
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -50,6 +54,42 @@ class UserControllerTest {
                         .andExpect(content().string(containsString(USER_1.NAME())))
                         .andExpect(content().string(containsString(USER_1.EMAIL())))
                         .andExpect(content().string(containsString(USER_1.PASSWORD())));
+            }
+        }
+    }
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class 회원_수정_API는 {
+
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 찾을_수_있는_id가_주어질_때 {
+            private Long fixtureId;
+
+            @BeforeEach
+            void setUpCreateFixture() {
+                UserCreateResponse user = userController.create(USER_1.생성_요청_데이터_생성());
+                fixtureId = user.getId();
+            }
+
+            @Nested
+            @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+            class 유효한_상품_정보라면 {
+                @Test
+                @DisplayName("200 코드를 반환한다")
+                void it_responses_200() throws Exception {
+                    mockMvc.perform(
+                                    patch("/users/" + fixtureId)
+                                            .contentType(MediaType.APPLICATION_JSON)
+                                            .content(JsonUtil.writeValue(USER_2.수정_요청_데이터_생성()))
+                            )
+                            .andExpect(status().isOk())
+                            .andExpect(content().string(containsString(String.valueOf(fixtureId))))
+                            .andExpect(content().string(containsString(USER_2.NAME())))
+                            .andExpect(content().string(containsString(USER_2.EMAIL())))
+                            .andExpect(content().string(containsString(USER_2.PASSWORD())));
+                }
             }
         }
     }
