4d55954cbce634b03ebe99cd4f845411676790a2
giibeom <kpmyung@gmail.com>
2022-11-12 23:42:51 +0900

    test: (user-application) 회원 생성 시 회원 목록 카운트 증가 케이스

--- app/src/test/java/com/codesoom/assignment/user/application/UserServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/user/application/UserServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static com.codesoom.assignment.support.UserFixture.USER_1;
+import static com.codesoom.assignment.support.UserFixture.USER_2;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -17,21 +18,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class UserServiceTest {
 
     private UserService userService;
+    private FakeInMemoryUserPersistenceAdapter fakeUserRepository;
 
     @BeforeEach
     void setUpVariable() {
-        FakeInMemoryUserPersistenceAdapter fakeUserRepository = new FakeInMemoryUserPersistenceAdapter();
+        fakeUserRepository = new FakeInMemoryUserPersistenceAdapter();
         userService = new UserService(fakeUserRepository);
     }
 
-    /*
-      CreateUser 메서드는
-       - 새로운 회원이 주어지면
-            - 회원을 저장하고 리턴한다.
-            - 회원이 1 증가한다.
-       - null이 주어지면
-            - 예외를 던진다
-     */
     @Nested
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
     class createUser_메서드는 {
@@ -47,6 +41,19 @@ class UserServiceTest {
                 assertThat(user.getName()).isEqualTo(USER_1.NAME());
                 assertThat(user.getEmail()).isEqualTo(USER_1.EMAIL());
                 assertThat(user.getPassword()).isEqualTo(USER_1.PASSWORD());
+            }
+
+            @Test
+            @DisplayName("회원이 1 증가한다")
+            void it_returns_user_size() {
+                // TODO: service 목록조회 기능 개발 및 테스트 완료 시 교체 필요
+                int oldSize = fakeUserRepository.findAll().size();
+
+                userService.createUser(USER_2.생성_요청_데이터_생성());
+
+                int newSize = fakeUserRepository.findAll().size();
+
+                assertThat(newSize - oldSize).isEqualTo(1);
             }
         }
 
