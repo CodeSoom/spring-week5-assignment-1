b1ba34f2622b6ae0dce0d7c3a87fc566cc0bc323
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-12-07 18:33:25 +0900

    Rename: ProductErrorAdvice로 클래스명 변경

--- app/src/main/java/com/codesoom/assignment/controllers/ProductErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductErrorAdvice.java
@@ -1,6 +1,7 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.ProductNotFoundException;
+import com.codesoom.assignment.ProductNotValidException;
 import com.codesoom.assignment.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,6 +15,13 @@ public class NotFoundErrorAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ProductNotFoundException.class)
     public ErrorResponse handleProductTaskNotFound() {
-        return new ErrorResponse("Product not found");
+        return new ErrorResponse("상품을 찾을 수 없습니다.");
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ProductNotValidException.class)
+    public ErrorResponse handleProductNotValid() {
+        return new ErrorResponse("[이름, 메이커, 가격]은 필수 입력사항입니다.");
     }
 }
