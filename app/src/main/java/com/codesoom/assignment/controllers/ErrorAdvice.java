ab12075c09699cd40d7099ae9e38ad729aa7048d
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-12-10 17:36:15 +0900

    Feat: ErrorAdvice 파일명 변경 및 UserNotFoundException 추가
    Product와 User 공통으로 사용하기 위해 ErrorAdvice로 파일명 변경함
    UserNotFoundExcpetion 추가함

--- app/src/main/java/com/codesoom/assignment/controllers/ErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ErrorAdvice.java
@@ -1,6 +1,7 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.ProductNotFoundException;
+import com.codesoom.assignment.UserNotFoundException;
 import com.codesoom.assignment.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,19 +12,29 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.stream.Collectors;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 @ControllerAdvice
 public class ErrorAdvice {
     @ResponseBody
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(ProductNotFoundException.class)
     public ErrorResponse handleProductTaskNotFound() {
         return new ErrorResponse("상품을 찾을 수 없습니다.");
     }
 
     @ResponseBody
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public ErrorResponse handleUserNotFound() {
+        return new ErrorResponse("해당하는 유저를 찾을 수 없습니다.");
+    }
+
+    @ResponseBody
+    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponse handleProductNotValid(MethodArgumentNotValidException ex) {
+    public ErrorResponse handleArgumentNotValid(MethodArgumentNotValidException ex) {
         String message = ex.getBindingResult()
                 .getFieldErrors()
                 .stream()
