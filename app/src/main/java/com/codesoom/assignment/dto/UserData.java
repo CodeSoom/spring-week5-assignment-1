476c03f8c0cff80fc3de63f56c0b3872f7e52548
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-12-10 17:40:13 +0900

    Fix: UserDto 클래스 Entity 관련 애노테이션 제거
    Entity는 User 클래스에서 사용하므로 UserDto에서는 제거함

--- app/src/main/java/com/codesoom/assignment/dto/UserData.java
+++ app/src/main/java/com/codesoom/assignment/dto/UserData.java
@@ -5,19 +5,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
-@Entity
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserData {
-    @Id
-    @GeneratedValue
     Long id;
 
     //이름, 이메일, 비밀번호 필수
