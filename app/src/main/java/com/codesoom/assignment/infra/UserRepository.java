cf7aef7f470a8434a2f69741f23c96c52f5c3ab1
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-12-10 17:41:16 +0900

    Feat: findAll 메소드 추가
    findAll 메소드 추가함

--- app/src/main/java/com/codesoom/assignment/infra/UserRepository.java
+++ app/src/main/java/com/codesoom/assignment/infra/UserRepository.java
@@ -4,10 +4,14 @@ import com.codesoom.assignment.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Override
+    List<User> findAll();
 
     @Override
     User save(User user);
