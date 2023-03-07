38e508851f42f98c5b71aace332b079c40f2da2c
tlatmsrud <tlatmsrud@naver.com>
2023-03-08 02:04:31 +0900

    NotFoundErrorAdvice를 GlobalErrorAdvice로 변경
    Vaildation 예외 Handler 기능 추가로 인한 클래스명 변경

---
+++ app/src/main/java/com/codesoom/assignment/controllers/GlobalErrorAdvice.java
@@ -0,0 +1,37 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.ProductNotFoundException;
+import com.codesoom.assignment.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
+public class NotFoundErrorAdvice {
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ErrorResponse handleProductTaskNotFound() {
+        return new ErrorResponse("Product not found");
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponse handleVaildation(MethodArgumentNotValidException e){
+        String errMessage = getNotValidErrorMessage(e);
+        return new ErrorResponse(errMessage);
+    }
+
+    private String getNotValidErrorMessage(MethodArgumentNotValidException e){
+
+        BindingResult bindingResult = e.getBindingResult();
+
+        if(bindingResult.hasErrors()){
+            return bindingResult.getFieldError().getDefaultMessage();
+        }
+
+        return "잘못된 요청정보입니다. 관리자에게 문의해주세요.";
+    }
+}
