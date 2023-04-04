de5d94cf307c64a07250570d51303c1b38bca1a4
yushin <kkad45@naver.com>
2023-04-04 22:21:19 +0900

    feat 멤버 생성 로직 유효성 검증을 실패했을 경우 400 에러를 반환하도록 테스트 케이스 구현

--- app/src/test/java/com/codesoom/assignment/controllers/MemberControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/MemberControllerTest.java
@@ -7,21 +7,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultHandler;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -35,18 +30,29 @@ class MemberControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    MemberData memberData;
+
+    @BeforeEach
+    public void init() {
+        MemberData.builder()
+                .name("김유신")
+                .phone("01047105883")
+                .build();
+
+        given(memberService.create(memberData)).willReturn(Member.builder()
+                .name("김유신")
+                .phone("01047105883")
+                .build());
+
+        given(memberService.create(any())).willReturn(Member.builder()
+                .name("김유신")
+                .phone("01047105883")
+                .build());
+    }
 
     @Nested
     @DisplayName("Create 메소드는")
     class Create {
-
-        @BeforeEach
-        public void init() {
-            given(memberService.create(any())).willReturn(Member.builder()
-                    .name("김유신")
-                    .phone("01047105883")
-                    .build());
-        }
 
         @Nested
         @DisplayName("모든 파라미터가 유효할 때")
@@ -55,13 +61,6 @@ class MemberControllerTest {
             @Test
             @DisplayName("정상적으로 멤버를 만든다.")
             public void createValidMemberWithAllParameter() throws Exception {
-                MemberController memberController = new MemberController(memberService);
-
-                MemberData memberData = MemberData.builder()
-                        .name("김유신")
-                        .phone("01047105883")
-                        .build();
-
                 mockMvc.perform(MockMvcRequestBuilders.post("/member")
                                 .accept(MediaType.APPLICATION_JSON_UTF8)
                                 .contentType(MediaType.APPLICATION_JSON)
@@ -78,6 +77,16 @@ class MemberControllerTest {
             @Test
             @DisplayName("400 에러를 던진다.")
             void throwBadRequest() throws Exception {
+                MemberData memberData = MemberData.builder()
+                        .name("name")
+                        .phone("")
+                        .build();
+
+                mockMvc.perform(MockMvcRequestBuilders.post("/member")
+                                .accept(MediaType.APPLICATION_JSON_UTF8)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(memberData.toString()))
+                        .andExpect(status().isBadRequest());
             }
         }
     }
