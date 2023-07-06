7be36801749ce28c389e417bc67539537b5af47e
= <dadajjjung@gmail.com>
2023-07-07 01:37:40 +0900

    UserControllerTest 수정
    fixture (테스트용 데이터) 추가

--- app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
@@ -6,6 +6,8 @@ import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.dto.CreateUserData;
 import com.codesoom.assignment.dto.UpdateUserData;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,41 +37,56 @@ class UserControllerTest {
     @MockBean
     UserService userService;
 
+    //fixture
     private User user;
     private User source;
+    private CreateUserData validCreateUserData;
+    private CreateUserData invalidCreateUserData;
+    private UpdateUserData validUpdateUserData;
+    private UpdateUserData invalidUpdateUserData;
+    private static final Long VALID_ID = 1L;
+    private static final Long INVALID_ID = 100L;
+    private static final String NAME = "dh";
+    private static final String EMAIL = "dh@gmail.com";
+    private static final String PASSWORD = "1111";
+    private static final String UPDATED_NAME = "dhj";
+    private static final String UPDATED_EMAIL = "dh@naver.com";
+    private static final String UPDATED_PASSWORD = "2222";
     @BeforeEach
     void setup(){
         user = User.builder()
-                .id(1L)
-                .name("dh")
-                .email("dh@gmail.com")
-                .password("1111")
+                .id(VALID_ID)
+                .name(NAME)
+                .email(EMAIL)
+                .password(PASSWORD)
                 .build();
 
         source = User.builder()
-                .id(1L)
-                .name("dh")
-                .email("dh@naver.com")
-                .password("2222")
+                .id(VALID_ID)
+                .name(UPDATED_NAME)
+                .email(UPDATED_EMAIL)
+                .password(UPDATED_PASSWORD)
                 .build();
 
+        validCreateUserData = DozerBeanMapperBuilder.buildDefault().map(user, CreateUserData.class);
+
+        validUpdateUserData = DozerBeanMapperBuilder.buildDefault().map(source, UpdateUserData.class);
+
+        invalidCreateUserData = new CreateUserData();
+
+        invalidUpdateUserData = new UpdateUserData();
+
         given(userService.createUser(any(CreateUserData.class))).willReturn(user);
-        given(userService.updateUser(eq(1L),any(UpdateUserData.class))).willReturn(source);
-        given(userService.updateUser(eq(100L),any(UpdateUserData.class))).willThrow(new UserNotFoundException(100L));
-        given(userService.deleteUser(100L)).willThrow(new UserNotFoundException(100L));
+        given(userService.updateUser(eq(VALID_ID),any(UpdateUserData.class))).willReturn(source);
+        given(userService.updateUser(eq(INVALID_ID),any(UpdateUserData.class))).willThrow(new UserNotFoundException(INVALID_ID));
+        given(userService.deleteUser(INVALID_ID)).willThrow(new UserNotFoundException(INVALID_ID));
     }
 
     @Test
     void createWithValidAttributes() throws Exception {
-        CreateUserData createUserData = CreateUserData.builder()
-                .name(user.getName())
-                .email(user.getEmail())
-                .password(user.getPassword())
-                .build();
-
         mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(createUserData)))
+                        .content(objectMapper.writeValueAsString(validCreateUserData)))
                 .andExpect(status().isCreated())
                 .andExpect(content().string(containsString(user.getName())));
 
@@ -78,77 +95,57 @@ class UserControllerTest {
 
     @Test
     void createWithInvalidAttributes() throws Exception {
-        CreateUserData invalidUser = CreateUserData.builder()
-                .name("")
-                .email("")
-                .password("")
-                .build();
-
         mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(invalidUser)))
+                        .content(objectMapper.writeValueAsString(invalidCreateUserData)))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
     void updateWithExistingUser() throws Exception {
-        UpdateUserData updateUserData = UpdateUserData.builder()
-                .name(source.getName())
-                .email(source.getEmail())
-                .password(source.getPassword())
-                .build();
-
-        mockMvc.perform(patch("/users/1")
+        mockMvc.perform(patch("/users/"+VALID_ID)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(source))
+                        .content(objectMapper.writeValueAsString(validUpdateUserData))
                         .accept(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString(source.getName())));
 
-        verify(userService).updateUser(eq(1L),any(UpdateUserData.class));
+        verify(userService).updateUser(eq(VALID_ID),any(UpdateUserData.class));
     }
 
     @Test
     void updateWithNotExistingUser() throws Exception {
-        UpdateUserData updateUserData = UpdateUserData.builder()
-                .name(source.getName())
-                .email(source.getEmail())
-                .password(source.getPassword())
-                .build();
-
-        mockMvc.perform(patch("/users/100")
+        mockMvc.perform(patch("/users/"+INVALID_ID)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(source))
+                        .content(objectMapper.writeValueAsString(validUpdateUserData))
                         .accept(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isNotFound());
 
-        verify(userService).updateUser(eq(100L),any(UpdateUserData.class));
+        verify(userService).updateUser(eq(INVALID_ID),any(UpdateUserData.class));
     }
 
     @Test
     void updateWithInvalidAttributes() throws Exception {
-        UpdateUserData invalidUser = new UpdateUserData();
-
         mockMvc.perform(patch("/users/1")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(invalidUser)))
+                        .content(objectMapper.writeValueAsString(invalidUpdateUserData)))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
     void deleteWithExistingUser() throws Exception {
-        mockMvc.perform(delete("/users/1"))
+        mockMvc.perform(delete("/users/"+VALID_ID))
                 .andExpect(status().isNoContent());
 
-        verify(userService).deleteUser(1L);
+        verify(userService).deleteUser(VALID_ID);
     }
 
     @Test
     void deleteWithNotExistingUser() throws Exception {
-        mockMvc.perform(delete("/users/100"))
+        mockMvc.perform(delete("/users/"+INVALID_ID))
                 .andExpect(status().isNotFound());
-        
-        verify(userService).deleteUser(100L);
+
+        verify(userService).deleteUser(INVALID_ID);
     }
 
 }
