1c2aa84014f9caf05cf51a55a69f19855f4b1252
juhyeon <tmxhsk99@naver.com>
2023-08-02 08:37:13 +0900

    test : 중복되는 어노테이션 삭제

--- app/src/test/java/com/codesoom/assignment/application/JpaTest.java
+++ app/src/test/java/com/codesoom/assignment/application/JpaTest.java
@@ -1,12 +1,9 @@
 package com.codesoom.assignment.application;
 
 import com.codesoom.assignment.domain.user.UserRepository;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class JpaTest {
     @Autowired
