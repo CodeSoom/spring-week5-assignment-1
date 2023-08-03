673231e564fdb5edd255081f277440bc0bb33c66
juhyeon <tmxhsk99@naver.com>
2023-08-04 08:55:36 +0900

    feat : validation 메시지 추가

--- app/src/main/java/com/codesoom/assignment/dto/product/ProductData.java
+++ app/src/main/java/com/codesoom/assignment/dto/product/ProductData.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 
 @Setter
 @Getter
@@ -13,16 +14,16 @@ import javax.validation.constraints.NotNull;
 public class ProductData {
     private Long id;
 
-    @NotBlank
+    @NotBlank (message = "상품 이름을 입력해주세요.")
     private String name;
 
-    @NotBlank
+    @NotBlank (message = "상품 제조사를 입력해주세요.")
     private String maker;
 
-    @NotNull
+    @NotNull (message = "상품 가격을 입력해주세요.")
+    @PositiveOrZero (message = "상품 가격은 0 이상이어야 합니다.")
     private Integer price;
 
     private String imageUrl;
-
 
 }
