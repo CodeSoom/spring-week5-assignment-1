1d999b9dac685e0f8a69219f8989f8cd428c77fc
tmxhsk99 <tmxhsk99@naver.com>
2023-08-05 10:32:47 +0900

    test : UserController 유저 수정 기능 작성

--- app/src/test/java/com/codesoom/assignment/controllers/user/UserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/user/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.controllers.user;
 
+import com.codesoom.assignment.domain.user.User;
 import com.codesoom.assignment.domain.user.UserRepository;
 import com.codesoom.assignment.dto.user.UserData;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,26 +21,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DisplayName("UserController 클래스")
 class UserControllerTest {
 
+    private final String TEST_NAME = "testName";
+    private final String TEST_EMAIL = "test@Email";
+    private final String TEST_PASSWORD = "testPassword";
     @Autowired
     MockMvc mockMvc;
-
     @Autowired
     UserRepository userRepository;
-
     @Autowired
     ObjectMapper objectMapper;
+    private UserData USER_REQUEST;
 
     @Nested
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
     class create_메서드는 {
-        private final String TEST_NAME = "testName";
-        private final String TEST_EMAIL = "test@Email";
-        private final String TEST_PASSWORD = "testPassword";
-        private UserData USER_REQUEST;
+
 
         @Nested
         @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-        class 유저_정보가_요청이_오면 {
+        class 유저_정보_요청이_오면 {
 
             @BeforeEach
             void setUp() {
@@ -50,7 +50,7 @@ class UserControllerTest {
                         .build();
             }
 
-            @DisplayName("해당_유저정보를_저장_후_저장한_유저정보를_리턴한다")
+            @DisplayName("해당_유저정보를_등록_후_저장한_유저정보를_리턴한다")
             @Test
             void it_saves_and_returns_user() throws Exception {
                 String jsonString = objectMapper.writeValueAsString(USER_REQUEST);
@@ -64,6 +64,52 @@ class UserControllerTest {
                         .andExpect(jsonPath("password").value("testPassword"))
                         .andDo(print());
             }
+        }
+    }
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class update_메서드 {
+
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 유저_수정_요청이_오면 {
+            private Long id;
+
+            @BeforeEach
+            void setUp() {
+                userRepository.deleteAll();
+                User user = User.builder()
+                        .name(TEST_NAME)
+                        .email(TEST_EMAIL)
+                        .password(TEST_PASSWORD)
+                        .build();
+
+                id = userRepository.save(user).getId();
+            }
+
+            @DisplayName("해당_유저정보를_수정_후_수정한_유저정보를_리턴한다")
+            @Test
+            void it_updates_and_returns_user() throws Exception {
+                UserData request = UserData.builder()
+                        .name("newName")
+                        .email("newEmail")
+                        .password("newPassword")
+                        .build();
+
+                String jsonString = objectMapper.writeValueAsString(request);
+
+                mockMvc.perform(post("/users/" + id)
+                                .contentType("application/json")
+                                .content(jsonString))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("name").value("newName"))
+                        .andExpect(jsonPath("email").value("newEmail"))
+                        .andExpect(jsonPath("password").value("newPassword"))
+                        .andDo(print());
+
+            }
+
         }
     }
 }
