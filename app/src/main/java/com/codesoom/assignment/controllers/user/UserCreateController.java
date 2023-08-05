e5e4d16ba2a7f3c0145ccf298e0af6369605e82c
tmxhsk99 <tmxhsk99@naver.com>
2023-08-05 17:37:36 +0900

    refactor : 유저 컨트롤러 분리

--- app/src/main/java/com/codesoom/assignment/controllers/user/UserCreateController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/user/UserCreateController.java
@@ -1,8 +1,6 @@
 package com.codesoom.assignment.controllers.user;
 
 import com.codesoom.assignment.application.user.UserCreator;
-import com.codesoom.assignment.application.user.UserDeleter;
-import com.codesoom.assignment.application.user.UserUpdater;
 import com.codesoom.assignment.domain.user.User;
 import com.codesoom.assignment.dto.user.UserData;
 import lombok.RequiredArgsConstructor;
@@ -14,28 +12,13 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
-public class UserController {
-
+public class UserCreateController {
     private final UserCreator userCreator;
-    private final UserUpdater userUpdater;
-    private final UserDeleter userDeleter;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public User create(@RequestBody @Valid UserData userData) {
         return userCreator.createUser(userData);
-    }
-
-    @PatchMapping({"/{id}"})
-    public User update(@PathVariable Long id,
-                       @RequestBody UserData userData) {
-        return userUpdater.update(id, userData);
-    }
-
-    @DeleteMapping({"/{id}"})
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id) {
-        userDeleter.delete(id);
     }
 
 }
